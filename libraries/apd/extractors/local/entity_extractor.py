a1f01f0c83f87a1c5ec4b39cb1b72dc69f28cb5a
NicholasMamo <nicholasmamo@gmail.com>
2019-09-18 16:33:16 +0200

    Update the entity extractor with an experimental new version

--- libraries/apd/extractors/local/entity_extractor.py
+++ libraries/apd/extractors/local/entity_extractor.py
@@ -2,6 +2,19 @@
 An extractor that only considers named entities to be potential candidates.
 """
 
+"""
+Experiment:
+	>>> import apd.extractors.local.entity_extractor as entity_extractor
+	>>> from vector.nlp.document import Document
+	>>> extractor = entity_extractor.EntityExtractor()
+	>>> corpus = [ Document("Watford vs Arsenal bets; Holebas to be booked 3.5 (Betfred/Betfair Exchange) 2pt; Capoue to be booked 3.5 (WillHill/Bethard) 1pt; Sokratis to be booked 4.33 (Bet365) 1pt; Andre Gray over 1.5 shots 1.8 (WillHill) 1pt") ]
+	>>> extractor.extract(corpus)
+	[['watford', 'arsenal', 'holebas', 'capoue', 'sokratis', 'bet365', 'andre gray', 'willhill']]
+
+Note that newlines have to be replaced with a sentence end
+
+"""
+
 import os
 import sys
 
@@ -48,52 +61,59 @@ class EntityExtractor(Extractor):
 
 		documents = []
 		for document in tokenized_corpus:
-			"""
-			Extract the named entities in the document.
-			"""
-			pos_tags = nltk.pos_tag(document.get_attribute("tokens"))
-			entities = [ entity for entity in nltk.ne_chunk(pos_tags) ]
+			sentences = nltk.sent_tokenize(document.get_text())
+			
+			document_entities = []
+			for sentence in sentences:
+				"""
+				Extract the named entities in the document.
+				"""
+				tokens = nltk.word_tokenize(sentence)
+				pos_tags = nltk.pos_tag(tokens)
+				entities = [ entity for entity in nltk.ne_chunk(pos_tags) ]
 
-			"""
-			Find the named entities and concatenate consecutive ones.
-			"""
-			named_entities = []
-			current_entity, current_entity_type = [], None
-			for entity in entities:
 				"""
-				If the entity is an actual named entity, try to connect it to previous entities.
-				If it is not an entity, add the current named entity sequence to the list of found ones.
-				Finally, reset the current entity memory.
+				Find the named entities and concatenate consecutive ones.
 				"""
-				if type(entity) == nltk.tree.Tree:
+				named_entities = []
+				current_entity, current_entity_type = [], None
+				for entity in entities:
 					"""
-					If the type of named entity has changed, retire it.
-					This means adding the sequence to the list of named entities, and resetting the memory.
+					If the entity is an actual named entity, try to connect it to previous entities.
+					If it is not an entity, add the current named entity sequence to the list of found ones.
+					Finally, reset the current entity memory.
 					"""
-					label = entity.label()
-					if label != current_entity_type:
+					if type(entity) == nltk.tree.Tree:
+						"""
+						If the type of named entity has changed, retire it.
+						This means adding the sequence to the list of named entities, and resetting the memory.
+						"""
+						label = entity.label()
+						if label != current_entity_type:
+							name = ' '.join(current_entity).strip()
+							named_entities.append(name.lower())
+							current_entity, current_entity_type = [], None
+
+						"""
+						Then, update the label - or leave it unchanged, if this is a sequence.
+						The tokens are added to the sequence.
+						"""
+						current_entity_type = label
+						named_entity_tokens = [pair[0].lower() for pair in entity]
+						current_entity.extend(named_entity_tokens)
+					else:
 						name = ' '.join(current_entity).strip()
 						named_entities.append(name.lower())
 						current_entity, current_entity_type = [], None
 
-					"""
-					Then, update the label - or leave it unchanged, if this is a sequence.
-					The tokens are added to the sequence.
-					"""
-					current_entity_type = label
-					named_entity_tokens = [pair[0].lower() for pair in entity]
-					current_entity.extend(named_entity_tokens)
-				else:
-					name = ' '.join(current_entity).strip()
-					named_entities.append(name.lower())
-					current_entity, current_entity_type = [], None
-
-			"""
-			Wrap up in the end by saving the named entity.
-			"""
-			name = ' '.join(current_entity).strip()
-			named_entities.append(name.lower())
-			named_entities = [entity for entity in named_entities if len(entity) > 0]
-			documents.append(named_entities)
+				"""
+				Wrap up in the end by saving the named entity.
+				"""
+				name = ' '.join(current_entity).strip()
+				named_entities.append(name.lower())
+				named_entities = [entity for entity in named_entities if len(entity) > 0]
+				document_entities.extend(named_entities)
+			
+			documents.append(document_entities)
 
 		return documents
