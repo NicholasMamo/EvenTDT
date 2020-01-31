802a873e5b52f52219ed9a4d79ace11e2de9bcf6
NicholasMamo <nicholasmamo@gmail.com>
2020-01-31 17:07:06 +0100

    Create the general term-weighting class

--- eventdt/nlp/term_weighting/scheme.py
+++ eventdt/nlp/term_weighting/scheme.py
@@ -2,6 +2,62 @@
 Term-weighting schemes give different weights to different terms in a document.
 """
 
+from abc import ABC, abstractmethod
+
+import os
+import sys
+
+path = os.path.join(os.path.dirname(__file__), '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from document import Document
+
+class TermWeightingScheme(object):
+	"""
+	This class defines a list of properties that all term-weighting schemes must have.
+
+	:ivar local_scheme: The local term-weighting scheme.
+	:vartype local_scheme: :class:`eventdt.nlp.term_weighting.scheme.Scheme`
+	:ivar global_scheme: The global term-weighting scheme.
+	:vartype global_scheme: :class:`eventdt.nlp.term_weighting.scheme.Scheme`
+	"""
+
+	def __init__(self, local_scheme, global_scheme):
+		"""
+		A term-weighting scheme is made up of a local component and a global component.
+
+		:param local_scheme: The local term-weighting scheme.
+		:type local_scheme: :class:`eventdt.nlp.term_weighting.scheme.Scheme`
+		:param global_scheme: The global term-weighting scheme.
+		:type global_scheme: :class:`eventdt.nlp.term_weighting.scheme.Scheme`
+		"""
+
+		self.local_scheme = local_scheme
+		self.global_scheme = global_scheme
+
+	def create(self, tokens, *args, **kwargs):
+		"""
+		Create a document from the given tokens.
+		The function multiplies the local and global scores of each token.
+		The function accepts other arguments or keyword arguments, such as the document text or attributes.
+		These are passed on to the :class:`eventdt.nlp.document.Document` constructor.
+
+		:param tokens: A list of tokens that can be converted into dimensions.
+		:type tokens: list of str
+
+		:return: A new document with the given tokens as the dimensions.
+				 The dimensions' weights depend on the term-weighting scheme.
+		:rtype: :class:`eventdt.nlp.document.Document`
+		"""
+
+		local_score, global_score = self.local_scheme.score(tokens), self.global_scheme.score(tokens)
+		dimensions = {}
+		for dimension in local_score.keys() | global_score.keys():
+			dimensions[dimension] = local_score.get(dimension, 0) * global_score.get(dimension, 0)
+
+		return Document(dimensions=dimensions, *args, **kwargs)
+
 class SchemeScorer(ABC):
 	"""
 	A scheme is used to score documents' tokens.
