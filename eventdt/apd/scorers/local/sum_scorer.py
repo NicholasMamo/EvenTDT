bdabb018c163745b8a3f2c2b443fdc70086d0d9a
NicholasMamo <nicholasmamo@gmail.com>
2020-02-12 15:37:45 +0100

    Add support to disable score normalization

--- eventdt/apd/scorers/local/sum_scorer.py
+++ eventdt/apd/scorers/local/sum_scorer.py
@@ -9,23 +9,25 @@ from ..scorer import Scorer
 class SumScorer(Scorer):
 	"""
 	The basic summation scorer assigns a score to tokens based on the significance of their appearance.
-	This scorer rescales the scores of each document to be between 0 and 1.
 	"""
 
-	def score(self, candidates, *args, **kwargs):
+	def score(self, candidates, normalize_scores=True, *args, **kwargs):
 		"""
 		Score the given candidates based on their relevance within the corpus.
 		The score is normalized using the maximum score
 
 		:param candidates: A list of candidates participants that were found earlier.
 		:type candidates: list
+		:param normalize_scores: A boolean indicating whether the scores should be normalized.
+								 Here, normalization means rescaling between 0 and 1.
+		:type normalize_scores: bool
 
 		:return: A dictionary of participants and their associated scores.
 		:rtype: dict
 		"""
 
 		scores = self._sum(candidates, *args, **kwargs)
-		return self._normalize(scores)
+		return self._normalize(scores) if normalize_scores else scores
 
 	def _sum(self, candidates, *args, **kwargs):
 		"""
@@ -76,7 +78,7 @@ class LogSumScorer(SumScorer):
 	This scorer rescales the scores of each document to be between 0 and 1.
 	"""
 
-	def score(self, candidates, base=10, *args, **kwargs):
+	def score(self, candidates, base=10, normalize_scores=True, *args, **kwargs):
 		"""
 		Score the given candidates based on their relevance within the corpus.
 		The score is normalized using the maximum score
@@ -85,6 +87,9 @@ class LogSumScorer(SumScorer):
 		:type candidates: list
 		:param base: The base of the logarithm.
 		:type base: int
+		:param normalize_scores: A boolean indicating whether the scores should be normalized.
+								 Here, normalization means rescaling between 0 and 1.
+		:type normalize_scores: bool
 
 		:return: A dictionary of participants and their associated scores.
 		:rtype: dict
@@ -92,4 +97,4 @@ class LogSumScorer(SumScorer):
 
 		scores = self._sum(candidates)
 		scores = { candidate: math.log(score + 1, base) for candidate, score in scores.items() } # apply Laplace smoothing
-		return self._normalize(scores)
+		return self._normalize(scores) if normalize_scores else scores
