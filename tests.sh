48ae214a7c0743905cb65c92ecefee0c33f1dbba
NicholasMamo <nicholasmamo@gmail.com>
2020-02-12 16:52:43 +0100

    Update the general tests script with the new tests

--- tests.sh
+++ tests.sh
@@ -18,6 +18,8 @@ apd_tests() {
 
 	echo -e "${HIGHLIGHT}Scorer${DEFAULT}"
 	python3 -m unittest eventdt.apd.scorers.local.tests.test_df_scorer
+	python3 -m unittest eventdt.apd.scorers.local.tests.test_log_df_scorer
+	python3 -m unittest eventdt.apd.scorers.local.tests.test_tf_scorer
 
 	# echo -e "${HIGHLIGHT}Resolver${DEFAULT}"
 	# python3 -m unittest eventdt.apd.resolvers.local.tests.test_local_resolvers
