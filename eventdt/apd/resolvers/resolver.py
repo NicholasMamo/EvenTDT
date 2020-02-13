3635cf4771fc2cfc766153b906a0a597230b970c
NicholasMamo <nicholasmamo@gmail.com>
2020-02-13 15:49:49 +0100

    Refactor the resolver and add documentation

--- eventdt/apd/resolvers/resolver.py
+++ eventdt/apd/resolvers/resolver.py
@@ -1,26 +1,30 @@
 """
-A resolver takes in candidates and resolves them to an alternative representation.
+The resolver is the fourth step of APD.
+Its job is to take a list of candidate participants and try to transform them into actual participants.
+
+The representation between candidates and participants can remain almost identical.
+For example a stemmed token can be replaced by a proper term.
+It can also change the representation radically.
+For example, it can map a named entity to a Wikipedia concept.
+
+Resolvers return a tuple, containing resolved and unresolved candidates.
+The unresolved candidates are usually discarded.
 """
 
 class Resolver(object):
 	"""
-	The simplest resolver returns the candidates without any resolution.
+	The simplest resolver returns all candidates without any resolution.
 	"""
 
-	def resolve(self, candidates, corpus, token_attribute="tokens", *args, **kwargs):
+	def resolve(self, candidates, *args, **kwargs):
 		"""
-		Resolve the given candidates.
-		This resolver changes nothing.
+		The resolution function returns the same candidates as they were given without making any changes.
 
 		:param candidates: The candidates to resolve.
 		:type candidates: list
-		:param corpus: The corpus of documents, which helps to resolve the candidates.
-		:type corpus: list
-		:param token_attribute: The attribute that contains the tokens.
-		:type token_attribute: str
 
-		:return: A tuple containing the resolved candidates, and unresolved ones.
+		:return: A tuple containing the resolved and unresolved candidates respectively.
 		:rtype: tuple
 		"""
 
-		return (candidates, [])
+		return (candidates, [ ])
