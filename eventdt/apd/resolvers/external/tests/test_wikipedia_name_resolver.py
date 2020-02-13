507d9f6982007006e8c89fec1c451dbf6b7de5e5
NicholasMamo <nicholasmamo@gmail.com>
2020-02-13 21:17:46 +0100

    Refactor the Wikipedia name resolver

---
+++ eventdt/apd/resolvers/external/tests/test_wikipedia_name_resolver.py
@@ -0,0 +1,88 @@
+"""
+Test the functionality of the Wikipedia name resolver.
+"""
+
+import os
+import random
+import string
+import sys
+import unittest
+import warnings
+
+path = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from nltk.corpus import stopwords
+
+from apd.extractors.local.entity_extractor import EntityExtractor
+from apd.scorers.local.tf_scorer import TFScorer
+from apd.filters.local.threshold_filter import ThresholdFilter
+from apd.resolvers.external.wikipedia_name_resolver import WikipediaNameResolver
+
+from nlp.document import Document
+from nlp.tokenizer import Tokenizer
+from nlp.term_weighting.tf import TF
+
+class TestWikipediaNameResolver(unittest.TestCase):
+	"""
+	Test the implementation and results of the Wikipedia name resolver.
+	"""
+
+	def test_wikipedia_name_resolver(self):
+		"""
+		Test the Wikipedia name resolver.
+		"""
+
+		"""
+		Create the test data
+		"""
+		tokenizer = Tokenizer(min_length=1, stem=False)
+		posts = [
+			"Manchester United falter against Burnley",
+		]
+		corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
+
+		candidates = EntityExtractor().extract(corpus, binary=True)
+		scores = TFScorer().score(candidates)
+		scores = ThresholdFilter().filter(scores, 0)
+
+		resolver = WikipediaNameResolver(TF(), tokenizer, 0)
+		resolved, unresolved = resolver.resolve(scores, corpus)
+
+		self.assertTrue('manchester united' in resolved)
+		self.assertTrue('burnley' in resolved)
+
+	def test_all_resolved_or_unresolved(self):
+		"""
+		Test that the resolver either resolves or does not resolve named entities.
+		"""
+
+		"""
+		Create the test data
+		"""
+		tokenizer = Tokenizer(min_length=1, stem=False)
+		posts = [
+			"Manchester United falter against Burnley",
+		]
+		corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
+
+		candidates = EntityExtractor().extract(corpus, binary=True)
+		scores = TFScorer().score(candidates)
+		scores = ThresholdFilter().filter(scores, 0)
+
+		resolver = WikipediaNameResolver(TF(), tokenizer, 0)
+		resolved, unresolved = resolver.resolve(scores, corpus)
+		self.assertEqual(len(scores), len(resolved + unresolved))
+
+	def test_random_string_unresolved(self):
+		"""
+		Test that a random string is unresolved.
+		"""
+
+		random_string = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(32))
+
+		tokenizer = Tokenizer(min_length=1, stem=False)
+		resolver = WikipediaNameResolver(TF(), tokenizer, 0)
+		resolved, unresolved = resolver.resolve({ random_string: 1 }, [ ])
+		self.assertTrue(random_string in unresolved)
--- eventdt/apd/resolvers/external/wikipedia_name_resolver.py
+++ eventdt/apd/resolvers/external/wikipedia_name_resolver.py
@@ -1,7 +1,5 @@
 """
-The Wikipedia Resolver
-A resolver that looks for keywords in Wikipedia pages.
-These pages are considered to be concepts.
+The Wikipedia name resolver looks to map candidates to pages that include their name.
 """
 
 import os
@@ -15,107 +13,176 @@ if path not in sys.path:
 
 from nltk.corpus import stopwords
 
-from logger import logger
+from vsm import vector_math
 
-from vector import vector_math
-
-from vector.nlp.document import Document
-from vector.nlp.tokenizer import Tokenizer
+from nlp.document import Document
+from nlp.tokenizer import Tokenizer
 
 from wikinterface.info_collector import InfoCollector, PageType
 from wikinterface.linkcollector import LinkCollector
-from wikinterface.searchcollector import SearchCollector
 from wikinterface.textcollector import TextCollector
 
 from ..resolver import Resolver
 
-class WikipediaResolver(Resolver):
+class WikipediaNameResolver(Resolver):
 	"""
-	The Wikipedia resolver looks for pages that match the candidate's name.
+	The Wikipedia name resolver looks for pages that match the candidate's name.
+	It then maps the candidate to that page by returning the page name instead of the candidate.
+
+	:ivar scheme: The term-weighting scheme to use to create documents from Wikipedia pages.
+				   These documents are used to compare the similarity with the domain of the candidates.
+	:vartype scheme: :class:`nlp.term_weighting.scheme.TermWeightingScheme`
+	:ivar tokenizer: The tokenizer to use to create documents.
+	:vartype tokenizer: :class:`nlp.tokenizer.Tokenizer`
+	:ivar threshold: The threshold below which candidates become unresolved.
+	:vartype threshold: float.
 	"""
 
-	def resolve(self, candidates, corpus, resolver_scheme, resolver_threshold=0, token_attribute="tokens", *args, **kwargs):
+	def __init__(self, scheme, tokenizer, threshold):
+		"""
+		Create the resolver.
+
+		:param scheme: The term-weighting scheme to use to create documents from Wikipedia pages.
+					   These documents are used to compare the similarity with the domain of the candidates.
+		:type scheme: :class:`nlp.term_weighting.scheme.TermWeightingScheme`
+		:param threshold: The threshold below which candidates become unresolved.
+		:type threshold: float.
+		:param tokenizer: The tokenizer to use to create documents.
+		:type tokenizer: :class:`nlp.tokenizer.Tokenizer`
+		:param threshold: The similarity threshold beyond which candidate participants are resolved.
+		:type threshold: float
+		"""
+
+		self.scheme = scheme
+		self.tokenizer = tokenizer
+		self.threshold = threshold
+
+	def resolve(self, candidates, corpus, *args, **kwargs):
 		"""
 		Resolve the given candidates.
 
 		:param candidates: The candidates to resolve.
-		:type candidates: list
-		:param corpus: The corpus of documents, which helps to resolve the candidates.
-		:type corpus: list
-		:param resolver_scheme: The term weighting scheme used to create documents that represent ambiguous pages.
-		:type resolver_scheme: :class:`vector.nlp.term_weighting.TermWeighting`
-		:param token_attribute: The attribute that contains the tokens.
-		:type token_attribute: str
+						   The candidates should be in the form of a dictionary.
+						   The keys should be the candidates, and the values the scores.
+		:type candidates: dict
+		:param corpus: The corpus of documents.
+		:type corpus: list of :class:`nlp.document.Document`
 
-		:return: A tuple containing the resolved candidates, and unresolved ones.
-		:rtype: tuple
+		:return: A tuple containing the resolved and unresolved candidates respectively.
+		:rtype: tuple of lists
 		"""
 
 		resolved_candidates, unresolved_candidates = [], []
-		info_collector = InfoCollector()
-		link_collector = LinkCollector()
-		search_collector = SearchCollector()
-		text_collector = TextCollector()
-		tokenizer = Tokenizer(stopwords=stopwords.words("english"))
 
-		candidates = [ candidate.title() for candidate in candidates ]
-		text = info_collector.get_type(candidates)
-		ambiguous_candidates = []
-		for candidate, content in text.items():
-			"""
-			Some pages resolve directly, though may need to redirect.
-			Those pages are retained unchanged to respect domain discourse.
-			"""
-			if content["type"] is PageType.NORMAL:
-				resolved_candidates.append(candidate)
-			elif content["type"] is PageType.DISAMBIGUATION:
-				ambiguous_candidates.append(candidate)
-			else:
-				unresolved_candidates.append(candidate)
+		candidates = [ candidate for candidate in candidates ]
+		resolved, unresolved, ambiguous = self._resolve_unambiguous_candidates(candidates)
+		resolved_candidates.extend(resolved)
+		unresolved_candidates.extend(unresolved)
+
+		return resolved_candidates, unresolved_candidates
 
 		"""
-		Get the concatenated corpus.
+		Get the concatenated corpus as a single document, representing the domain.
 		"""
-		tokenized_corpus = []
-		for document in corpus:
-			tokens = tokenizer.tokenize(document.get_text())
-			document = Document(document.get_text(), tokens, scheme=resolver_scheme)
-			tokenized_corpus.append(document)
-		corpus_document = vector_math.concatenate(tokenized_corpus)
-		corpus_document.normalize()
+		domain = Document.concatenate(*corpus, tokenizer=self.tokenizer, scheme=self.scheme)
+		domain.normalize()
 
 		"""
-		Get the links, or potential disambiguations of candidates.
+		Get the potential disambiguations of the ambiguous candidates.
+		Then, find the best page for each candidate.
+		If its similarity with the domain is sufficiently high, the candidate is resolved.
 		"""
-		ambiguous_candidates = link_collector.get_links(ambiguous_candidates, first_section_only=False)
-		for ambiguous_candidate, links in ambiguous_candidates.items():
+		ambiguous = LinkCollector().get_links(ambiguous, first_section_only=False)
+		for candidate, pages in ambiguous.items():
 			"""
-			The ambiguous titles are sorted by performing text extraction.
-			From this search, only the most similar are retained.
+			If there are candidate pages, get the most similar page.
+			If the most similar page exceeds the similarity threshold, resolve the candidate to that page.
+			Otherwise, the candidate cannot be resolved.
 			"""
+			if len(pages) > 0:
+				page, score = self._disambiguate(pages, domain)
+				if score >= self.threshold:
+					resolved_candidates.append(page)
+					continue
 
-			if len(links) > 0:
-				text_content = text_collector.get_plain_text(links, introduction_only=True)
-
-				"""
-				Get the score of each page
-				"""
-				page_scores = { }
-				for page, text in text_content.items():
-					tokens = tokenizer.tokenize(text)
-					document = Document(text, tokens, scheme=resolver_scheme)
-					document.normalize()
-					page_scores[page] = vector_math.cosine(document, corpus_document)
-
-				"""
-				Save only the most similar page if it exceeds the threshold.
-				"""
-				chosen_candidate, score = sorted(page_scores.items(), key=lambda x:x[1])[:-2:-1][0]
-				if score >= resolver_threshold:
-					resolved_candidates.append(chosen_candidate)
-				else:
-					unresolved_candidates.append(ambiguous_candidate)
-			else:
-				unresolved_candidates.append(ambiguous_candidate)
+			unresolved_candidates.append(candidate)
 
 		return (resolved_candidates, unresolved_candidates)
+
+	def _resolve_unambiguous_candidates(self, candidates):
+		"""
+		Resolve the candidates that are unambiguous.
+		The function handles three possiblities:
+
+			#. There are candidates that have a page, and therefore the candidate is resolved to them.
+			#. Others have a disambiguation page and are thus ambiguous.
+			   These candidates are resolved elsewhere.
+			#. Other candidates return an empty result.
+			   In this case, they are said to be unresolved.
+
+		:param candidates: The candidates to resolve.
+						   The candidates should be in the form of a dictionary.
+						   The keys should be the candidates, and the values the scores.
+		:type candidates: dict
+
+		:return: A tuple containing the resolved, unresolved and ambiguous candidates respectively.
+		:rtype: tuple of lists
+		"""
+
+		resolved_candidates, unresolved_candidates, ambiguous_candidates = [], [], []
+
+		for candidate in candidates:
+			text = InfoCollector().get_type([ candidate ])
+			for page, content in text.items():
+				"""
+				Some pages resolve directly, though may need to redirect.
+				Those pages are retained unchanged to respect domain discourse.
+				"""
+				if content["type"] is PageType.NORMAL:
+					resolved_candidates.append(candidate)
+					break
+				elif content["type"] is PageType.DISAMBIGUATION:
+					ambiguous_candidates.append(candidate)
+					break
+
+			"""
+			If the candidate could not be resolved or if it does not have a disambiguation, the candidate cannot be resolved.
+			"""
+			if (candidate not in resolved_candidates and
+				candidate not in ambiguous_candidates):
+				unresolved_candidates.append(candidate)
+
+		return resolved_candidates, unresolved_candidates, ambiguous_candidates
+
+	def _disambiguate(self, pages, domain):
+		"""
+		Disambiguate a candidate by finding the link that is most similar to the domain.
+		The function returns the link's page name and the associated score.
+		Only one page is returned: the one with the highest score.
+
+		:param pages: A list
+		:type pages: TODO: complete
+		:param domain: A document that represents the domain.
+		:type domain: :class:`nlp.document.Document`
+
+		:return: A tuple containing the most similar page and its similarity score.
+		:rtype: tuple
+		"""
+
+		"""
+		Get the first section of each page.
+		Then, convert them into documents.
+		"""
+		pages = TextCollector().get_plain_text(pages, introduction_only=True)
+		for page, text in pages.items():
+			pages[page] = Document(text, self.tokenizer.tokenize(text),
+								   scheme=self.scheme)
+			pages[page].normalize()
+
+		"""
+		Rank the page scores in descending order.
+		Then, choose the best page and return it alongside its score.
+		"""
+		page_scores = { page: vector_math.cosine(text, domain) for page, text in pages.items() }
+		best_page = max(page_scores, key=lambda page: page_scores.get(page))
+		return (best_page, page_scores[best_page])
