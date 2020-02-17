f1d8785bd0761388a340963e791f1cdcc07c606a
NicholasMamo <nicholasmamo@gmail.com>
2020-02-17 15:12:10 +0100

    Create a function to revert redirections

--- eventdt/wikinterface/__init__.py
+++ eventdt/wikinterface/__init__.py
@@ -18,3 +18,59 @@ def is_error_response(response):
 
 	return 'error' in response
 
+def revert_redirects(results, redirects, keep_redirects=False):
+	"""
+	Revert redirections.
+
+	The function takes in search results with keys as the page titles.
+	The redirections are those retorned by Wikipedia to redirect these keys.
+
+	:param results: Any results obtained from Wikipedia.
+					It is assumed that the keys are the page titles.
+	:type results: dict
+	:param redirects: The redirects provided by Wikipedia.
+					  This dictionary has keys 'from' and 'to'.
+	:type redirects: dict
+	:param keep_redirects: A boolean indicating whether the redirected pages should be retained.
+	:type keep_redirects: bool
+
+	:return: A new dictionary with redirections.
+	:rtype: dict
+	"""
+
+	pages = dict(results)
+
+	"""
+	Recreate the redirection representation.
+	The redirects are represented as to-from instead of from-to.
+	"""
+	targets = { redirect["to"]: redirect["from"] for redirect in redirects }
+
+	for page in results:
+		"""
+		If a page was redirected, 'redirect' it back.
+		"""
+		if page in targets:
+			pages[targets[page]] = pages[page]
+
+			"""
+			If the page was only created because of a redirection, remove it.
+			"""
+			if not keep_redirects:
+				del pages[page]
+
+	"""
+	In some cases, two pages may redirect to the same page.
+	For example, `Striker (association football)` and `Inside forward` both point to `Forward (association football)`.
+	Therefore if a redirection (the `from`) has no page, create a page for it.
+
+	Note that when a query is split, the result could be empty for this missing page might not have been loaded yet.
+	This happens when there are more than `x` results and Wikipedia returns only the first `x`.
+	The redirection for a page that hasn't been loaded yet is given, but the content isn't there yet.
+	Therefore this has to be checked in advance.
+	"""
+	for page in redirects:
+		if page["from"] not in pages and page["to"] in results:
+			pages[page["from"]] = results[page["to"]]
+
+	return pages
---
+++ eventdt/wikinterface/tests/test_module.py
@@ -0,0 +1,68 @@
+"""
+Run unit tests on the :module:`wikinterface` module.
+"""
+
+import os
+import sys
+import unittest
+
+path = os.path.join(os.path.dirname(__file__), '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from wikinterface import *
+
+class TestWikinterface(unittest.TestCase):
+	"""
+	Test the :module:`wikinterface` module.
+	"""
+
+	def test_revert_redirects(self):
+		"""
+		Test that when reverting redirections, the resolved page is not included.
+		"""
+
+		"""
+		Create the test data.
+		"""
+		redirects = [
+			{ 'from': 'Striker (association football)' , 'to': 'Forward (association football)' },
+			{ 'from': 'Inside forward' , 'to': 'Forward (association football)' },
+		]
+		results = {
+			'Forward (association football)': ''
+		}
+
+		"""
+		Revert the redirections.
+		"""
+
+		pages = revert_redirects(results, redirects)
+		self.assertTrue('Inside forward' in pages)
+		self.assertTrue('Striker (association football)' in pages)
+		self.assertFalse('Forward (association football)' in pages)
+
+	def test_keep_redirects(self):
+		"""
+		Test that when keeping redirections, the redirected pages are retained.
+		"""
+
+		"""
+		Create the test data.
+		"""
+		redirects = [
+			{ 'from': 'Striker (association football)' , 'to': 'Forward (association football)' },
+			{ 'from': 'Inside forward' , 'to': 'Forward (association football)' },
+		]
+		results = {
+			'Forward (association football)': ''
+		}
+
+		"""
+		Revert the redirections.
+		"""
+
+		pages = revert_redirects(results, redirects, keep_redirects=True)
+		self.assertTrue('Inside forward' in pages)
+		self.assertTrue('Striker (association football)' in pages)
+		self.assertTrue('Forward (association football)' in pages)
