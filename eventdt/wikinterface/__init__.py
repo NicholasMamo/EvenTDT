369de81dadf9d64d9dbdd619139bd2bb77c7f954
NicholasMamo <nicholasmamo@gmail.com>
2020-02-17 14:48:57 +0100

    Create a function to check whether the API response is an error

--- eventdt/wikinterface/__init__.py
+++ eventdt/wikinterface/__init__.py
@@ -5,3 +5,16 @@ This module makes it easier to access the API.
 
 API_ENDPOINT = "https://en.wikipedia.org/w/api.php?"
 
+def is_error_response(response):
+	"""
+	Validate whether the given Wikipedia response is an error.
+
+	:param response: The response to validate.
+	:type response: dict
+
+	:return: A boolean indicating whether the response is valid.
+	:rtype: bool
+	"""
+
+	return 'error' in response
+
