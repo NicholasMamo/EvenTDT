e455f02f0cd618c45f4399294c70d658c8ea64f7
NicholasMamo <nicholasmamo@gmail.com>
2020-02-17 17:22:50 +0100

    Refactor the text collection script

--- eventdt/wikinterface/text.py
+++ eventdt/wikinterface/text.py
@@ -1,5 +1,6 @@
 """
-The text collector is used to fetch data from Wikipedia.
+Wikipedia articles are predominantly made up of text.
+This module contains functions to help extract that text from articles.
 """
 
 import json
@@ -7,80 +8,71 @@ import math
 import re
 import urllib.request
 
-from .wikicollector import WikiCollector
+from . import *
 
-class TextCollector(WikiCollector):
+def collect(titles, introduction_only=False):
 	"""
-	The TextCollector class creates an interface to fetch information from Wikipedia.
-	It abstracts obscure parameters.
+	Get the plain text content of the pages having the given titles.
+
+	:param titles: The titles of the pages from where to get the plain text.
+	:type titles: list of str or str
+	:param introduction_only: A boolean indicating whether to get only the introduction's text.
+	:type introduction_only: bool
+
+	:return: A dictionary with page titles as keys and the text as values.
+	:rtype: dict
 	"""
 
-	def get_plain_text(self, page_titles, introduction_only=False):
-		"""
-		Get the plain text content of the pages with the given titles.
+	titles = titles if type(titles) is list else [ titles ]
 
-		:param page_titles: The titles of the pages from where to get the plain text.
-		:type page_titles: list
-		:param introduction_only: A boolean indicating whether to get only the introduction's text.
-		:type introduction_only: bool
+	text = { }
 
-		:return: A dict with page title-text pairs for keys and values respectively.
-		:rtype: dict
-		"""
+	stagger = 20 if introduction_only else 1
+	parameters = {
+		'format': 'json',
+		'action': 'query',
+		'prop': 'extracts',
+		'exlimit': stagger, # the number of text extracts to retrieve at a time
+		'explaintext': True,
+		'titles': urllib.parse.quote('|'.join(titles)),
+		'redirects': True, # allow redirects
+		'exintro': introduction_only, # get only the introduction
+		'excontinue': 0, # the page number from where to continue
+	}
 
-		# TODO: Surround requests with try-catch, and same for the other wikinterfaces
-
-		text = {}
-		stagger = 40
-		base_endpoint = "format=json&action=query&prop=extracts&exlimit=20&explaintext&titles=%s&redirects%s" % (urllib.parse.quote('|'.join(page_titles)), "&exintro" if introduction_only else "")
-
-		"""
-		In some cases, the GET arguments could become far too long.
-		Therefore in such cases, stagger the process.
-		"""
-		if len(page_titles) > stagger:
-			for i in range(0, math.ceil(len(page_titles) / stagger)):
-				subset = self.get_plain_text(page_titles[(i*stagger):((i+1)*stagger)], introduction_only=introduction_only)
-				text.update(subset)
-		else:
-			excontinue = 0
-			if len(page_titles) > 20:
-				while excontinue is not None:
-					endpoint = "%s&excontinue=%s" % (base_endpoint, excontinue)
-					response = urllib.request.urlopen(self.BASE_URL + endpoint)
-					response = json.loads(response.read().decode("utf-8"))
-
-					pages = response["query"]["pages"]
-					redirects = response["query"]["redirects"] if "redirects" in response["query"] else {}
-
-					for page in pages.values():
-						if "extract" in page:
-							text[page["title"]] = page["extract"]
-
-					"""
-					Put the original page titles as keys.
-					This is useful in case there were any redirects.
-					"""
-					text = self._resolve_redirects(text, redirects)
-
-					excontinue = response["continue"]["excontinue"] if "continue" in response else None
-			elif len(page_titles) > 0:
-				endpoint = base_endpoint
-
-				response = urllib.request.urlopen(self.BASE_URL + endpoint)
-				response = json.loads(response.read().decode("utf-8"))
-				
-				pages = response["query"]["pages"]
-				redirects = response["query"]["redirects"] if "redirects" in response["query"] else {}
-
-				for page in pages.values():
-					if "extract" in page:
-						text[page["title"]] = page["extract"]
-
-				"""
-				Put the original page titles as keys.
-				This is useful in case there were any redirects.
-				"""
-				text = self._resolve_redirects(text, redirects)
-
+	"""
+	When there are many page titles, the GET parameters could become far too long.
+	Therefore in such cases, stagger the process.
+	"""
+	if len(urllib.parse.quote('|'.join(titles))) > 1024:
+		for i in range(0, math.ceil(len(titles) / stagger)):
+			subset = self.get_plain_text(titles[(i * stagger):((i + 1) * stagger)],
+										 introduction_only=introduction_only)
+			text.update(subset)
 		return text
+
+	"""
+	If page titles are given, collect them.
+	Pages are returned 20 at a time.
+	When this happens, the response contains a continue marker.
+	The loop continues fetching requests until there are no such markers.
+	"""
+	if len(titles):
+		while parameters['excontinue'] is not None:
+			endpoint = construct_url(parameters)
+			response = urllib.request.urlopen(endpoint)
+			response = json.loads(response.read().decode("utf-8"))
+
+			"""
+			Extract the pages, redirects and the text from the
+			Put the original page titles as keys, reverting any redirects.
+			"""
+			pages = response['query']['pages']
+			redirects = response['query']['redirects'] if 'redirects' in response['query'] else {}
+			pages = { page['title']: page.get('extract') for page in pages.values() if 'extract' in page }
+			pages = revert_redirects(pages, redirects)
+			text.update(pages)
+
+			parameters['excontinue'] = response['continue']['excontinue'] if 'continue' in response else None
+
+	return text
