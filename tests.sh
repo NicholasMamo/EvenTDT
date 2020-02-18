4d9d8e878d160fb7049f2fa6db218834ad85a12b
NicholasMamo <nicholasmamo@gmail.com>
2020-02-18 19:20:31 +0100

    Add the Wikipedia links tests to the general tests script

--- tests.sh
+++ tests.sh
@@ -131,6 +131,7 @@ wikipedia_tests() {
 	echo -e "${HIGHLIGHT}Wikipedia${DEFAULT}"
 
 	python3 -m unittest eventdt.wikinterface.tests.test_info
+	python3 -m unittest eventdt.wikinterface.tests.test_links
 	python3 -m unittest eventdt.wikinterface.tests.test_package
 	python3 -m unittest eventdt.wikinterface.tests.test_search
 	python3 -m unittest eventdt.wikinterface.tests.test_text
