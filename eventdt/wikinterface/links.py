f8ca8f90d8cd2ca755de06c888c7256c91891793
NicholasMamo <nicholasmamo@gmail.com>
2020-02-18 15:55:06 +0100

    Refactor the links module

--- eventdt/wikinterface/links.py
+++ eventdt/wikinterface/links.py
@@ -1,5 +1,5 @@
 """
-The link collector is used to collect links from Wikipedia pages.
+The link collector collects links from Wikipedia articles.
 """
 
 import json
@@ -7,185 +7,237 @@ import math
 import re
 import urllib.request
 
-from .wikicollector import WikiCollector
+from . import *
 
-class LinkCollector(WikiCollector):
+def collect(titles, separate=True, encoding=None, introduction_only=False):
 	"""
-	The LinkCollector class is responsible for collecting links from Wikipedia articles.
+	Get a list of outgoing links from the given list of articles.
 
-	:cvar REFERENCE_PATTERN: The regex pattern to look for references.
-	:vartype REFERENCE_PATTERN: :class:`re.RegexObject`
-	:cvar HTML_COMMENTS_PATTERN: The regex pattern to look for HTML comments.
-	:vartype HTML_COMMENTS_PATTERN: :class:`re.RegexObject`
-	:cvar LINK_PATTERN: The regex pattern to look for internal links.
-	:vartype LINK_PATTERN: :class:`re.RegexObject`
+	:param titles: A list of article titles, or a single article title, from where to fetch links.
+	:type titles: list of str or str
+	:param separate: A boolean indicating whether the links should be separated according to articles.
+	:type separate: bool
+	:param encoding: The encoding to use for links.
+	:type encoding: None or str
+	:param introduction_only: A boolean indicating whether the links should be fetched only from the introduction.
+	:type introduction_only: bool
+
+	:return: A list of outgoing links from the article titles provided.
+			 If the `separate` boolean is `True`, a dictionary is returned.
+			 This dictionary's keys are the article titles, and the values are lists of outgoing links.
+			 Otherwise, a list of article titles is returned.
+	:rtype: list or dict
+
+	:raises RuntimeError: when the request returns an error response.
 	"""
 
-	REFERENCE_PATTERN = re.compile("<ref(.|\\n)*?((\/>)|(>(.|\\n)*?<\/ref>))")
-	HTML_COMMENTS_PATTERN = re.compile("<!--(.|\n)+?-->")
-	LINK_PATTERN = re.compile("\[\[(?!File|#)(.*?)(\|.*?)?\]\]")
+	links = { } if separate else [ ]
 
-	def get_recursive_links(self, seed, max_level, separate=True, encoding=None, collected_links=None, first_section_only=False):
-		"""
-		Keep fetching links, starting from the seed set.
-		Important - this is a very expensive operation as it grows exponentially.
-		If the first_section_only flag is True, links are only fetched from the first section.
+	titles = titles if type(titles) is list else [ titles ]
 
-		:param seed: The seed set, representing the pages from where to start (or continue) looking.
-		:type seed: list
-		:param max_level: The maximum level to traverse.
-		:type max_level: int
-		:param separate: A boolean indicating whether the links should be separated according to the source.
-			The alternative is to get a set of all the encountered links.
-		:type separate: bool
-		:param encoding: The encoding to use.
-		:type encoding: str
-		:param collected_links: Any links that have already been collected.
-		:type collected_links: list
-		:param first_section_only: A boolean indicating whether links should only be fetched from the first section.
-			The first section is usually the introduction.
-			This flag may be set to limit the number of links that are retrieved since they grow exponentially.
-		:type first_section_only: bool
+	stagger = 20
+	"""
+	Some parameters are required only when using the introduction to collect links.
+	Similarly, others are required only when all links are required.
+	"""
+	parameters = {
+		'format': 'json',
+		'action': 'query',
+		'prop': 'revisions' if introduction_only else 'links',
+		'redirects': True,
+		'rvprop': 'content' if introduction_only else False,
+		'rvsection': 0 if introduction_only else False,
+		'explaintext': introduction_only,
+		'pllimit': 500 if not introduction_only else False,
+		'titles': urllib.parse.quote('|'.join(titles)),
+		'plcontinue': 0,
+	}
 
-		:return: A list of links.
-		:rtype: dict if the separate flag is True, list otherwise
-		"""
+	"""
+	When there are many page titles, the GET parameters could become far too long.
+	Therefore in such cases, stagger the process.
+	Pages are always fetched separately, and then merged later if need be.
+	"""
+	if len(urllib.parse.quote('|'.join(titles))) > 1024:
+		for i in range(0, math.ceil(len(titles)/float(stagger))):
+			new_links = collect(titles[(i * stagger):((i + 1) * stagger)],
+								separate=True, encoding=encoding,
+								introduction_only=introduction_only)
+			for title, link_set in new_links.items():
+				links[title] = links.get(title, []) + link_set
 
-		"""
-		In the base case, get a list of links again, and stop looking
-		In the recursive case, fetch the links, and use these same links to look for outgoing links from them
-		"""
+	"""
+	If page titles are given, collect their links.
+	Pages are returned 20 at a time.
+	When this happens, the response contains a continue marker.
+	The loop continues fetching requests until there are no such markers.
+	"""
+	if len(titles):
+		while parameters['plcontinue'] is not None:
+			endpoint = construct_url(parameters)
+			response = urllib.request.urlopen(endpoint)
+			response = json.loads(response.read().decode("utf-8"))
 
-		collected_links = list() if collected_links is None else collected_links
+			if is_error_response(response):
+				raise RuntimeError(response)
 
-		seed = list(set(seed).difference(set(collected_links))) # remove duplicates and links that were already collected
-		if max_level <= 1:
-			return self.get_links(list(seed), separate=separate, encoding=encoding, first_section_only=first_section_only)
-		else:
-			links = self.get_links(list(seed), separate=separate, encoding=encoding, first_section_only=first_section_only) # get the links from this level
-			if separate:
-				flattened_links  = [link for link_set in links.values() for link in link_set]
-				next_links = next_links = self.get_recursive_links(flattened_links,
-																	max_level=(max_level - 1),
-																	separate=separate,
-																	encoding=encoding,
-																	collected_links=list(set(seed + collected_links)),
-																	first_section_only=first_section_only) # go a level deeper
-				links.update(next_links)
-				return { page_title: list(set(links[page_title])) for page_title in links }
-			else:
-				next_links = self.get_recursive_links(links,
-														max_level=(max_level - 1),
-														separate=separate, encoding=encoding,
-														collected_links=list(set(seed + collected_links)),
-														first_section_only=first_section_only)
-				return list(set(links + next_links))
-
-	def get_links(self, page_titles, separate=True, encoding=None, first_section_only=False):
-		"""
-		Get a list of outgoing links from the given list of page title
-		The returned links may be separated according to the pages
-		"""
-
-		if len(page_titles) == 0:
-			return {}
-
-		"""
-		Since there is a 2048-character limit on GET requests, stagger the searches.
-		"""
-		break_point = 50
-		if len(page_titles) > break_point:
-			links = { page_title: [] for page_title in page_titles }
-			for i in range(0, math.ceil(len(page_titles)/float(break_point))):
-				"""
-				Pages are always fetched separately, and then merged later if need be.
-				"""
-				new_links = self.get_links(page_titles[(i * break_point):((i + 1) * break_point)], separate=True, encoding=encoding, first_section_only=first_section_only)
-				for page_title, link_set in new_links.items():
-					links[page_title] = links.get(page_title, []) + link_set
-
-			if separate:
-				return { page_title: list(set(links[page_title])) for page_title in links }
-			else:
-				return list(set([ title for page_title in page_titles for title in links[page_title] ])) # if need be, flatten the list
-		else:
-			page_titles = [ page_title.decode("utf-8") if type(page_title) == bytes else page_title for page_title in page_titles ]
-			links = { page_title: [] for page_title in page_titles }
-
-			if first_section_only:
-				base_endpoint = "format=json&action=query&prop=revisions&rvprop=content&rvsection=0&explaintext=&titles=%s&redirects" % (urllib.parse.quote('|'.join(page_titles)))
-			else:
-				base_endpoint = "action=query&titles=%s&prop=links&pllimit=500&format=json&redirects" % (urllib.parse.quote('|'.join(page_titles)))
+			pages = response["query"]["pages"]
+			redirects = response["query"]["redirects"] if "redirects" in response["query"] else {}
 
 			"""
-			The collector keeps looking for links until the batch is complete.
+			Extract the links from each article.
+			Depending on whether the introduction is being used or the entire article, extract them differently.
+			Then, revert any redirections.
 			"""
-			endpoint = base_endpoint
-			while True:
-				response = urllib.request.urlopen(self.BASE_URL + endpoint)
-				response = json.loads(response.read().decode("utf-8"))
+			for page in pages:
+				internal_links = _get_intro_links(pages[page], encoding=encoding) if introduction_only else _get_all_links(pages[page], encoding=encoding)
+				links[pages[page]["title"]] = links.get(pages[page]["title"], []) + internal_links
 
-				if "query" in response:
-					pages = response["query"]["pages"]
-					redirects = response["query"]["redirects"] if "redirects" in response["query"] else {}
+			links = revert_redirects(links, redirects)
 
-					"""
-					Go through each page, fetching its content
-					"""
-					for page in pages:
-						title = pages[page]["title"]
-						if (first_section_only):
-							internal_links = self.get_intro_links(pages[page], encoding=encoding)
-						else:
-							internal_links = self.get_all_links(pages[page], encoding=encoding)
-						links[title] = links.get(title, []) + internal_links
-
-					"""
-					Put the original page titles as keys.
-					This is useful in case there were any redirects.
-					"""
-					links = self._resolve_redirects(links, redirects)
-
-					"""
-					If there are no more links to fetch, stop iterating
-					If the limit was reached, the collector must have stopped midway
-					Therefore, the endpoint is updated to continue looking from where it left off
-					"""
-					if "batchcomplete" in response:
-						break
-					elif "continue" in response:
-						endpoint = base_endpoint + "&plcontinue=%s" % urllib.parse.quote(response["continue"]["plcontinue"])
-
-			if separate:
-				return { page_title: list(set(links[page_title])) for page_title in links }
+			"""
+			If there are no more links to fetch, stop iterating
+			If the limit was reached, the collector must have stopped midway
+			Therefore, the endpoint is updated to continue looking from where it left off
+			"""
+			if "continue" in response:
+				parameters['plcontinue'] = response['continue']['plcontinue']
 			else:
-				return list(set([ title for page_title in links for title in links[page_title] ])) # if need be, flatten the list
+				break
 
-	def get_intro_links(self, page, encoding=None):
-		"""
-		Get a list of outgoing links from the given list of page titles, but only from the first section, usually the introduction
-		The returned links may be separated according to the pages
-		"""
+	"""
+	If the links need to be separate, get the unique links for each page.
+	Otherwise, flatten the links and get a unique set.
+	"""
+	if separate:
+		return { page: list(set(links[page])) for page in links }
+	else:
+		return list(set([ link for link in link_set for link_set in links.values() ]))
 
-		if "revisions" in page:
-			latest_revision = page["revisions"][0]
-			content = latest_revision["*"]
-			content = self.REFERENCE_PATTERN.sub("", content) # remove references
-			content = self.HTML_COMMENTS_PATTERN.sub("", content) # remove HTML comments
-			internal_links = self.LINK_PATTERN.findall(content) # fetch all internal links
-			internal_links = [ internal_link[0] for internal_link in internal_links ] # isolate the internal links, ignoring the labels
-			return internal_links
-		else:
-			return []
+def collect_recursive(titles, level, collected_links=None, separate=True, *args, **kwargs):
+	"""
+	Recursively fetch links, starting from a seed set.
+	The function uses the :func:`wikinterface.links.collect` function.
+	Any additional arguments and keyword arguments are passed on to it.
 
-	def get_all_links(self, page, encoding=None):
-		"""
-		Get a list of outgoing links from the given list of page titles
-		The returned links may be separated according to the pages
-		"""
+	.. warning::
 
-		if "links" in page:
-			internal_links = [link["title"].encode(encoding) if encoding else link["title"] for link in page["links"]]
-			return internal_links
-		else:
-			return []
+		This is a very expensive operation as it grows exponentially.
+
+	:param titles: The articles from where to start (or continue) looking.
+	:type titles: list of str or str
+	:param level: The current level.
+				 If the level is 1, the links are fetched once and the function returns.
+	:type level: int
+	:param collected_links: Any links that have already been collected.
+	:type collected_links: list of str or None
+	:param separate: A boolean indicating whether the links should be separated according to articles.
+	:type separate: bool
+
+	:return: A list of links if they should not be separated.
+			 If they are, the links are returned separated by the page in which they appear.
+	:rtype: dict or list
+
+	:raises ValueError: if the level is not positive.
+	:raises ValueError: if the level is not an integer.
+	"""
+
+	if level <= 0:
+		raise ValueError(f"The level number be a positive integer; received {level}")
+
+	if type(level) is not int:
+		raise ValueError(f"The level must be an integer; received {level}")
+
+	collected_links = collected_links or [ ]
+
+	"""
+	In the base case, collect links from the seed set.
+	In the recursive case, fetch the links, and look for outgoing links in these results.
+	Pages for whom links have already been collected are not collected anew.
+	"""
+	seed = list(set(seed).difference(set(collected_links)))
+	links = collect(list(seed), separate=separate, *args, **kwargs)
+	if level <= 1:
+		return links
+
+	next_links = collect_recursive(list(links.keys()), level=(level - 1),
+								   collected_links=list(set(seed + collected_links)),
+								   separate=separate, *args, **kwargs)
+
+	if separate:
+		links.update(next_links)
+		return { page_title: list(set(links[page])) for page in links }
+	else:
+		return list(set(links + next_links))
+
+def _get_intro_links(page, encoding=None):
+	"""
+	Get a list of outgoing links from the introduction of the given page.
+
+	:param page: The page from where to get the introduction.
+				 The function expects a `revisions` key containing the text of the latest revision.
+	:type page: dict
+	:param encoding: The encoding to use for links.
+	:type encoding: None or str
+
+	:return: A list of outgoing links found in the given text.
+	:rtype: list of str
+
+	:raise ValueError: when no revisions are found in the page.
+	:raise ValueError: when no text is found in the revision.
+	"""
+
+	links = [ ]
+
+	"""
+	Create the regular expressions that are used to clean the text and extract links.
+	"""
+	reference_pattern = re.compile("<ref(.|\\n)*?((\/>)|(>(.|\\n)*?<\/ref>))")
+	html_comments_pattern = re.compile("<!--(.|\n)+?-->")
+	link_pattern = re.compile("\[\[(?!File|#)(.*?)(\|.*?)?\]\]")
+
+	"""
+	Get the text from the latest revision.
+	"""
+	revisions = page.get('revisions', [])
+	if not revisions:
+		raise ValueError("No revisions found")
+
+	text = revisions[0].get('*', None)
+	if not text:
+		raise ValueError("No text found in the revision")
+
+	"""
+	Remove all references and HTML comments from the text.
+	Then, extract the actual links, removing the labels.
+	"""
+	text = reference_pattern.sub("", text)
+	text = html_comments_pattern.sub("", text)
+	links = link_pattern.findall(text)
+	links = [ link[0] for link in links ]
+	return links
+
+def _get_all_links(page, encoding=None):
+	"""
+	Get a list of outgoing links from the given page.
+
+	:param page: The page from where to get the text.
+				 The function expects a `links` key containing the list of links in the article
+	:type page: dict
+	:param encoding: The encoding to use for links.
+	:type encoding: None or str
+
+	:return: A list of outgoing links found in the given text.
+	:rtype: list of str
+
+	:raise ValueError: when no links are found in the page.
+	"""
+
+	links = [ ]
+
+	if 'links' not in page:
+		raise ValueError("No links found in the page")
+
+	links = [ link["title"] for link in page["links"] ]
+	links = [ link.encode(encoding) if encoding else link for link in links ]
+	return links
