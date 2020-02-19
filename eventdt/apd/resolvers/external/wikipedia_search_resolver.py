6a8f8eb078f392203364633cbabe216766b03420
NicholasMamo <nicholasmamo@gmail.com>
2020-02-19 16:41:57 +0100

    Add missing class documentation

--- eventdt/apd/resolvers/external/wikipedia_search_resolver.py
+++ eventdt/apd/resolvers/external/wikipedia_search_resolver.py
@@ -30,6 +30,18 @@ class WikipediaSearchResolver(Resolver):
 	The Wikipedia search resolver looks for pages that include candidate names.
 	The Wikipedia API automatically ranks articles by relevance.
 	This resolver exploits that to try and match the candidate with any of the top results.
+
+	:ivar scheme: The term-weighting scheme to use to create documents from Wikipedia pages.
+				  These documents are used to compare the similarity with the domain of the candidates.
+	:vartype scheme: :class:`nlp.term_weighting.scheme.TermWeightingScheme`
+	:ivar threshold: The threshold below which candidates become unresolved.
+	:vartype threshold: float.
+	:ivar tokenizer: The tokenizer to use to create documents.
+	:vartype tokenizer: :class:`nlp.tokenizer.Tokenizer`
+	:ivar threshold: The similarity threshold beyond which candidate participants are resolved.
+	:vartype threshold: float
+	:ivar corpus: The corpus of documents.
+	:vartype corpus: list of :class:`nlp.document.Document`
 	"""
 
 	def __init__(self, scheme, tokenizer, threshold, corpus):
