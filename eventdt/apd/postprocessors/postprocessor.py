eca415886964e7056d5fe3d4470525b287a0687b
NicholasMamo <nicholasmamo@gmail.com>
2020-02-22 19:26:27 +0100

    Refactor the postprocessor

--- eventdt/apd/postprocessors/postprocessor.py
+++ eventdt/apd/postprocessors/postprocessor.py
@@ -1,58 +1,25 @@
 """
-A postprocessor takes in participants and does something with them to make them ready for use.
+The postprocessor is the sixth and last step in the APD process.
+A postprocessor formats the participants.
+
+The postprocessor returns the same number of participants with any changes to them.
+The functionality revolves around one method: the :meth:`apd.postprocessors.postprocessor.Postprocessor.postprocess` method.
 """
-
-import re
-
-import unicodedata
 
 class Postprocessor(object):
 	"""
 	The simplest postprocessor returns the participants without any changes.
 	"""
 
-	def postprocess(self, participants, corpus, token_attribute="tokens", *args, **kwargs):
+	def postprocess(self, participants, *args, **kwargs):
 		"""
-		Postprocess the given participants.
-		This postprocessor changes nothing.
+		Return the same participants as those received.
 
 		:param participants: The participants to postprocess.
-		:type participants: list
-		:param corpus: The corpus of documents.
-		:type corpus: list
-		:param token_attribute: The attribute that contains the tokens.
-		:type token_attribute: str
+		:type participants: list of str
 
 		:return: The postprocessed participants.
-		:rtype: list
+		:rtype: list of str
 		"""
 
 		return participants
-
-	def remove_accents(self, participants):
-		"""
-		Remove the accents from the participants.
-
-		:param participants: The participants whose accents will be removed.
-		:type participants: list
-
-		:return: The participants without any accents.
-		:rtype: list
-		"""
-
-		return [ ''.join((c for c in unicodedata.normalize('NFD', participant) if unicodedata.category(c) != 'Mn')) for participant in participants ]
-
-	def remove_disambiguation(self, participants):
-		"""
-		Remove the accents from the participants.
-
-		:param participants: The participants which will be cleaned.
-		:type participants: list
-
-		:return: The participants without any disambiguation text.
-		:rtype: list
-		"""
-
-		disambiguation_pattern = re.compile(" \(.*?\)$") # a pattern of information that ends a title with disambiguation information, or more information about the concept
-
-		return [ disambiguation_pattern.sub('', participant.strip()) for participant in participants ]
