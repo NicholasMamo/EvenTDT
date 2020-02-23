ff00d2372b1ade42679f8144356c2013f1b375b7
NicholasMamo <nicholasmamo@gmail.com>
2020-02-23 19:35:33 +0100

    Refactor the clustering algorithm

--- eventdt/vsm/clustering/algorithms/clustering.py
+++ eventdt/vsm/clustering/algorithms/clustering.py
@@ -1,58 +1,49 @@
 """
-A class for clustering algorithms.
-The class is used to maintain state.
+Clusters are normally the product of clustering algorithms
+A clustering algorithm takes in vectors or documents and clusters them.
+
+Clustering algorithms are represented as classes so that they maintain their state.
+Each class stores, at least, a list of :class:`vsm.clustering.cluster.Cluster` instances.
+New documents can be clustered by calling the :func:`vsm.clustering.algorithms.clustering.ClusteringAlgorithm.cluster` method.
 """
 
 from abc import ABC, abstractmethod
 
-from ...vector import Vector
-from ...vector_math import *
-from ..cluster import Cluster
+import os
+import sys
+
+path = os.path.join(os.path.dirname(__file__), '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
 
 class ClusteringAlgorithm(ABC):
 	"""
-	The ClusteringAlgorithm class retains a state.
+	Clustering algorithms maintain a state.
+	The state is, at least, a list of :class:`vsm.clustering.cluster.Cluster` instances.
 
-	:ivar _similarity_measure: The similarity function to use to compare the likeliness of the vector with the cluster.
-	:vartype _similarity_measure: function
-	:ivar _clusters: A list of clusters.
-	:vartype _clusters: list of :class:`vector.cluster.cluster.Cluster` instances
+	:ivar clusters: A list of clusters.
+	:vartype clusters: list of :class:`vsm.clustering.cluster.Cluster`
 	"""
 
 	@abstractmethod
-	def __init__(self, similarity_measure=cosine):
+	def __init__(self):
 		"""
-		The constructor is used to initialize the state.
-
-		:param similarity_measure: The similarity function to use to compare the likeliness of the vector with the cluster.
-		:type similarity_measure: function
+		Initialize the state of the clustering algorithm with an empty list of clusters.
 		"""
 
-		self._similarity_measure = similarity_measure
-		self._clusters = []
-
-		pass
+		self.clusters = [ ]
 
 	@abstractmethod
-	def cluster(self, vectors):
+	def cluster(self, vectors, *args, **kwargs):
 		"""
 		Cluster the given vectors.
+		The function returns the active clusters.
 
-		:param vectors: The list of vectors to cluster.
-		:type vectors: list of :class:`vector.vector.Vector` instances
+		:param vectors: The vectors to cluster.
+		:type vectors: list of :class:`vsm.vector.Vector`
 
-		:return: The clusters that received documents.
-		:rtpye: list of :class:`vector.cluster.cluster.Cluster` instances
+		:return: The clusters in the algorithm state.
+		:rtpye: list of :class:`vsm.clustering.cluster.Cluster`
 		"""
 
 		pass
-
-	def get_clusters(self):
-		"""
-		Get the list of clusters.
-
-		:return: The stored list of clusters.
-		:rtype: list of :class:`vector.cluster.cluster.Cluster` instances
-		"""
-
-		return self._clusters
