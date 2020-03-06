13de4a78dbea5a097d13daa3624ad8baed98b11a
NicholasMamo <nicholasmamo@gmail.com>
2020-03-06 20:33:35 +0100

    Refactor Cataldi et al.'s algorithm to use the new class

--- eventdt/tdt/algorithms/cataldi.py
+++ eventdt/tdt/algorithms/cataldi.py
@@ -1,96 +1,50 @@
 """
-Implementation of the algorithm by Cataldi et al. (2014).
+Cataldi et al.'s algorithm was among the first to introduce the notion of nutrition and burst.
+The feature-pivot TDT algorithm first calculates the worth of terms as nutrition.
+Then, periodically, it calculates the burst of these terms.
+Burst is a measure of how much a term is bursting.
+
+The calculations for burst are based on time windows.
+Therefore calls to :meth:`~tdt.algorithms.cataldi.Cataldi.draw` should give the timestamp of the time window with respect to which the burst is to be calculated.
+
+This implementation calculates the burst of terms based on the given nutrition.
+
+.. note::
+
+	Implementation based on the algorithm presented in `Personalized Emerging Topic Detection Based on a Term Aging Model by Cataldi et al. (2014) <https://dl.acm.org/doi/abs/10.1145/2542182.2542189>`_.
 """
 
 import math
+import time
 
-def detect_topics(nutrition_store, # the store contraining historical data
-	timestamp, # the timestamp of the time window being considered
-	sets=None, # the number of sets to consider
-	min_nutrition=0, # consider only nutrition having a minimal value
-	max_breaking=10, # the maximum number of breaking terms
-	min_breaking=0 # the minimum number of breaking terms
-	):
+from .tdt import TDTAlgorithm
+
+class Cataldi(TDTAlgorithm):
 	"""
-	Detect topics using historical data from the given NutritionStore.
+	Cataldi et al.'s algorithm is a feature-pivot TDT approach to detect topics.
+	The algorithm uses nutrition to calculate burst.
+	Burst is a measure of how much a term is bursting.
 
-	:param nutrition_store: The store contraining historical data.
-	:type NutritionStore: :class:`~topic_detection.nutrition_store.nutrition_store.NutritionStore`
-	:param timestamp: The current time window's timestamp (in seconds).
-	:type timestamp: int
-	:param sets: The number of time windows to consider.
-	:type sets: int
-	:param min_nutrition: The minimum nutrition of a term to be considered.
-	:type min_nutrition: float
-	:param max_breaking: The maximum number of breaking terms to return.
-	:type max_breaking: int
-	:param min_breaking: The minimum number of breaking terms to return.
-	:type min_breaking: int
-
-	:return: A list of breaking terms in the considered time window.
-	:rtype: list
+	:ivar store: The store contraining historical nutrition data.
+				 The algorithm expects the nutrition values to represent term nutrition.
+				 Therefore the values should be dictionaries with terms as keys, and nutrition as the respective values.
+				 Furthermore, the algorithm expects time windows.
+				 In other words, the timestamps should represent an entire time window, not just a particular second.
+	:vartype store: :class:`~tdt.nutrition.store.NutritionStore`
 	"""
 
-	term_nutrition = nutrition_store.get_nutrition_set(timestamp) # fetch the nutrition
-	historical_data = nutrition_store.get_recent_nutrition_sets(sets, timestamp=(timestamp - 1)) # get the historical data
 
-	term_nutrition = { term: value for term, value in term_nutrition.items() if value >= min_nutrition } # remove seldomly-used terms
+	def detect(self, timestamp, since=None):
+		"""
+		Detect topics using historical data from the given nutrition store.
 
-	burstiness = { term: _get_burstiness(term, term_nutrition[term], historical_data) for term in term_nutrition } # calculate the burstiness for each term
-	burstiness = sorted(burstiness.items(), key=lambda x: x[1])[::-1] # order the burstiness values in descending order based on the burstiness values
+		:param timestamp: The timestamp at which to try to identify emerging topics.
+						  This value is exclusive.
+		:type timestamp: float
+		:param since: The timestamp since when nutrition should be considered.
+					  If it is not given, all of the nutrition that is available is used.
+		:type since: float or None
 
-	burstiness_values = [ value for term, value in burstiness ] # isolate the burstiness values
-	burstiness_drops = [burstiness_values[i - 1] - burstiness_values[i] for i in range(1, len(burstiness_values))]  # find the drops in burstiness values
-
-	"""
-	Find the critical drop index
-	"""
-	if len(burstiness_drops) > 0:
-		critical_drop_index = _get_critical_drop_index(burstiness_drops)
-		critical_drop_index = max(min(critical_drop_index, max_breaking), min_breaking)
-	else:
-		critical_drop_index = 0
-
-	return [ term for term, _ in burstiness[:critical_drop_index] ]
-
-def _get_burstiness(term, nutrition, historical_data):
-	"""
-	Calculate the burstiness for the given term using the historical data.
-
-	:param term: The term whose burstiness is being calculated.
-	:type term: str
-	:param nutrition: The term's nutrition in the current time window.
-	:type nutrition: float
-	:param historical_data: The historical data to consider.
-	:type historical_data: dict
-
-	:return: The term's burstiness.
-	:rtype: float
-	"""
-	windows = len(historical_data)
-	return sum([(nutrition ** 2 - historical_data[i].get(term, 0) ** 2) / math.log(i + 2, 10) for i in range(0, windows)])
-
-def _get_critical_drop_index(burstiness_drops):
-	"""
-	Find the critical drop index.
-
-	:param burstiness_drops: A list of burstiness drops.
-	:type burstiness_drops: list
-
-	:return: The index where the critical drop appears.
-	:rtype: int
-	"""
-
-	"""
-	First find the maximum and average drops
-	"""
-	maximum_drop = max(burstiness_drops)
-	maximum_drop_index = burstiness_drops.index(maximum_drop)
-	average_drop = sum(burstiness_drops[0:maximum_drop_index])/maximum_drop_index if maximum_drop_index > 0 else 0 # get the average drop
-
-	"""
-	Then identify the critical index
-	"""
-	for i in range(1, len(burstiness_drops)):
-		if (burstiness_drops[i - 1] >= average_drop):
-			return i
+		:return: A list of breaking terms in the considered time window in descending order of their burst.
+		:rtype: list of str
+		"""
