49bf4c1da0b556f53d30f8e01109c8b4b736016f
NicholasMamo <nicholasmamo@gmail.com>
2020-03-07 19:56:47 +0100

    Refactor the exportable

--- eventdt/objects/exportable.py
+++ eventdt/objects/exportable.py
@@ -1,21 +1,20 @@
 """
-An exportable object can be exported as a JSON string and loaded back.
+Exportable objects are normal objects that can be exported as a JSON string and loaded back.
 """
 
 from abc import ABC, abstractmethod
 
 class Exportable(ABC):
 	"""
-	An abstract class of an object that can be exported as a JSON string, and imported back.
-	The representation returns an associative array.
+	An abstract class of an object that can be exported as a JSON string and imported back.
 	"""
 
 	@abstractmethod
 	def to_array(self):
 		"""
-		Export the object as an associative array.
+		Export the object as a dictionary.
 
-		:return: The object as an associative array.
+		:return: The object as a dictionary.
 		:rtype: dict
 		"""
 
@@ -25,9 +24,9 @@ class Exportable(ABC):
 	@abstractmethod
 	def from_array(array):
 		"""
-		Create an instance of the object from the given associative array.
+		Create an instance of the object from the given dictionary.
 
-		:param array: The associative array with the attributes to create the object instance.
+		:param array: The dictionary with the attributes to create the object instance.
 		:type array: dict
 
 		:return: A new instance of an object with the same attributes stored in the object.
