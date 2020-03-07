1a6b07620f1ae7bb63a2e2cda785a5117f9108e7
NicholasMamo <nicholasmamo@gmail.com>
2020-03-07 18:04:44 +0100

    Refactor the summary class

--- eventdt/summarization/summary.py
+++ eventdt/summarization/summary.py
@@ -6,189 +6,76 @@ import importlib
 import os
 import sys
 
-path = os.path.dirname(__file__)
-path = os.path.join(path, '../')
+path = os.path.join(os.path.dirname(__file__), '..')
 if path not in sys.path:
 	sys.path.insert(1, path)
 
-from logger import logger
+from objects.attributable import Attributable
 
-from objects.exportable import Exportable
-
-from vector.nlp.cleaners import cleaner
-
-class Summary(Exportable):
+class Summary(Attributable):
 	"""
 	A summary is made up of various attributes.
 	It is made up of the actual list of :class:`~vector.nlp.document.Document` of the summary.
 	Associated with the summary are also two timestamps - the timestamps when the summary was creation and last updated.
 
-	:ivar _documents: The documents making up the summary.
-	:vartype _documents: list of :class:`~vector.nlp.document.Document` instances.
-	:ivar _created: The timestamp when the summary was created.
-	:vartype _created: int
-	:ivar _last_updated: The timestamp when the summary was last updated.
-	:vartype _last_updated: int
+	:ivar documents: The documents that make up the summary.
+	:vartype documents: list of :class:`~nlp.document.Document`
 	"""
 
-	def __init__(self, documents=None, timestamp=None):
+	def __init__(self, documents=None, *args, **kwargs):
 		"""
 		Create the summary.
 
 		:param documents: A list of documents that make up the summary.
-		:type documents: list of :class:`~vector.nlp.document.Document` instances
-		:param timestamp: The timestamp when the summary was created.
-		:type timestamp: int
+		:type documents: list of :class:`~nlp.document.Document`
 		"""
 
-		self._documents = documents if documents is not None else []
-		self._created = timestamp if timestamp is not None else 0
-		self._last_updated = self._created
+		super(Summary, self).__init__(*args, **kwargs)
+		self.documents = documents or [ ]
 
-	def add_document(self, document):
+	def __repr__(self):
 		"""
-		Add the given document to the summary.
+		Get the string representation of the summary.
+		This is equivalent to concatenating the text of all documents.
 
-		:param document: The new document.
-		:type document: :class:`~vector.nlp.document.Document`
-		"""
-
-		self._documents.append(document)
-
-	def remove_document(self, document):
-		"""
-		Remove the given document from the summary.
-
-		:param document: The document to remove from the summary.
-		:type document: :class:`~vector.nlp.document.Document`
-		"""
-
-		if document in self._documents:
-			self._documents.remove(document)
-
-	def set_documents(self, documents):
-		"""
-		Set the list of documents making up the summary.
-
-		:param documents: The list of documents that now make up the summary.
-		:type documents: list of :class:`~vector.nlp.document.Document` instances
-		"""
-
-		self._documents = documents
-
-	def get_documents(self):
-		"""
-		Get the list of documents in the summary.
-
-		:return: A list of documents that make up the summary.
-		:rtype: list of :class:`~vector.nlp.document.Document` instances
-		"""
-
-		return self._documents
-
-	def created_at(self):
-		"""
-		Get the time when the summary was created.
-
-		:return: The time when the summary was created as a timestamp.
-		:rtype: int
-		"""
-
-		return self._created
-
-	def set_created_at(self, timestamp):
-		"""
-		Set the timestamp when the summary was created.
-
-		:param timestamp: The timestamp when the summary was created.
-		:type timestamp: int
-		"""
-
-		self._created = timestamp
-
-	def set_last_updated(self, timestamp):
-		"""
-		Set the timestamp when the Summary was last updated.
-
-		:param timestamp: The timestamp when the summary was last updated.
-		:type timestamp: int
-		"""
-
-		self._last_updated = timestamp
-
-	def last_updated(self):
-		"""
-		Get the last time that the summary was updated.
-
-		:return: The last time that the summary was updated as a timestamp.
-		:rtype: int
-		"""
-
-		return self._last_updated
-
-	def get_concatenated_summary(self, cleaner=cleaner.Cleaner, *args, **kwargs):
-		"""
-		Get the concatenated summary as a document.
-		"""
-
-	def generate_summary(self, cleaner=cleaner.Cleaner, *args, **kwargs):
-		"""
-		Generate the actual textual summary. This is a concatenation of all the documents.
-
-		:param cleaner: The class to use to clean the documents.
-			By default, no cleaning is performed.
-			Each document is cleaned individually.
-		:type cleaner: :class:`~summarization.cleaners.cleaner.Cleaner`
-
-		:return: The textual summary.
+		:return: The string representation of the summary.
 		:rtype: str
 		"""
 
-		cleaner = cleaner()
-
-		documents = sorted(self._documents, key=lambda document: document.get_attribute("timestamp", 0))
-		snippets = [ cleaner.clean(document.get_text()) for document in documents ]
-		snippets = [ snippet for snippet in snippets if len(snippet) > 0 ]
-		return ' '.join(snippets)
+		return ' '.join([ document.text for document in self.documents ])
 
 	def to_array(self):
 		"""
-		Export the summary as an associative array.
+		Export the summary as a dictionary.
 
-		:return: The summary as an associative array.
+		:return: The summary as a dictionary.
 		:rtype: dict
 		"""
 
-		array = {}
-		array.update({
-			"documents": [ document.to_array() for document in self._documents ],
-			"last_updated": self._last_updated,
-			"created": self._created
-		})
-		return array
+		return {
+			'attributes': self._attributes,
+			'documents': [ document.to_array() for document in self.documents ]
+		}
 
 	@staticmethod
 	def from_array(array):
 		"""
-		Create a summary instance from the given associative array.
+		Create a summary instance from the given dictionary.
 
-		:param array: The associative array with the attributes to create the summary.
+		:param array: The dictionary with the necessary information to re-create the summary.
 		:type array: dict
 
-		:return: A new instance of an object with the same attributes stored in the object.
+		:return: A new summary.
 		:rtype: :class:`~summarization.summary.Summary`
 		"""
 
-		vectors = array.get("documents", [])
-		loaded_vectors = []
-		for vector in vectors:
-			c = vector.get("class", "")
-			c = c[c.index("'")+1:c.rindex("'")]
-			module_name, class_name = c[:c.rindex(".")], c[c.rindex(".")+1:]
+		documents = []
+		for vector in array.get('documents'):
+			cls = vector.get('class', '')
+			cls = cls[ cls.index('\'') + 1:cls.rindex('\'') ]
+			module_name, class_name = cls[ :cls.rindex('.') ], cls[ cls.rindex('.') + 1: ]
 			module = importlib.import_module(module_name)
-			c = getattr(module, class_name)
-			loaded_vectors.append(c.from_array(vector))
+			cls = getattr(module, class_name)
+			documents.append(cls.from_array(vector))
 
-		summary = Summary(documents=loaded_vectors, timestamp=array.get("created", 0))
-		summary.set_last_updated(array.get("last_updated"))
-		return summary
+		return Summary(documents=documents, attributes=array.get('attributes'))
--- eventdt/summarization/tests/test_summary.py
+++ eventdt/summarization/tests/test_summary.py
@@ -2,37 +2,62 @@
 Run unit tests on the Summary class.
 """
 
-import math
 import os
 import sys
 import unittest
 
-path = os.path.dirname(__file__)
-path = os.path.join(path, '../')
+path = os.path.join(os.path.dirname(__file__), '..')
 if path not in sys.path:
     sys.path.append(path)
 
-from libraries.summarization.summary import Summary
-
-from libraries.vector.nlp.document import Document
+from summary import Summary
+from nlp.document import Document
 
 class TestSummary(unittest.TestCase):
 	"""
 	Test the Summary class.
 	"""
 
+	def test_empty_summary(self):
+		"""
+		Test that when creating an empty summary, it is created with an empty list.
+		"""
+
+		summary = Summary()
+		self.assertEqual([ ], summary.documents)
+
+	def test_repr_empty(self):
+		"""
+		Test that the string representation of an empty summary is an empty string.
+		"""
+
+		summary = Summary()
+		self.assertEqual('', str(summary))
+
+	def test_repr(self):
+		"""
+		Test that the string representation of the summary is equivalent to all of the documents in it.
+		"""
+
+		documents = [ Document('this is not'), Document('a pipe') ]
+		summary = Summary(documents)
+		self.assertEqual('this is not a pipe', str(summary))
+
 	def test_export(self):
 		"""
 		Test exporting and importing vectors.
 		"""
 
-		d = [
-			Document(text="A"),
-			Document(text="B")
-		]
-		s = Summary(d, 100)
-		e = s.to_array()
+		documents = [ Document('A'), Document('B') ]
+		summary = Summary(documents, { 'a': 0 })
+		export = summary.to_array()
 
-		self.assertEqual(s.created_at(), Summary.from_array(e).created_at())
-		self.assertEqual(s.last_updated(), Summary.from_array(e).last_updated())
-		self.assertEqual(s.generate_summary(), Summary.from_array(e).generate_summary())
+		self.assertEqual(summary._attributes, Summary.from_array(export)._attributes)
+
+		"""
+		Check that the documents were exported correctly.
+		"""
+		self.assertEqual(len(documents), len(Summary.from_array(export).documents))
+		for document in documents:
+			self.assertTrue(any(document.__dict__ == imported.__dict__
+								for imported in Summary.from_array(export).documents))
