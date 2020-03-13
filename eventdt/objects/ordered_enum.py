f1dd304a65fc365a1b1952d2152b1a0d1af57fc4
NicholasMamo <nicholasmamo@gmail.com>
2020-03-13 19:59:01 +0100

    Fix the ordered enum's comparisons not working with integers

--- eventdt/objects/ordered_enum.py
+++ eventdt/objects/ordered_enum.py
@@ -11,6 +11,19 @@ class OrderedEnum(Enum):
 	Outlined in https://docs.python.org/3/library/enum.html#orderedenum
 	"""
 
+	def __eq__(self, other):
+		"""
+		Equality comparison.
+
+		:param other: The other value to compare with.
+		:type other: :class:`~objects.ordered_enum.OrderedEnum`
+		"""
+
+		if self.__class__ == other.__class__:
+			return self.value == other.value
+		else:
+			return self.value == other
+
 	def __ge__(self, other):
 		"""
 		Greater than or equal to comparison.
@@ -18,8 +31,11 @@ class OrderedEnum(Enum):
 		:param other: The other value to compare with.
 		:type other: :class:`~objects.ordered_enum.OrderedEnum`
 		"""
+
 		if self.__class__ == other.__class__:
 			return self.value >= other.value
+		else:
+			return self.value >= other
 
 	def __le__(self, other):
 		"""
@@ -28,8 +44,11 @@ class OrderedEnum(Enum):
 		:param other: The other value to compare with.
 		:type other: :class:`~objects.ordered_enum.OrderedEnum`
 		"""
+
 		if self.__class__ == other.__class__:
 			return self.value <= other.value
+		else:
+			return self.value <= other
 
 	def __gt__(self, other):
 		"""
@@ -38,8 +57,11 @@ class OrderedEnum(Enum):
 		:param other: The other value to compare with.
 		:type other: :class:`~objects.ordered_enum.OrderedEnum`
 		"""
+
 		if self.__class__ == other.__class__:
 			return self.value > other.value
+		else:
+			return self.value > other
 
 	def __lt__(self, other):
 		"""
@@ -48,5 +70,8 @@ class OrderedEnum(Enum):
 		:param other: The other value to compare with.
 		:type other: :class:`~objects.ordered_enum.OrderedEnum`
 		"""
+
 		if self.__class__ == other.__class__:
 			return self.value < other.value
+		else:
+			return self.value < other
