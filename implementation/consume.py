0db8069230fc0dcaca90ad62d378fbbf9f666b1c
NicholasMamo <nicholasmamo@gmail.com>
2020-03-16 16:49:36 +0100

    Rename the consumption tool

---
+++ implementation/consume.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+The consumer receives an input file and consumes it with one of the given consumers.
+This consumer is split into two asynchronous tasks.
+The first task reads the file, and the second consumes it.
+
+To run the script, use:
+
+.. code-block:: bash
+
+    ./implementation/consume.py
+
+"""
