eba8801a2ab2137811e8a282848f99ac6235cf70
NicholasMamo <nicholasmamo@gmail.com>
2020-03-16 19:16:03 +0100

    Create initial package test file

--- eventdt/twitter/tests/test_package.py
+++ eventdt/twitter/tests/test_package.py
@@ -2,11 +2,12 @@
 Test the functionality of the tweet package-level functions.
 """
 
-import math
+import json
 import os
 import sys
 import unittest
-import warnings
+
+from datetime import datetime
 
 path = os.path.join(os.path.dirname(__file__), '..', '..')
 if path not in sys.path:
@@ -19,4 +20,17 @@ class TestPackage(unittest.TestCase):
 	Test the functionality of the tweet package-level functions.
 	"""
 
-	
+	def test_timestamp_date(self):
+		"""
+		Test that the timestamp date is the same and correct for all tweets in the corpus.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			for line in f:
+				tweet = json.loads(line)
+				date = datetime.fromtimestamp(twitter.extract_timestamp(tweet))
+				self.assertEqual(2020, date.year)
+				self.assertEqual(3, date.month)
+				self.assertEqual(14, date.day)
+				self.assertEqual(14, date.hour)
+				self.assertTrue(date.minute in range(39, 42))
