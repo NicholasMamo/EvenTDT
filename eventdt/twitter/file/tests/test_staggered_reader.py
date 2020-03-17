02c5925303be930a0e555f63cf46811ca3c0cbbd
NicholasMamo <nicholasmamo@gmail.com>
2020-03-17 14:09:06 +0100

    Add support for asynchronous tests

--- eventdt/twitter/file/tests/test_staggered_reader.py
+++ eventdt/twitter/file/tests/test_staggered_reader.py
@@ -2,6 +2,7 @@
 Test the functionality of the staggered file reader.
 """
 
+import asyncio
 import json
 import os
 import sys
@@ -22,6 +23,14 @@ class TestStaggeredFileReader(unittest.TestCase):
 	"""
 	Test the functionality of the staggered file reader.
 	"""
+
+	def async_test(f):
+		def wrapper(*args, **kwargs):
+			coro = asyncio.coroutine(f)
+			future = coro(*args, **kwargs)
+			loop = asyncio.get_event_loop()
+			loop.run_until_complete(future)
+		return wrapper
 
 	def test_floating_point_rate(self):
 		"""
