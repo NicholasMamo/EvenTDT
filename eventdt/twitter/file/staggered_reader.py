3cbc35f830dc9e941ceea6f9334177cc337e421c
NicholasMamo <nicholasmamo@gmail.com>
2020-03-17 13:20:33 +0100

    Refactor the constructor of the staggered file reader and add exceptions

--- eventdt/twitter/file/staggered_reader.py
+++ eventdt/twitter/file/staggered_reader.py
@@ -1,53 +1,95 @@
-class StaggeredFileListener(FileListener):
-	"""
-	The StaggeredFileListener reads a line, then stops and waits.
+"""
+The staggered file reader reads lines a few at a time.
+This reader allows a steady stream of tweets to be read and processed.
+"""
 
-	:ivar _skip_lines: The number of lines to skip from the beginning of the file.
-	:vartype _skip_lines: int
-	:ivar _skip_time: The time (in seconds) to skip from the beginning of the file.
-		The time is taken from tweets' `created_at` attribute.
-	:vartype _skip_time: int
-	:ivar _skip_rate: The number of lines to skip for each line read.
-	:vartype _skip_rate: int
-	:ivar _lines_per_second: The number of lines to read per second.
-	:vartype lines_per_second: float
+from .reader import FileReader
+
+class StaggeredFileReader(FileReader):
+	"""
+	The staggered file reader reads lines a few at a time, then stops and waits.
+
+	:ivar rate: The number of lines to read per second.
+	:vartype rate: float
+	:ivar skip_lines: The number of lines to skip from the beginning of the file.
+	:vartype skip_lines: int
+	:ivar skip_time: The time in seconds to skip from the beginning of the file.
+					  The time is taken from tweets' `created_at` attribute.
+	:vartype skip_time: int
+	:ivar skip_rate: The number of lines to skip for each line read.
+	:vartype skip_rate: int
 	"""
 
-	def __init__(self, queue, f, skip_lines=0, skip_time=0, max_lines=-1, max_time=-1, skip_rate=0, lines_per_second=-1):
+	def __init__(self, queue, f, max_lines=-1, max_time=-1, rate=1, skip_lines=0, skip_time=0, skip_rate=0):
 		"""
 		Create the listener.
 		Simultaneously set the queue, the list of tweets and the number of processed tweets.
-		By default, the FileListener skips no lines, though it can be overwritten to skip any number of lines for every one read.
+		By default, the FileReader skips no lines, though it can be overwritten to skip any number of lines for every one read.
 		This makes it possible to sample the file.
-		By default, the FileListener has no limit on the number of lines to read per second.
+		By default, the FileReader has no limit on the number of lines to read per second.
 
 		:param queue: The queue to which to add the tweets.
 		:type queue: :class:`~queues.queue.Queue`
 		:param f: The opened file from where to read the tweets.
 		:type f: file
+		:param max_lines: The maximum number of lines to read.
+						  If the number is negative, it is ignored.
+		:type max_lines: int
+		:param max_time: The maximum time in seconds to spend reading from the file.
+						 The time is taken from tweets' `created_at` attribute.
+						 If the number is negative, it is ignored.
+		:type max_time: int
+		:param rate: The number of lines to read per second.
+		:type rate: float
 		:param skip_lines: The number of lines to skip from the beginning of the file.
 		:type skip_lines: int
 		:param skip_time: The time (in seconds) to skip from the beginning of the file.
-			The time is taken from tweets' `created_at` attribute.
+						  The time is taken from tweets' `created_at` attribute.
 		:type skip_time: int
 		:param max_lines: The maximum number of lines to read.
-			If the number is negative, it is ignored.
+						  If the number is negative, it is ignored.
 		:type max_lines: int
-		:param max_time: The maximum time (in seconds) to spend reading the file.
-			If the number is negative, it is ignored.
-		:type max_time: int
 		:param skip_rate: The number of lines to skip for each line read.
 		:type skip_rate: int
-		:param lines_per_second: The number of lines to read per second.
-		:type lines_per_second: float
+
+		:raises ValueError: When the rate is not an integer.
+		:raises ValueError: When the rate is zero or negative.
+		:raises ValueError: When the number of lines to skip is negative.
+		:raises ValueError: When the number of seconds to skip is negative.
+		:raises ValueError: When the number of lines to skip after each read is not an integer.
+		:raises ValueError: When the number of lines to skip after each read is negative.
 		"""
 
-		super(StaggeredFileListener, self).__init__(queue, f, max_lines=max_lines, max_time=max_time)
+		super(StaggeredFileReader, self).__init__(queue, f, max_lines=max_lines, max_time=max_time)
 
-		self._skip_lines = skip_lines
-		self._skip_time = skip_time
-		self._skip_rate = skip_rate
-		self._lines_per_second = lines_per_second
+		"""
+		Validate the inputs.
+		"""
+		if rate % 1:
+			raise ValueError(f"The rate must be an integer; received {rate}")
+
+		if rate <= 0:
+			raise ValueError(f"The rate must be positive; received {rate}")
+
+		if skip_lines % 1:
+			raise ValueError(f"The number of lines to skip must be an integer; received {skip_lines}")
+
+		if skip_lines < 0:
+			raise ValueError(f"The number of lines to skip cannot be negative; received {skip_lines}")
+
+		if skip_time < 0:
+			raise ValueError(f"The number of seconds to skip cannot be negative; received {skip_time}")
+
+		if skip_rate % 1:
+			raise ValueError(f"The rate of lines to skip after each read must be an integer; received {skip_rate}")
+
+		if skip_rate < 0:
+			raise ValueError(f"The rate of lines to skip after each read cannot be negative; received {skip_rate}")
+
+		self.rate = rate
+		self.skip_lines = skip_lines
+		self.skip_time = skip_time
+		self.skip_rate = skip_rate
 
 	async def read(self):
 		"""
@@ -65,7 +107,7 @@ class StaggeredFileListener(FileListener):
 		"""
 		Keep reading lines until the first encountered tweet published after the given number of seconds
 		"""
-		while get_timestamp(data["created_at"]) - original_start < self._skip_time :
+		while get_timestamp(data["created_at"]) - original_start < self.skip_time :
 			last_pos = self._file.tell() # record the file's position
 			skip += 1 # a new line has been skipped
 			"""
@@ -80,7 +122,7 @@ class StaggeredFileListener(FileListener):
 		"""
 		Skip a number of lines
 		"""
-		for i in range(skip, self._skip_lines):
+		for i in range(skip, self.skip_lines):
 			self._file.readline()
 
 		start = datetime.now().timestamp() # start timing the procedure
@@ -104,11 +146,11 @@ class StaggeredFileListener(FileListener):
 			"""
 			Skip some lines if need be
 			"""
-			for l in range(0, self._skip_rate):
+			for l in range(0, self.skip_rate):
 				self._file.readline()
 
 			"""
 			If there is a limit on the number of lines to read per minute, sleep a bit
 			"""
-			if self._lines_per_second > 0:
-				time.sleep(1/self._lines_per_second)
+			if self.rate > 0:
+				time.sleep(1/self.rate)
---
+++ eventdt/twitter/file/tests/test_staggered_reader.py
@@ -0,0 +1,184 @@
+"""
+Test the functionality of the staggered file reader.
+"""
+
+import json
+import os
+import sys
+import time
+import unittest
+
+from datetime import datetime
+from tweepy import OAuthHandler, Stream
+
+path = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from queues import Queue
+from twitter.file.staggered_reader import StaggeredFileReader
+
+class TestStaggeredFileReader(unittest.TestCase):
+	"""
+	Test the functionality of the staggered file reader.
+	"""
+
+	def test_floating_point_rate(self):
+		"""
+		Test that when creating a staggered file reader with a floating point rate, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, rate=0.1)
+
+	def test_float_rate(self):
+		"""
+		Test that when creating a staggered file reader with a rounded float rate, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, rate=1.0))
+
+	def test_integer_rate(self):
+		"""
+		Test that when creating a staggered file reader with an integer rate, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, rate=1))
+
+	def test_zero_rate(self):
+		"""
+		Test that when creating a staggered file reader with a rate of zero, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, rate=0)
+
+	def test_negative_rate(self):
+		"""
+		Test that when creating a staggered file reader with a negative rate, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, rate=-1)
+
+	def test_floating_point_skip_lines(self):
+		"""
+		Test that when creating a staggered file reader with a floating point number of lines to skip, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, skip_lines=0.1)
+
+	def test_float_skip_lines(self):
+		"""
+		Test that when creating a staggered file reader with a rounded float number of lines to skip, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_lines=1.0))
+
+	def test_integer_skip_lines(self):
+		"""
+		Test that when creating a staggered file reader with an integer number of lines to skip, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_lines=1))
+
+	def test_negative_skip_lines(self):
+		"""
+		Test that when creating a staggered file reader with a negative number of lines to skip, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, skip_lines=-1)
+
+	def test_zero_skip_lines(self):
+		"""
+		Test that when creating a staggered file reader that skips no lines, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_lines=0))
+
+	def test_positive_skip_lines(self):
+		"""
+		Test that when creating a staggered file reader that skips a positive number of lines, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_lines=1))
+
+	def test_negative_skip_time(self):
+		"""
+		Test that when creating a staggered file reader with a negative number of seconds to skip, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, skip_time=-1)
+
+	def test_zero_skip_time(self):
+		"""
+		Test that when creating a staggered file reader that skips no time, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_time=0))
+
+	def test_positive_skip_time(self):
+		"""
+		Test that when creating a staggered file reader that skips a positive number of seconds, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_time=1))
+
+	def test_floating_point_skip_rate(self):
+		"""
+		Test that when creating a staggered file reader with a floating point number of lines to skip after each read, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, skip_rate=0.1)
+
+	def test_float_skip_rate(self):
+		"""
+		Test that when creating a staggered file reader with a rounded float number of lines to skip after each read, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_rate=1.0))
+
+	def test_integer_skip_rate(self):
+		"""
+		Test that when creating a staggered file reader with an integer number of lines to skip after each read, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_rate=1))
+
+	def test_negative_skip_rate(self):
+		"""
+		Test that when creating a staggered file reader with a negative number of lines to skip after each read, a ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertRaises(ValueError, StaggeredFileReader, Queue(), f, skip_rate=-1)
+
+	def test_zero_skip_rate(self):
+		"""
+		Test that when creating a staggered file reader that skips no lines, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_rate=0))
+
+	def test_positive_skip_rate(self):
+		"""
+		Test that when creating a staggered file reader that skips a positive number of lines after each read, no ValueError is raised.
+		"""
+
+		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
+			self.assertTrue(StaggeredFileReader(Queue(), f, skip_rate=1))
