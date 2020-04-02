0084b9eac97b4e00fffa69ef7e20ce0cc4ad4f89
NicholasMamo <nicholasmamo@gmail.com>
2020-04-02 19:00:50 +0200

    Remove old files

--- eventdt/testcases/graph_testcase.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-sys.path.insert(1, os.path.join(sys.path[0], '../../'))
-
-from libraries.graph import graph
--- eventdt/testcases/postprocessor_testcase.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(1, os.path.join(sys.path[0], '../'))
-
-from apd.postprocessors.external.wikipedia_postprocessor import WikipediaPostprocessor
-
-pp = WikipediaPostprocessor()
-pp.postprocess(["Striker (association football)", "Inside forward"], [])
