026282006cf668a8f3c01f472676dbcf87b41a2a
NicholasMamo <nicholasmamo@gmail.com>
2020-05-27 17:47:27 +0200

    Simplify the aggregation function

--- eventdt/twitter/corpus/aggregate.py
+++ eventdt/twitter/corpus/aggregate.py
@@ -16,94 +16,23 @@ from nlp.document import Document
 from nlp.tokenizer import Tokenizer
 import twitter
 
-def volume(bin, track=None, *args, **kwargs):
-	"""
-	Count the number of documents in the given bin.
-	If a tracking keyword is given, the function returns the number of documents in which the keyword appears.
-	The function looks for the keyword in the dimensions.
-
-	:param bin: A bin containing a list of documents to count.
-	:type bin: list of :class:`~nlp.document.Document`
-	:param track: The keyword to track.
-	:type track: None or str
-
-	:return: The number of documents in the bin.
-			 If a tracking keyword is given, the function counts the number of documents in the bin that contain the keyword.
-	:rtype: int
-	"""
-
-	if track:
-		return len([ document for document in bin if track.lower() in document.dimensions ])
-	else:
-		return len(bin)
-
-def to_documents(tweets, tokenizer, scheme=None):
-	"""
-	Convert the given tweets into documents.
-
-	:param tweets: A list of tweets.
-	:type tweets: list of dict
-	:param tokenizer: The tokenizer used to create documents from tweets.
-	:type tokenizer: :class:`~nlp.tokenizer.Tokenizer`
-	:param scheme: The term-weighting scheme that is used to create dimensions.
-				   If `None` is given, the :class:`~nlp.term_weighting.tf.TF` term-weighting scheme is used.
-	:type scheme: None or :class:`~nlp.term_weighting.scheme.TermWeightingScheme`
-
-	:return: A list of documents created from the tweets in the same order as the given tweets.
-			 Documents are normalized and store the original tweet in the `tweet` attribute.
-	:rtype: list of :class:`~nlp.document.Document`
-	"""
-
-	documents = [ ]
-
-	"""
-	The text used for the document depend on what kind of tweet it is.
-	If the tweet is too long to fit in the tweet, the full text is used;
-
-	Retain the comment of a quoted status.
-	However, if the tweet is a plain retweet, get the full text.
-	"""
-	for tweet in tweets:
-		original = tweet
-		while "retweeted_status" in tweet:
-			tweet = tweet["retweeted_status"]
-
-		if "extended_tweet" in tweet:
-			text = tweet["extended_tweet"].get("full_text", tweet.get("text", ""))
-		else:
-			text = tweet.get("text", "")
-
-		"""
-		Create the document and save the tweet in it.
-		"""
-		tokens = tokenizer.tokenize(text)
-		document = Document(text, tokens, scheme=scheme)
-		document.attributes['tweet'] = original
-		document.normalize()
-		documents.append(document)
-
-	return documents
-
-def aggregate(corpus, bin_size=60, aggregation=volume, track=None, tokenizer=None, scheme=None, *args, **kwargs):
+def aggregate(corpus, agg, bin_size=60, track=None, *args, **kwargs):
 	"""
 	Aggregate the given corpus.
 	The tracking keywords as well as any other arguments or keyword arguments are passed on to the aggregation function.
 
+	The function is data-agnostic in terms of whether it receives tweets or not.
+	However in order to bin tweets, the function expects that there is a `timestamp_ms` or `created_at` attribute in each object.
+
 	:param corpus: The file handle containing the data.
     :type corpus: :class:`_io.TextIOWrapper`
-    :param bin_size: The length in seconds of each time window.
-    :type bin_size: int
-	:param aggregation: The aggregation function to use.
-						Defaults to simple volume.
-	:type aggregation: func
+	:param agg: The aggregation function to use.
+	:type agg: function
+	:param bin_size: The length in seconds of each time window.
+	:type bin_size: int
     :param track: The keywords to aggregate.
                   If it is not None, only documents containing the keyword are considered in the aggregation.
     :type track: None, str or list of str
-	:param tokenizer: The tokenizer used to create documents from tweets.
-	:type tokenizer: :class:`~nlp.tokenizer.Tokenizer`
-	:param scheme: The term-weighting scheme that is used to create dimensions.
-				   If `None` is given, the :class:`~nlp.term_weighting.tf.TF` term-weighting scheme is used.
-	:type scheme: None or :class:`~nlp.term_weighting.scheme.TermWeightingScheme`
 
 	:return: The tweets in the corpus aggregated into time windows.
 			 The time windows have keys that correspond to the tracking keywords.
@@ -118,10 +47,9 @@ def aggregate(corpus, bin_size=60, aggregation=volume, track=None, tokenizer=Non
 	"""
 	track = [ track ] if type(track) is str else track
 	track = track or [ ]
-	tokenizer = tokenizer or Tokenizer()
 
 	"""
-	Go through each line to count the documents.
+	Go through each line to aggregate the tweets.
 	"""
 	bin = []
 	current_time_window = None
@@ -139,15 +67,11 @@ def aggregate(corpus, bin_size=60, aggregation=volume, track=None, tokenizer=Non
 			"""
 			If the time window is over, process the tweets.
 			"""
-			documents = to_documents(bin, tokenizer)
-			for tweet, document in zip(bin, documents):
-				document.attributes['tweet'] = tweet
-
 			bins[current_time_window] = bins.get(current_time_window, { })
-			bins[current_time_window]['*'] =  bins[current_time_window].get('*', 0) + aggregation(documents, *args, **kwargs)
+			bins[current_time_window]['*'] =  bins[current_time_window].get('*', 0) + agg(bin, *args, **kwargs)
 			if track:
 				for keyword in track:
-					bins[current_time_window][keyword] =  bins[current_time_window].get(keyword, 0) + aggregation(documents, keyword, *args, **kwargs)
+					bins[current_time_window][keyword] =  bins[current_time_window].get(keyword, 0) + agg(bin, keyword, *args, **kwargs)
 
 			"""
 			Finally, reset the records.
@@ -160,14 +84,10 @@ def aggregate(corpus, bin_size=60, aggregation=volume, track=None, tokenizer=Non
 	"""
 	Finalize the aggregation with any last tweets that have not been committed.
 	"""
-	documents = to_documents(bin, tokenizer)
-	for tweet, document in zip(bin, documents):
-		document.attributes['tweet'] = tweet
-	
 	bins[current_time_window] = bins.get(current_time_window, { })
-	bins[current_time_window]['*'] =  bins[current_time_window].get('*', 0) + aggregation(documents, *args, **kwargs)
+	bins[current_time_window]['*'] =  bins[current_time_window].get('*', 0) + agg(bin, *args, **kwargs)
 	if track:
 		for keyword in track:
-			bins[current_time_window][keyword] =  bins[current_time_window].get(keyword, 0) + aggregation(documents, keyword, *args, **kwargs)
+			bins[current_time_window][keyword] =  bins[current_time_window].get(keyword, 0) + agg(bin, keyword, *args, **kwargs)
 
 	return bins
--- eventdt/twitter/corpus/tests/test_aggregate.py
+++ eventdt/twitter/corpus/tests/test_aggregate.py
@@ -24,175 +24,62 @@ class TestAggregate(unittest.TestCase):
 	Test the functionality of the aggregation functions.
 	"""
 
+	def volume(self, bin, track=None, *args, **kwargs):
+		"""
+		Count the number of tweets in the given bin.
+		If a tracking keyword is given, the function returns the number of tweets in which the keyword appears.
+		The function looks for the keyword in the dimensions.
+
+		:param bin: A bin containing a list of tweets to count.
+		:type bin: list of dict
+		:param track: The keyword to track.
+		:type track: None or str
+
+		:return: The number of tweets in the bin.
+				 If a tracking keyword is given, the function counts the number of tweets in the bin that contain the keyword.
+		:rtype: int
+		"""
+
+		if track:
+			return len([ tweet for tweet in bin if track.lower() in tweet['text'] ])
+		else:
+			return len(bin)
+
 	def test_volume_empty_bin(self):
 		"""
 		Test that the volume of an empty bin is 0.
 		"""
 
-		self.assertEqual(0, volume([ ]))
+		self.assertEqual(0, self.volume([ ]))
 
 	def test_volume_all(self):
 		"""
 		Test that the overall volume is the length of the bin.
 		"""
 
-		self.assertEqual(10, volume(range(0, 10)))
+		self.assertEqual(10, self.volume(range(0, 10)))
 
 	def test_volume_track(self):
 		"""
 		Test that the volume of a tracked keyword looks in the document's dimensions.
 		"""
 
-		self.assertEqual(1, volume([ Document('a', { 'b': 1 }) ], track='b'))
+		self.assertEqual(1, self.volume([ { 'text': 'b' } ], track='b'))
 
 	def test_volume_track_no_matches(self):
 		"""
 		Test that the volume of a tracked keyword returns 0 if there are no matches.
 		"""
 
-		self.assertEqual(0, volume([ Document('a', { 'b': 1 }) ], track='a'))
+		self.assertEqual(0, self.volume([ { 'text': 'b' } ], track='a'))
 
 	def test_volume_track_matches(self):
 		"""
 		Test that the volume of a tracked keyword returns the number of matches.
 		"""
 
-		self.assertEqual(1, volume([ Document('a', { 'b': 1 }),
-									 Document('a', { 'c': 1 }) ], track='b'))
-
-	def test_to_documents_tweet(self):
-		"""
-		Test that when creating a document from a tweet, the tweet is saved as an attribute.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			tweet = json.loads(f.readline())
-			document = to_documents([ tweet ], tokenizer)[0]
-			self.assertEqual(tweet, document.attributes['tweet'])
-
-	def test_to_documents_ellipsis(self):
-		"""
-		Test that when the text has an ellipsis, the full text is used.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			for line in f:
-				tweet = json.loads(line)
-				if '…' in tweet['text']:
-					document = to_documents([ tweet ], tokenizer)[0]
-
-					"""
-					Make an exception for a special case.
-					"""
-					if not ('retweeted_status' in tweet and tweet['retweeted_status']['id_str'] == '1238513167573147648'):
-						self.assertFalse(document.text.endswith('…'))
-
-	def test_to_documents_quoted(self):
-		"""
-		Test that when the tweet is a quote, the text is used, not the quoted tweet's text.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			for line in f:
-				tweet = json.loads(line)
-				if 'retweeted_status' in tweet:
-					timestamp = tweet['timestamp_ms']
-					tweet = tweet['retweeted_status']
-					tweet['timestamp_ms'] = timestamp
-
-				if 'quoted_status' in tweet:
-					document = to_documents([ tweet ], tokenizer)[0]
-
-					if 'extended_tweet' in tweet:
-						self.assertEqual(tweet["extended_tweet"].get("full_text", tweet.get("text", "")), document.text)
-					else:
-						self.assertEqual(tweet.get('text'), document.text)
-
-	def test_to_documents_retweeted(self):
-		"""
-		Test that when the tweet is a quote, the retweet's text is used.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			for line in f:
-				tweet = json.loads(line)
-				if 'retweeted_status' in tweet:
-					document = to_documents([ tweet ], tokenizer)[0]
-
-					retweet = tweet['retweeted_status']
-					if 'extended_tweet' in retweet:
-						self.assertEqual(retweet["extended_tweet"].get("full_text", retweet.get("text", "")), document.text)
-					else:
-						self.assertEqual(retweet.get('text'), document.text)
-
-					"""
-					Tweets shouldn't start with 'RT'.
-					"""
-					self.assertFalse(document.text.startswith('RT'))
-
-	def test_to_documents_normal(self):
-		"""
-		Test that when the tweet is not a quote or retweet, the full text is used.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			for line in f:
-				tweet = json.loads(line)
-				if not 'retweeted_status' in tweet and not 'quoted_status' in tweet:
-					document = to_documents([ tweet ], tokenizer)[0]
-
-					if 'extended_tweet' in tweet:
-						self.assertEqual(tweet["extended_tweet"].get("full_text", tweet.get("text", "")), document.text)
-					else:
-						self.assertEqual(tweet.get('text'), document.text)
-
-					"""
-					There should be no ellipsis in the text now.
-					"""
-					self.assertFalse(document.text.endswith('…'))
-
-	def test_to_documents_normalized(self):
-		"""
-		Test that the documents are returned normalized.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			for line in f:
-				tweet = json.loads(line)
-				document = to_documents([ tweet ], tokenizer)[0]
-				self.assertEqual(1, round(vector_math.magnitude(document), 10))
-
-	def test_to_documents_all(self):
-		"""
-		Test that when a list of documents are given, they are all converted to documents.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			lines = f.readlines()
-			tweets = [ json.loads(line) for line in lines ]
-			documents = to_documents(tweets, tokenizer)
-			self.assertEqual(len(tweets), len(documents))
-
-	def test_to_documents_order(self):
-		"""
-		Test that when a list of documents are given, the correct order is retained,
-		This test checks that the tweets are assigned correctly.
-		"""
-
-		tokenizer = Tokenizer()
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			lines = f.readlines()
-			tweets = [ json.loads(line) for line in lines ]
-			documents = to_documents(tweets, tokenizer)
-			for (tweet, document) in zip(tweets, documents):
-				self.assertEqual(tweet, document.attributes['tweet'])
+		self.assertEqual(1, self.volume([ { 'text': 'a' },
+										  { 'text': 'b' } ], track='b'))
 
 	def test_aggregate_timestamps(self):
 		"""
@@ -200,7 +87,7 @@ class TestAggregate(unittest.TestCase):
 		"""
 
 		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			volume = aggregate(f, bin_size=1)
+			volume = aggregate(f, agg=self.volume, bin_size=1)
 
 			f.seek(0)
 			lines = f.readlines()
@@ -214,7 +101,7 @@ class TestAggregate(unittest.TestCase):
 		"""
 
 		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			volume = aggregate(f, bin_size=1)
+			volume = aggregate(f, agg=self.volume, bin_size=1)
 
 			f.seek(0)
 			lines = f.readlines()
@@ -227,7 +114,7 @@ class TestAggregate(unittest.TestCase):
 
 		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
 			lines = reversed(f.readlines())
-			volume = aggregate(lines, bin_size=1)
+			volume = aggregate(lines, agg=self.volume,bin_size=1)
 
 			f.seek(0)
 			lines = f.readlines()
@@ -243,7 +130,7 @@ class TestAggregate(unittest.TestCase):
 		"""
 
 		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			volume = aggregate(f, bin_size=1, track='coronaviru')
+			volume = aggregate(f, agg=self.volume, bin_size=1, track='coronaviru')
 
 			self.assertTrue(all([ 'coronaviru' in volume[bin] for bin in volume ]))
 
@@ -253,30 +140,10 @@ class TestAggregate(unittest.TestCase):
 		"""
 
 		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			volume = aggregate(f, bin_size=1, track='terrier')
+			volume = aggregate(f, agg=self.volume, bin_size=1, track='terrier')
 
 			self.assertTrue(all([ 'terrier' in volume[bin] for bin in volume ]))
 			self.assertEqual(0, sum([ volume[bin]['terrier'] for bin in volume ]))
-
-	def test_aggregate_custom_tokenizer(self):
-		"""
-		Test that when a custom tokenizer is given, it is used.
-		"""
-
-		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			volume = aggregate(f, bin_size=1, track=[ 'coronaviru', 'coronavirus' ])
-
-			self.assertTrue(all([ 'coronaviru' in volume[bin] for bin in volume ]))
-			self.assertTrue(all([ volume[bin]['coronaviru'] > 0 for bin in volume ]))
-			self.assertTrue(all([ volume[bin]['coronavirus'] == 0 for bin in volume ]))
-
-			f.seek(0)
-			tokenizer = Tokenizer(stem=False)
-			volume = aggregate(f, bin_size=1, track=[ 'coronaviru', 'coronavirus' ], tokenizer=tokenizer)
-
-			self.assertTrue(all([ 'coronaviru' in volume[bin] for bin in volume ]))
-			self.assertTrue(all([ volume[bin]['coronaviru'] == 0 for bin in volume ]))
-			self.assertTrue(all([ volume[bin]['coronavirus'] > 0 for bin in volume ]))
 
 	def test_bin_size(self):
 		"""
@@ -284,7 +151,7 @@ class TestAggregate(unittest.TestCase):
 		"""
 
 		with open(os.path.join(os.path.dirname(__file__), 'corpus.json'), 'r') as f:
-			volume = aggregate(f, bin_size=5, track=[ 'coronaviru', 'coronavirus' ])
+			volume = aggregate(f, agg=self.volume, bin_size=5, track=[ 'coronaviru', 'coronavirus' ])
 			self.assertEqual(3, len(volume))
 			self.assertTrue(all(timestamp % 5 == 0 for timestamp in volume))
 
