d78af0bc659e729b04422b3d89392229ae11e299
NicholasMamo <nicholasmamo@gmail.com>
2020-05-28 14:46:25 +0200

    Implement the basic probability function

--- eventdt/ate/stat/probability.py
+++ eventdt/ate/stat/probability.py
@@ -1,3 +1,49 @@
 """
 Methods that evaluate unithood or termhood based on probability.
 """
+
+import json
+
+def p(corpora):
+	"""
+	Calculate the probability of tokens appearing in the corpus.
+	The probability is computed in terms of all tokens.
+
+	:param corpora: A corpus, or corpora, of documents.
+					If a string is given, it is assumed to be one corpus.
+					If a list is given, it is assumed to be a list of corpora.
+
+					It is assumed that the corpora were extracted using the tokenizer tool.
+					Therefore each line should be a JSON string representing a document.
+					Each document should have a `tokens` attribute.
+	:type corpora: str or list of str
+
+	:return: A dictionary with tokens as keys and probabilities as values.
+	:rtype dict:
+	"""
+
+	"""
+	Convert the corpora into a list if they aren't already.
+	"""
+	corpora = [ corpora ] if type(corpora) is str else corpora
+
+	"""
+	Count the total number of tokens encountered and a separate count for each token.
+	"""
+	tokens = 0
+	counts = { }
+	for corpus in corpora:
+		with open(corpus, 'r') as f:
+			for line in f:
+				document = json.loads(line)
+				tokens += len(document['tokens'])
+				for token in document['tokens']:
+					counts[token] = counts.get(token, 0) + 1
+
+	"""
+	Compute the probability by dividing the count by the number of tokens.
+	"""
+	if tokens:
+		return { token: count / tokens for token, count in counts.items() }
+
+	return { }
--- eventdt/ate/stat/tests/test_probability.py
+++ eventdt/ate/stat/tests/test_probability.py
@@ -17,3 +17,62 @@ class TestProbability(unittest.TestCase):
 	"""
 	Test the functionality of the probability functions.
 	"""
+
+	def test_probability_empty_corpus(self):
+		"""
+		Test that computing the probability on an empty corpus returns an empty probability dictionary.
+		"""
+
+		path = os.path.join(os.path.dirname(__file__), 'empty.json')
+		self.assertEqual({ }, p(path))
+
+	def test_probability_sums_one(self):
+		"""
+		Test that the probability sums up to one.
+		"""
+
+		path = os.path.join(os.path.dirname(__file__), 'c1.json')
+		self.assertEqual(1, round(sum(p(path).values()), 10))
+
+	def test_probability_corpora_sums_one(self):
+		"""
+		Test that the probability from multiple corpora sums up to one.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+		self.assertEqual(1, round(sum(p(paths).values()), 10))
+
+	def test_probability_nonzero(self):
+		"""
+		Test that all probabilities are non-zero.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+		self.assertTrue(all( probability > 0 for probability in p(paths).values() ))
+
+	def test_probability_less_than_one(self):
+		"""
+		Test that all probabilities are less than or equal to one.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+		self.assertTrue(all( probability <= 1 for probability in p(paths).values() ))
+
+	def test_probability_all_vocabulary(self):
+		"""
+		Test that probability is computed for the entire vocabulary.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		vocabulary = [ ]
+		for path in paths:
+			with open(path, 'r') as f:
+				for line in f:
+					vocabulary.extend(json.loads(line)['tokens'])
+
+		self.assertEqual(set(vocabulary), set(p(paths).keys()))
