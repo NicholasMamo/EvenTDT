9f3410e0aa768ce8a60f9b4bdeeea6e47d25f401
NicholasMamo <nicholasmamo@gmail.com>
2020-05-28 16:09:10 +0200

    Add support for subset probability calculations

--- eventdt/ate/stat/probability.py
+++ eventdt/ate/stat/probability.py
@@ -4,10 +4,16 @@ Methods that evaluate unithood or termhood based on probability.
 
 import json
 
-def p(corpora):
+def p(corpora, only=None):
 	"""
 	Calculate the probability of tokens appearing in the corpus.
 	The probability is computed in terms of all tokens.
+
+	Apart from calculating the probability of single tokens, the joint probability can be calculated by providing tuples.
+
+	.. note::
+
+		The joint probability is the minimum count of any token in each document.
 
 	:param corpora: A corpus, or corpora, of documents.
 					If a string is given, it is assumed to be one corpus.
@@ -17,15 +23,29 @@ def p(corpora):
 					Therefore each line should be a JSON string representing a document.
 					Each document should have a `tokens` attribute.
 	:type corpora: str or list of str
+	:param only: The tokens for which to compute the probability.
+				 If nothing is given, the probability is calculated for all tokens.
+				 The tokens can be provided as:
+
+				 - A single word,
+				 - A list of tokens,
+				 - A tuple,
+				 - A list of tuples.
+
+				 A tuple can be used to compute joint probabilities.
+	:type only: None or str or list of str or tuple or list of tuple
 
 	:return: A dictionary with tokens as keys and probabilities as values.
-	:rtype dict:
+	:rtype: dict
 	"""
 
 	"""
-	Convert the corpora into a list if they aren't already.
+	Convert the corpora and tokens into a list if they aren't already.
+	The list of tokens is always made into a list, even if it's a list of one string or tuple.
 	"""
 	corpora = [ corpora ] if type(corpora) is str else corpora
+	only = only or [ ]
+	only = [ only ] if type(only) is tuple or type(only) is str else only
 
 	"""
 	Count the total number of tokens encountered and a separate count for each token.
@@ -36,9 +56,31 @@ def p(corpora):
 		with open(corpus, 'r') as f:
 			for line in f:
 				document = json.loads(line)
+
+				"""
+				The number of tokens is always incremented by all the tokens.
+				"""
 				tokens += len(document['tokens'])
-				for token in document['tokens']:
-					counts[token] = counts.get(token, 0) + 1
+
+				"""
+				If there is no specification for which tokens to compute probability, compute the prior probability for all tokens.
+				"""
+				if not only:
+					for token in document['tokens']:
+						counts[token] = counts.get(token, 0) + 1
+				else:
+					"""
+					Convert each item in the list of tokens for which to compute the probability into a tuple.
+					"""
+					for item_set in only:
+						item_set = (item_set, ) if type(item_set) is str else item_set
+						min_count = min(document['tokens'].count(item) for item in item_set )
+
+						if len(item_set) > 1:
+							counts[item_set] = counts.get(item_set, 0) + min_count
+						else:
+							item = item_set[0]
+							counts[item] = counts.get(item, 0) + min_count
 
 	"""
 	Compute the probability by dividing the count by the number of tokens.
--- eventdt/ate/stat/tests/test_probability.py
+++ eventdt/ate/stat/tests/test_probability.py
@@ -76,3 +76,143 @@ class TestProbability(unittest.TestCase):
 					vocabulary.extend(json.loads(line)['tokens'])
 
 		self.assertEqual(set(vocabulary), set(p(paths).keys()))
+
+	def test_probability_token(self):
+		"""
+		Test that when computing the probability of one token, its probability is correct.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		probability = p(paths)
+
+		"""
+		Compute the probability for one token.
+		"""
+		subset = p(paths, only='yellow')
+		self.assertEqual(1, len(subset))
+		self.assertEqual({ 'yellow' }, set(subset.keys()))
+		self.assertLess(0, sum(subset.values()))
+		self.assertGreater(1, sum(subset.values()))
+		for token in subset:
+			self.assertEqual(probability[token], subset[token])
+
+	def test_probability_multiple_tokens(self):
+		"""
+		Test that when computing the probability of multiple tokens, their probabilities are correct.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		probability = p(paths)
+
+		"""
+		Compute the probability for a few tokens.
+		"""
+		subset = p(paths, only=[ 'yellow', 'card' ])
+		self.assertEqual(2, len(subset))
+		self.assertEqual({ 'yellow', 'card' }, set(subset.keys()))
+		self.assertLess(0, sum(subset.values()))
+		self.assertGreater(1, sum(subset.values()))
+		for token in subset:
+			self.assertEqual(probability[token], subset[token])
+
+	def test_probability_joint(self):
+		"""
+		Test that when computing the joint probability, they are returned correctly.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		probability = p(paths)
+
+		"""
+		Compute the probability for a few tokens.
+		"""
+		subset = p(paths, only=( 'yellow', 'card' ))
+		self.assertEqual(1, len(subset))
+		self.assertEqual({ ('yellow', 'card') }, set(subset.keys()))
+		self.assertLess(0, sum(subset.values()))
+		self.assertGreater(1, sum(subset.values()))
+
+		"""
+		Ensure that the total probability for the tuple's components exceeds or is equal to the joint probability.
+		"""
+		for tuple in subset:
+			total_probability = sum( probability[token] for token in tuple )
+			self.assertGreater(total_probability, subset[tuple])
+
+	def test_probability_multiple_joint(self):
+		"""
+		Test that when computing the joint probability for multiple tuples, they are returned correctly.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		probability = p(paths)
+
+		"""
+		Compute the probability for a few tokens.
+		"""
+		subset = p(paths, only=[ ( 'yellow', 'card' ), ( 'free', 'kick' ) ])
+		self.assertEqual(2, len(subset))
+		self.assertEqual({ ('yellow', 'card'), ( 'free', 'kick' ) }, set(subset.keys()))
+		self.assertLess(0, sum(subset.values()))
+		self.assertGreater(1, sum(subset.values()))
+
+		"""
+		Ensure that the total probability for the tuple's components exceeds or is equal to the joint probability.
+		"""
+		for tuple in subset:
+			total_probability = sum( probability[token] for token in tuple )
+			self.assertGreater(total_probability, subset[tuple])
+
+	def test_probability_token_joint_mix(self):
+		"""
+		Test that when computing the probability for a mix of tuples and tokens, they are returned correctly.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		probability = p(paths)
+
+		"""
+		Compute the probability for a few tokens.
+		"""
+		subset = p(paths, only=[ ( 'yellow', 'card' ), 'kick' ])
+		self.assertEqual(2, len(subset))
+		self.assertEqual({ ('yellow', 'card'), 'kick' }, set(subset.keys()))
+		self.assertLess(0, sum(subset.values()))
+		self.assertGreater(1, sum(subset.values()))
+
+		self.assertEqual(probability['kick'], subset['kick'])
+
+		"""
+		Ensure that the total probability for the tuple's components exceeds or is equal to the joint probability.
+		"""
+		for tokens in subset:
+			if type(tokens) is str:
+				continue
+
+			total_probability = sum( probability[token] for token in tokens )
+			self.assertGreater(total_probability, subset[tokens])
