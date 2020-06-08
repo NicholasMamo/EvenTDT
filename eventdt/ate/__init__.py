7daebe5ddd188fa9ac09dffacc3c8383443b8a06
NicholasMamo <nicholasmamo@gmail.com>
2020-06-08 16:14:08 +0200

    Add support to count the number of documents containing tokens or tuples

--- eventdt/ate/__init__.py
+++ eventdt/ate/__init__.py
@@ -2,7 +2,9 @@
 Package-level functions that are likely to be useful to both linguistic and statistical approaches.
 """
 
-def total_documents(corpora):
+import json
+
+def total_documents(corpora, focus=None):
 	"""
 	Count the total number of documents in the given corpora.
 
@@ -16,21 +18,58 @@ def total_documents(corpora):
 						Therefore each line should be a JSON string representing a document.
 						Each document should have a `tokens` attribute.
 	:type corpora: str or list of str
+	:param focus: The tokens for which to compute the document frequency.
+				  If nothing is given, the document frequency is calculated for all tokens.
+				  The tokens can be provided as:
+
+				  - A single word,
+				  - A list of tokens,
+				  - A tuple, or
+				  - A list of tuples.
+
+				  A tuple can be used to compute joint frequencies.
+	:type focus: None or str or list of str or tuple or list of tuple
 
 	:return: The number of documents in the given corpora.
-	:rtype: float
+			 If one token or one tuple is given, the number of documents in which the token or tuple appears are returned.
+			 If multiple tokens or tuples are given, a dictionary is given with the separate counts.
+	:rtype: int or dict
 	"""
 
-	documents = 0
+	documents = { '*': 0 } if not focus else { }
 
 	"""
 	Convert the corpora into a list if they aren't already.
+	Also convert the focus items into a list.
 	"""
 	corpora = [ corpora ] if type(corpora) is str else corpora
+	focus = focus or [ ]
+	focus = [ focus ] if type(focus) is tuple or type(focus) is str else focus
+	focus = [ (itemset, ) if type(itemset) is str else itemset for itemset in focus ]
+
+	"""
+	Create the initial counts for all tokens and joint probabilities.
+	This avoids returning missing probabilities for tokens or joint tokens that never appear.
+	"""
+	for itemset in focus:
+		documents[itemset if len(itemset) > 1 else itemset[0]] = 0
 
 	for corpus in corpora:
 		with open(corpus, 'r') as f:
 			for line in f:
-				documents += 1
+				document = json.loads(line)
 
-	return documents
+				"""
+				If focus tokens or tuples were given, check which itemsets appear in the given documents.
+				"""
+				if focus:
+					for itemset in focus:
+						if all( item in document['tokens'] for item in itemset ):
+							documents[itemset if len(itemset) > 1 else itemset[0]] += 1
+				else:
+					documents['*'] += 1
+
+	if len(documents) == 1:
+		return list(documents.values())[0]
+	else:
+		return documents
--- eventdt/ate/tests/test_package.py
+++ eventdt/ate/tests/test_package.py
@@ -60,3 +60,163 @@ class TestPackage(unittest.TestCase):
 				total += len(f.readlines())
 
 		self.assertEqual(documents, total)
+
+	def test_total_documents_int(self):
+		"""
+		Test that when no focus tokens or tuples are given, an integer is returned.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+				  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+		documents = ate.total_documents(paths)
+		self.assertEqual(int, type(documents))
+
+	def test_total_documents_token(self):
+		"""
+		Test that when computing the number of documents in which one token appears, the frequency is correct.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		token = 'yellow'
+		freq = ate.total_documents(paths, focus=token)
+		self.assertEqual(int, type(freq))
+
+		"""
+		Count the documents in which the token appears by reading all lines.
+		"""
+		total = 0
+		for path in paths:
+			with open(path, 'r') as f:
+				for line in f:
+					document = json.loads(line)
+					if token in document['tokens']:
+						total += 1
+
+		self.assertEqual(freq, total)
+
+	def test_total_documents_tuple(self):
+		"""
+		Test that when computing the number of documents in which one tuple appears, the frequency is correct.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		tuple = ('yellow', 'foul')
+		freq = ate.total_documents(paths, focus=tuple)
+		self.assertEqual(int, type(freq))
+
+		"""
+		Count the documents in which the tuple appears by reading all lines.
+		"""
+		total = 0
+		for path in paths:
+			with open(path, 'r') as f:
+				for line in f:
+					document = json.loads(line)
+					if all(token in document['tokens'] for token in tuple):
+						total += 1
+
+		self.assertEqual(freq, total)
+
+	def test_total_documents_multiple_tokens(self):
+		"""
+		Test that when computing the number of documents in which tokens appear, the frequencies are correct.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		tokens = [ 'yellow', 'foul' ]
+		freq = ate.total_documents(paths, focus=tokens)
+		self.assertEqual(dict, type(freq))
+		self.assertEqual(set(tokens), set(freq.keys()))
+
+		"""
+		Count the documents in which the tokens appears by reading all lines.
+		"""
+		counts = dict.fromkeys(tokens, 0)
+		for path in paths:
+			with open(path, 'r') as f:
+				for line in f:
+					document = json.loads(line)
+					for token in tokens:
+						if token in document['tokens']:
+							counts[token] += 1
+
+		self.assertEqual(counts, freq)
+
+	def test_total_documents_multiple_tuples(self):
+		"""
+		Test that when computing the number of documents in which tuples appear, the frequencies are correct.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		tuples = [ ('yellow', 'foul'), ('yellow', 'tackl') ]
+		freq = ate.total_documents(paths, focus=tuples)
+		self.assertEqual(dict, type(freq))
+		self.assertEqual(set(tuples), set(freq.keys()))
+
+		"""
+		Count the documents in which the tuples appears by reading all lines.
+		"""
+		counts = dict.fromkeys(tuples, 0)
+		for path in paths:
+			with open(path, 'r') as f:
+				for line in f:
+					document = json.loads(line)
+					for tuple in tuples:
+						if all(token in document['tokens'] for token in tuple):
+							counts[tuple] += 1
+
+		self.assertEqual(counts, freq)
+
+	def test_total_documents_mix(self):
+		"""
+		Test that when computing the number of documents in which a mix of tokens and tuples appear, the frequencies are correct.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'c1.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'c2.json') ]
+
+		"""
+		Compute the probability for all tokens in the corpora first.
+		"""
+		focus = [ 'yellow', ('yellow', 'tackl') ]
+		freq = ate.total_documents(paths, focus=focus)
+		self.assertEqual(dict, type(freq))
+		self.assertEqual(set(focus), set(freq.keys()))
+
+		"""
+		Count the documents in which the tokens and tuples appears by reading all lines.
+		"""
+		counts = dict.fromkeys(focus, 0)
+		for path in paths:
+			with open(path, 'r') as f:
+				for line in f:
+					document = json.loads(line)
+					for itemset in focus:
+						if type(itemset) is str:
+							if itemset in document['tokens']:
+								counts[itemset] += 1
+						else:
+							if all(token in document['tokens'] for token in itemset):
+								counts[itemset] += 1
+
+		self.assertEqual(counts, freq)
