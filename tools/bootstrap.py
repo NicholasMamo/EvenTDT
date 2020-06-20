92384146caaf155f65eb5839fbd0eedc0f6b82ea
NicholasMamo <nicholasmamo@gmail.com>
2020-06-20 20:42:48 +0200

    Create the initial bootstrap tool

---
+++ tools/bootstrap.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+A tool that receives a seed set of terms and looks for similar terms in the given corpora.
+
+To run the script, use:
+
+.. code-block:: bash
+
+    ./tools/bootstrap.py
+"""
