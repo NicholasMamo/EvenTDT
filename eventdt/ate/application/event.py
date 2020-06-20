631a1e6705d362c26d6a6899c267140870df4681
NicholasMamo <nicholasmamo@gmail.com>
2020-06-20 14:48:16 +0200

    Implement the event frequency

--- eventdt/ate/application/event.py
+++ eventdt/ate/application/event.py
@@ -1,3 +1,57 @@
 """
 Event-based ATE approaches use either event corpora or the output timelines to score and rank domain terms.
 """
+
+import json
+import os
+import sys
+
+path = os.path.join(os.path.dirname(__file__), '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from objects.exportable import Exportable
+
+def EF(timelines):
+	"""
+	Calculate the event frequency of terms from the given timelines.
+	The event frequency is simply the number of events in which term appears in a development.
+
+	:param timelines: The path to a timeline or a list of paths to timelines.
+					  If a string is given, it is assumed to be one event timeline.
+					  If a list is given, it is assumed to be a list of event timelines.
+
+					  .. note::
+
+					      It is assumed that the event timelines were extracted using the collection tool.
+						  Therefore each file should be a JSON string representing a :class:`~summarization.timeline.timeline.Timeline`.
+	:type corpora: str or list of str
+
+	:return: A dictionary with terms as keys and their event frequency as the values.
+	:rtype: dict
+	"""
+
+	ef = { }
+
+	timelines = [ timelines ] if type(timelines) is str else timelines
+
+	for timeline in timelines:
+		with open(timeline, 'r') as f:
+			data = json.loads(''.join(f.readlines()))
+
+			"""
+			Decode the timeline and extract all the terms in it.
+			"""
+			timeline = Exportable.decode(data)['timeline']
+			terms = set( term for node in timeline.nodes
+							  for topic in node.topics
+							  for term in topic.dimensions )
+
+			"""
+			Increment the event frequency of all the terms in the timeline.
+			"""
+
+			for term in terms:
+				ef[term] = ef.get(term, 0) + 1
+
+	return ef
--- eventdt/ate/application/tests/test_event.py
+++ eventdt/ate/application/tests/test_event.py
@@ -8,13 +8,101 @@ import string
 import sys
 import unittest
 
-path = os.path.join(os.path.dirname(__file__), '..')
-if path not in sys.path:
-    sys.path.append(path)
+paths = [ os.path.join(os.path.dirname(__file__), '..', '..', '..') ,
+		  os.path.join(os.path.dirname(__file__), '..') ]
+for path in paths:
+	if path not in sys.path:
+	    sys.path.append(path)
 
+from objects.exportable import Exportable
 import event
 
 class TestEvent(unittest.TestCase):
 	"""
 	Test the functionality of the event-based ATE approaches.
 	"""
+
+	def test_ef_one_timeline(self):
+		"""
+		Test that when providing one timeline, the algorithm extracts terms only from it.
+		"""
+
+		path = os.path.join(os.path.dirname(__file__), 'corpora', 'CRYCHE_FUL.json')
+		self.assertTrue(event.EF(path))
+
+	def test_ef_multiple_timeline(self):
+		"""
+		Test that when providing multiple timelines, the algorithm extracts terms from all of them.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'corpora', 'CRYCHE_FUL.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'corpora', 'LIVNAP_FUL.json') ]
+		ef = event.EF(paths)
+		self.assertTrue(ef)
+		self.assertTrue(all( term in ef for term in event.EF(paths[0]) ))
+		self.assertTrue(all( term in ef for term in event.EF(paths[1]) ))
+
+	def test_ef_lower_limit(self):
+		"""
+		Test that the minimum event frequency is 1, not 0.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'corpora', 'CRYCHE_FUL.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'corpora', 'LIVNAP_FUL.json') ]
+		ef = event.EF(paths)
+		self.assertEqual(1, min(ef.values()))
+
+	def test_ef_max_limit(self):
+		"""
+		Test that the maximum event frequency is equivalent to the number of timelines provided.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'corpora', 'CRYCHE_FUL.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'corpora', 'LIVNAP_FUL.json') ]
+		ef = event.EF(paths)
+		self.assertEqual(len(paths), max(ef.values()))
+
+	def test_ef_integers(self):
+		"""
+		Test that the event frequency is always an integer.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'corpora', 'CRYCHE_FUL.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'corpora', 'LIVNAP_FUL.json') ]
+		ef = event.EF(paths)
+		self.assertTrue(all( type(value) == int for value in ef.values() ))
+
+	def test_ef_all_terms(self):
+		"""
+		Test that the event frequency includes all breaking terms.
+		"""
+
+		paths = [ os.path.join(os.path.dirname(__file__), 'corpora', 'CRYCHE_FUL.json'),
+		 		  os.path.join(os.path.dirname(__file__), 'corpora', 'LIVNAP_FUL.json') ]
+
+		"""
+		Calculate the event frequency.
+		"""
+		ef = event.EF(paths)
+
+		"""
+		Extract all terms from the timelines.
+		"""
+		all_terms = set()
+		for timeline in paths:
+			with open(timeline, 'r') as f:
+				data = json.loads(''.join(f.readlines()))
+
+				"""
+				Decode the timeline and extract all the terms in it.
+				"""
+				timeline = Exportable.decode(data)['timeline']
+				terms = set( term for node in timeline.nodes
+								  for topic in node.topics
+								  for term in topic.dimensions )
+				all_terms = all_terms.union(terms)
+
+		"""
+		Assert that all terms are in the event frequency.
+		"""
+		self.assertEqual(all_terms, set(ef))
