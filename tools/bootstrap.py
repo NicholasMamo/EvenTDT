ab7ce0c7f9c667235b5c02e35a6404ff66490f7d
NicholasMamo <nicholasmamo@gmail.com>
2020-06-20 20:43:08 +0200

    Create outline of the bootstrap tool

--- tools/bootstrap.py
+++ tools/bootstrap.py
@@ -9,3 +9,24 @@ To run the script, use:
 
     ./tools/bootstrap.py
 """
+
+import argparse
+
+def setup_args():
+	"""
+	Set up and get the list of command-line arguments.
+	"""
+
+	parser = argparse.ArgumentParser(description="Bootstrap a seed set of terms.")
+	args = parser.parse_args()
+	return args
+
+def main():
+	"""
+	Main program loop.
+	"""
+
+	args = setup_args()
+
+if __name__ == "__main__":
+	main()
