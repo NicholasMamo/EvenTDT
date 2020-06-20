34d7f8f33cd4ada204778676e707857ec7bd0633
NicholasMamo <nicholasmamo@gmail.com>
2020-06-20 21:06:06 +0200

    Add support to choose a bootstrapping method in the bootstrap tool

--- tools/bootstrap.py
+++ tools/bootstrap.py
@@ -15,9 +15,20 @@ Accepted arguments:
 
 	- ``-s --seed``			*<Required>* The seed set of keywords.
 	- ``-f --files``		*<Required>* The input corpora where to look for similar keywords.
+	- ``--m -method``		*<Required>* The method to use to look for similar keywords; supported: `PMI`, `CHI`.
 """
 
 import argparse
+import os
+import sys
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+root = os.path.join(file_path, '..')
+lib = os.path.join(root, 'eventdt')
+sys.path.insert(-1, root)
+sys.path.insert(-1, lib)
+
+from ate.bootstrapping.probability import PMI, CHI
 
 def setup_args():
 	"""
@@ -27,6 +38,7 @@ def setup_args():
 
 		- ``-s --seed``			*<Required>* The seed set of keywords.
 		- ``-f --files``		*<Required>* The input corpora where to look for similar keywords.
+		- ``-m --method``		*<Required>* The method to use to look for similar keywords; supported: `PMI`, `CHI`.
 
 	:return: The command-line arguments.
 	:rtype: :class:`argparse.Namespace`
@@ -40,6 +52,9 @@ def setup_args():
 	parser.add_argument('-f', '--files',
 						nargs='+', required=True,
 						help='<Required> The input corpora where to look for similar keywords.')
+	parser.add_argument('-m', '--method',
+						type=method, required=True,
+						help='<Required> The method to use to look for similar keywords; supported: `PMI`, `CHI`.')
 
 	args = parser.parse_args()
 	return args
@@ -50,6 +65,34 @@ def main():
 	"""
 
 	args = setup_args()
+	print(args.method)
+
+def method(method):
+	"""
+	Convert the given string into a bootstrapping function.
+	The accepted methods are:
+
+		#. :func:`~ate.bootstrapping.probability.PMI`,
+		#. :func:`~ate.bootstrapping.probability.CHI`
+
+	:param method: The method string.
+	:type method: str
+
+	:return: The function that corresponds to the given method.
+	:rtype: function
+
+	:raises argparse.ArgumentTypeError: When the given method string is invalid.
+	"""
+
+	methods = {
+		'pmi': PMI,
+		'chi': CHI,
+	}
+
+	if method.lower() in methods:
+		return methods[method.lower()]
+
+	raise argparse.ArgumentTypeError(f"Invalid method value: {method}")
 
 if __name__ == "__main__":
 	main()
