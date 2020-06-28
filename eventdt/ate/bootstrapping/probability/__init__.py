2b6506f1ca85630790845d7e7d9872fcbdab57ea
NicholasMamo <nicholasmamo@gmail.com>
2020-06-28 16:04:00 +0200

    Add documentation to the probability bootstrapping package

--- eventdt/ate/bootstrapping/probability/__init__.py
+++ eventdt/ate/bootstrapping/probability/__init__.py
@@ -1,2 +1,8 @@
+"""
+Probability-based bootstrapping approaches use principles founded in probability theory.
+These include measures that measure correlation.
+This correlation, in turn, is taken to signify the statistical significance between a seed term and a candidate term.
+"""
+
 from .pmi import PMIBootstrapper
 from .chi import ChiBootstrapper
