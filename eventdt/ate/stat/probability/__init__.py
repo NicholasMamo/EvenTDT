04cbf3008f8be63ece2bb96ed32a2365804dcee2
NicholasMamo <nicholasmamo@gmail.com>
2020-06-28 12:59:48 +0200

    Create a new probability ATE package

---
+++ eventdt/ate/stat/probability/__init__.py
@@ -0,0 +1,6 @@
+"""
+The probability package includes probability-based ATE approaches.
+In addition, the package comes with functions that are likely to be useful to probability-based algorithms.
+This includes caching functionality, which collects all documents across all corpora that mention a particular token.
+"""
+
