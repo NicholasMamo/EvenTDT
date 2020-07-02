d065a9bec88a2ba3f61c92a8a1fe2ddeb8e0bd15
NicholasMamo <nicholasmamo@gmail.com>
2020-07-02 18:30:55 +0200

    Update the filter's documentation

--- eventdt/apd/filters/filter.py
+++ eventdt/apd/filters/filter.py
@@ -1,18 +1,18 @@
 """
-The filter is the third step of the APD process.
-It is responsible for removing candidates that it deems to be invalid.
-The output is still a dictionary of candidate participants and their scores.
-
-The input candidates should be the product of a :class:`~apd.scorers.scorer.Scorer` process.
-In other words, they should be a dictionary, with the keys being the candidates and the values being the score.
-
-The functionality revolves around one method: the :func:`~apd.filters.filter.Filter.filter` method.
-The function returns a dictionary of candidate participants and their scores.
+Scoring is the third step of the APD process.
+This step removes candidate participants that are unlikely to be participants.
+Possibly, they are removed on the basis of a low score.
+The functionality revolves around the :class:`~apd.filters.filter.Filter`'s :func:`~apd.filters.filter.Filter.filter` method.
 """
 
 class Filter(object):
 	"""
-	The filter goes through candidate participants and removes candidates that are not credible enough.
+	The filter goes through candidate participants and removes those that are unlikely to be valid participants.
+
+	The functionality revolves around one method: the :func:`~apd.filters.filter.Filter.filter` method.
+	The input candidates should be the product of a :class:`~apd.scorers.scorer.Scorer` process.
+	In other words, they should be a dictionary, with the keys being the candidates and the values being the score.
+	The function returns a subset of the input candidates (and their scores) that passed the filtering test.
 	"""
 
 	def filter(self, candidates, *args, **kwargs):
