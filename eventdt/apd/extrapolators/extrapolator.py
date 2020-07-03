90d005cf21815079ef6fc353aa4caa7eba1022d2
NicholasMamo <nicholasmamo@gmail.com>
2020-07-03 12:04:21 +0200

    Update the extrapolator's documentation

--- eventdt/apd/extrapolators/extrapolator.py
+++ eventdt/apd/extrapolators/extrapolator.py
@@ -1,20 +1,20 @@
 """
-The extrapolator is the fifth step of APD.
-The extrapolator receives a list of participants and tries to find other relevant participants.
-This step is analogous to entity set expansion.
+Extrapolation is the fifth step of the APD process.
+This step is analogous to entity set expansion: it looks for similar participants to those that were resolved previously.
+
+The functionality revolves around the :class:`~apd.extrapolators.extrapolator.Extrapolator`'s :func:`~apd.extrapolators.extrapolator.Extrapolator.extrapolate` method.
+The main function returns the participants in descending order of their relevance to the event's corpus.
 The participants should be ranked in descending order of their relevance.
-
-The input participants should be the product of a :class:`~apd.resolvers.resolver.Resolver` process.
-This input is a simple list of strings, each representing a participant.
-
-Extrapolators return a list of new participants: simple strings as well.
-The functionality revolves around one method: the :func:`~apd.extrapolators.extrapolator.Extrapolator.extrapolate` method.
-The function should return participants in descending order of their relevance to the domain.
 """
 
 class Extrapolator(object):
 	"""
 	The simplest extrapolator returns no new participants.
+	However, it defines the functionality that is common to all other extrapolators.
+
+	The functionality revolves around the :func:`~apd.extrapolators.extrapolator.Extrapolator.extrapolate` method.
+	The input participants should be the product of a :class:`~apd.resolvers.resolver.Resolver` process: a list of strings representing participants.
+	The output, then, is a list of new participants that the extrapolator considers valid for the event.
 	"""
 
 	def extrapolate(self, participants, *args, **kwargs):
