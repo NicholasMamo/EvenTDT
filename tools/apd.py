affcf9efb4bb4df03f50546222700136581deab8
NicholasMamo <nicholasmamo@gmail.com>
2020-07-20 16:16:43 +0200

    Create the initial APD tool

---
+++ tools/apd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+A tool to automatically extract participants from the given corpora.
+This tool is meant to run on the understanding corpora to extract the event's participants.
+
+To run the script, use:
+
+.. code-block:: bash
+
+    ./tools/apd.py
+
+"""
