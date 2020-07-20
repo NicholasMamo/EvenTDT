c58baa5743360b504496b9be981a9ed8b802dca1
NicholasMamo <nicholasmamo@gmail.com>
2020-07-20 16:21:50 +0200

    Create the APD tool's loop

--- tools/apd.py
+++ tools/apd.py
@@ -11,3 +11,35 @@ To run the script, use:
     ./tools/apd.py
 
 """
+
+import argparse
+import os
+import sys
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+root = os.path.join(file_path, '..')
+lib = os.path.join(root, 'eventdt')
+sys.path.insert(-1, root)
+sys.path.insert(-1, lib)
+
+parser = argparse.ArgumentParser(description="Extract terms from domain-specific corpora.")
+def setup_args():
+	"""
+	Set up and get the list of command-line arguments.
+
+	:return: The command-line arguments.
+	:rtype: :class:`argparse.Namespace`
+	"""
+
+	args = parser.parse_args()
+	return args
+
+def main():
+	"""
+	Main program loop.
+	"""
+
+	args = setup_args()
+
+if __name__ == "__main__":
+	main()
