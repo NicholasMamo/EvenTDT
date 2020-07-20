9684950ff5d98e5bcf756aa2347d3527ce590983
NicholasMamo <nicholasmamo@gmail.com>
2020-07-20 16:50:37 +0200

    Remove the TwitterNER entity extractor from package-level

--- eventdt/apd/extractors/local/__init__.py
+++ eventdt/apd/extractors/local/__init__.py
@@ -4,4 +4,3 @@ Local extractors look for candidate participants in the event's corpus.
 
 from .entity_extractor import EntityExtractor
 from .token_extractor import TokenExtractor
-from .twitterner_entity_extractor import TwitterNEREntityExtractor
