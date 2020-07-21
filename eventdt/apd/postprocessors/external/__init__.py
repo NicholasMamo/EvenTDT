23ed5a1c34969cf1e7d4755f564243df536103ab
NicholasMamo <nicholasmamo@gmail.com>
2020-07-21 13:13:15 +0200

    Add the Wikipedia post processor to package-level

--- eventdt/apd/postprocessors/external/__init__.py
+++ eventdt/apd/postprocessors/external/__init__.py
@@ -3,3 +3,5 @@ External post-processors use information from outside the event's corpus to modi
 These post-processors are useful when resolution or extrapolation maps participants to semantic concepts, like Wikipedia articles.
 In these cases, the extra information can be used to post-process participants.
 """
+
+from .wikipedia_postprocessor import WikipediaPostprocessor
