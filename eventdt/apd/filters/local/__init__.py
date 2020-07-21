e1618db12943ae2cc650c250fea56e2cc58aeaba
NicholasMamo <nicholasmamo@gmail.com>
2020-07-21 11:56:40 +0200

    Add the filters to the package level

--- eventdt/apd/filters/local/__init__.py
+++ eventdt/apd/filters/local/__init__.py
@@ -2,3 +2,6 @@
 Local filtering strategies consider only the scores of candidate participants to decide whether they are credible.
 Therefore these kind of filters are usually very limited in what they can do.
 """
+
+from .rank_filter import RankFilter
+from .threshold_filter import ThresholdFilter
