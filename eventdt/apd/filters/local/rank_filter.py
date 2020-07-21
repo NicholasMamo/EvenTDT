6271304bfeefca1d0e9cd3ad036ec96bcd8f2c60
NicholasMamo <nicholasmamo@gmail.com>
2020-07-21 11:48:10 +0200

    Create the initial rank filter

---
+++ eventdt/apd/filters/local/rank_filter.py
@@ -0,0 +1,37 @@
+"""
+The rank filter extracts the top _k_ candidates.
+Anything else is filtered out.
+"""
+
+import math
+
+from ..filter import Filter
+
+class RankFilter(Filter):
+	"""
+	The rank filter is used to extract the top _k_ candidates.
+	The _k_ parameter is passed on to the constructor and stored as an instance variable.
+	It is then re-used by the :func:`~apd.filters.local.rank_filter.RankFilter.filter` method.
+
+	:vartype k: The number of candidates to retain.
+	:vartype k: int
+	"""
+
+	def __init__(self, k):
+		"""
+		Create the filter with the k that will decide whether candidate participants will be retained.
+
+		:param k: The number of candidates to retain.
+		:type k: int
+
+		:raises ValueError: When _k_ is not an integer.
+		:raises ValueError: When _k_ is not positive.
+		"""
+
+		if type(k) is not int:
+			raise ValueError(f"k must be an integer; received {k} ({ type(k) })")
+
+		if k <= 0:
+			raise ValueError(f"k must be a positive integer; received {k}")
+
+		self.k = k
---
+++ eventdt/apd/filters/local/tests/test_rank_filter.py
@@ -0,0 +1,67 @@
+"""
+Test the functionality of the rank filter.
+"""
+
+import math
+import os
+import sys
+import unittest
+import warnings
+
+path = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from apd.extractors.local.entity_extractor import EntityExtractor
+from apd.scorers.local.tf_scorer import TFScorer
+from apd.filters.local.rank_filter import RankFilter
+
+from nlp.document import Document
+from nlp.tokenizer import Tokenizer
+
+class TestRankFilter(unittest.TestCase):
+	"""
+	Test the implementation and results of the rank filter.
+	"""
+
+	def test_k_float(self):
+		"""
+		Test that when k is a float, the rank filter raises a ValueError.
+		"""
+
+		self.assertRaises(ValueError, RankFilter, 1.1)
+
+	def test_k_string(self):
+		"""
+		Test that when k is a string, the rank filter raises a ValueError.
+		"""
+
+		self.assertRaises(ValueError, RankFilter, "1")
+
+	def test_k_int(self):
+		"""
+		Test that when k is a int, the rank filter does not raise a ValueError.
+		"""
+
+		self.assertTrue(RankFilter(1))
+
+	def test_k_negative(self):
+		"""
+		Test that when k is negative, the rank filter raises a ValueError.
+		"""
+
+		self.assertRaises(ValueError, RankFilter, -1)
+
+	def test_k_zero(self):
+		"""
+		Test that when k is zero, the rank filter raises a ValueError.
+		"""
+
+		self.assertRaises(ValueError, RankFilter, 0)
+
+	def test_k_positive(self):
+		"""
+		Test that when k is positive, the rank filter does not raise a ValueError.
+		"""
+
+		self.assertTrue(RankFilter(1))
