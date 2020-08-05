70ea71ec159411ab878cd2c28a501f8a4b68fbb2
NicholasMamo <nicholasmamo@gmail.com>
2020-08-05 13:18:54 +0200

    Add variability to the package level

--- eventdt/ate/application/__init__.py
+++ eventdt/ate/application/__init__.py
@@ -5,3 +5,4 @@ Application-based approaches use the target application to learn the domain lexi
 from .event import EF
 from .event import LogEF
 from .event import EFIDF
+from .event import Variability
