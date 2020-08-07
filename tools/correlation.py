4120fa34a6dba6d9056f49b030e7fe01f37a8fb4
NicholasMamo <nicholasmamo@gmail.com>
2020-08-07 14:46:46 +0200

    Add the initial argument parser

--- tools/correlation.py
+++ tools/correlation.py
@@ -10,3 +10,29 @@ To run the script, use:
     ./tools/correlation.py
 
 """
+
+import argparse
+
+def setup_args():
+	"""
+	Set up and get the list of command-line arguments.
+
+	:return: The command-line arguments.
+	:rtype: :class:`argparse.Namespace`
+	"""
+
+	parser = argparse.ArgumentParser(description="Calculate the correlation between the given set of terms.")
+
+	args = parser.parse_args()
+	return args
+
+def main():
+	"""
+	The main program loop.
+	"""
+
+	args = setup_args()
+	print(args)
+
+if __name__ == "__main__":
+	main()
