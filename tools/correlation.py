135d62c66a74ad6830450685b2832e11ef7ae054
NicholasMamo <nicholasmamo@gmail.com>
2020-08-07 14:43:24 +0200

    Create the initial correlation tool

---
+++ tools/correlation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+The correlation tool receives a list of words and a list of files and computes their correlation.
+
+To run the script, use:
+
+.. code-block:: bash
+
+    ./tools/correlation.py
+
+"""
