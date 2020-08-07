72ae4758e38a583c040e0d7d06fecb10ac3d23ba
NicholasMamo <nicholasmamo@gmail.com>
2020-08-07 15:05:32 +0200

    Update the correlation tool to support the method parameter

--- tools/correlation.py
+++ tools/correlation.py
@@ -9,11 +9,13 @@ To run the script, use:
 
     ./tools/correlation.py \\
 	--files data/tokenized_corpus.json \\
+	--method CHI \\
 	--output data/correlation.json
 
 Accepted arguments:
 
 	- ``-f --files``		*<Required>* The input corpora from which to calculate the correlation betwee terms, expected to be already tokenized by the `tokenize` tool.
+	- ``-m --method``		*<Required>* The method to use to compute the correlation values; supported: `PMI`, `CHI`.
 	- ``-o --output``		*<Required>* The path to the file where to store the correlation values.
 """
 
@@ -28,6 +30,7 @@ sys.path.insert(-1, root)
 sys.path.insert(-1, lib)
 
 import tools
+from ate.bootstrapping.probability import PMIBootstrapper, ChiBootstrapper
 
 def setup_args():
 	"""
@@ -36,6 +39,7 @@ def setup_args():
 	Accepted arguments:
 
 		- ``-f --files``		*<Required>* The input corpora from which to calculate the correlation betwee terms, expected to be already tokenized by the `tokenize` tool.
+		- ``-m --method``		*<Required>* The method to use to compute the correlation values; supported: `PMI`, `CHI`.
 		- ``-o --output``		*<Required>* The path to the file where to store the correlation values.
 
 	:return: The command-line arguments.
@@ -47,6 +51,9 @@ def setup_args():
 	parser.add_argument('-f', '--files',
 						nargs='+', required=True,
 						help='<Required> The input corpora from which to calculate the correlation betwee terms, expected to be already tokenized by the `tokenize tool.')
+	parser.add_argument('-m', '--method',
+						type=method, required=True,
+						help='<Required> The method to use to compute the correlation values; supported: `PMI`, `CHI`.')
 	parser.add_argument('-o', '--output',
 						type=str, required=True,
 						help='<Required> The path to the file where to store the correlation values.')
@@ -65,8 +72,36 @@ def main():
 	Get the meta arguments.
 	"""
 	cmd = tools.meta(args)
+	cmd['method'] = str(vars(args)['method'])
 
 	tools.save(args.output, { 'meta': cmd })
+
+def method(method):
+	"""
+	Convert the given string into a correlation function.
+	The accepted methods are:
+
+		#. :func:`~ate.bootstrapping.probability.pmi.PMIBootstrapper`,
+		#. :func:`~ate.bootstrapping.probability.chi.ChiBootstrapper`
+
+	:param method: The method string.
+	:type method: str
+
+	:return: The function that corresponds to the given method.
+	:rtype: function
+
+	:raises argparse.ArgumentTypeError: When the given method string is invalid.
+	"""
+
+	methods = {
+		'pmi': PMIBootstrapper,
+		'chi': ChiBootstrapper,
+	}
+
+	if method.lower() in methods:
+		return methods[method.lower()]
+
+	raise argparse.ArgumentTypeError(f"Invalid method value: {method}")
 
 if __name__ == "__main__":
 	main()
