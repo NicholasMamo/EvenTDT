57e1b9c663bc29311c5ce01208e8a21a6f47e1ea
NicholasMamo <nicholasmamo@gmail.com>
2020-08-14 16:03:58 +0200

    Update the cluster class' documentation

--- eventdt/vsm/clustering/cluster.py
+++ eventdt/vsm/clustering/cluster.py
@@ -6,7 +6,7 @@ To achieve this goal, good clusters:
 1. Have a high distance between them if they (or, their instances) represent different topics, and
 2. Have a small distance between their own :class:`~vsm.vector.Vector` instances.
 
-Although you can create a :class:`~vsm.cluster.Cluster` instance yourself, it is more common to generate clusters automatically using a :class:`~vsm.clustering.algorithms.clustering.ClusteringAlgorithm`.
+Although you can create a :class:`~vsm.clustering.cluster.Cluster` instance yourself, it is more common to generate clusters automatically using a :class:`~vsm.clustering.algorithms.clustering.ClusteringAlgorithm`.
 """
 
 import importlib
@@ -24,13 +24,19 @@ from vsm import Vector, vector_math
 
 class Cluster(Attributable, Exportable):
 	"""
-	The cluster class is a collection of vectors, or documents.
-	It also has a centroid.
-	Clusters are based on :class:`~objects.Attributable` so that they may have additional properties.
+	The :class:`~vsm.clustering.cluster.Cluster` class is a collection of :class:`~vsm.vector.Vector` instances, or inherited classes, like :class:`~nlp.document.Document`.
+	You can add :class:`~vsm.vector.Vector` instances to the :class:`~vsm.clustering.cluster.Cluster` using the ``vectors`` property.
+	This property wraps a ``list``, which means that you can also use it to retrieve the :class:`~vsm.vector.Vector` instances.
+
+	The :class:`~vsm.clustering.cluster.Cluster` also has a special property: ``centroid``.
+	The centroid is a :class:`~vsm.vector.Vector` that represents the cluster's general direction.
+	It is calculated as an average over all the dimensions of the :class:`~vsm.vector.Vector` instances making up the cluster.
+
+	Clusters are based on the :class:`~objects.attributable.Attributable` class so they may have additional properties.
 
 	:ivar vectors: The list of vectors that make up the cluster.
 	:vartype vectors: list of :class:`~vsm.vector.Vector`
-	:ivar centroid: The centroid of the cluster, representing the average vector.
+	:ivar centroid: The centroid of the cluster, representing the average vector in the cluster.
 	:vartype centroid: :class:`~vsm.vector.Vector`
 	"""
 
@@ -39,7 +45,7 @@ class Cluster(Attributable, Exportable):
 		Initialize the cluster with an empty centroid and a list of vectors.
 
 		:param vectors: An initial list of vectors, or a single vector.
-						If `None` is given, an empty list is initialized instead.
+						If ``None`` is given, an empty list is initialized instead.
 		:type vectors: list of :class:`~vsm.vector.Vector` or :class:`~vsm.vector.Vector` or `None`
 		"""
 
@@ -50,6 +56,8 @@ class Cluster(Attributable, Exportable):
 	def similarity(self, vector, similarity_measure=vector_math.cosine):
 		"""
 		Calculate the similarity between the given vector and this cluster's centroid.
+		The similarity and its bounds depends on the similarity measure that is used.
+		The similarity measure defaults to :func:`~vsm.vector_math.cosine`, but a different measure can be provided.
 
 		:param vector: The vector that will be compared with the centroid.
 		:type vector: :class:`~vsm.vector.Vector`
@@ -65,6 +73,21 @@ class Cluster(Attributable, Exportable):
 	def recalculate_centroid(self):
 		"""
 		Recalculate the centroid.
+		The centroid is the average value of all dimensions across all vectors.
+		For example, the centroid's magnitude :math:`c_f` of feature :math:`f` is calculated as:
+
+		.. math::
+
+			c_f = \\frac{1}{|V|} \\sum^{v \\in V} v_f
+
+		where :math:`V` is the list of vectors in the cluster and :math:`v_f` is the magnitude of feature :math:`f` in vector :math:`V`.
+		After calculating the centroid, the function automatically normalizes it so that the centroid's magnitude is 1.
+
+		.. note::
+
+			Normally, you wouldn't need to call this function.
+			In fact, this function does not return the centroid.
+			This function is invoked automatically when you fetch the centroid.
 		"""
 
 		dimensions = set([ dimension for vector in self.vectors
@@ -80,6 +103,7 @@ class Cluster(Attributable, Exportable):
 	def centroid(self):
 		"""
 		Get the cluster's centroid.
+		Before returning the centroid, this function automatically re-calculates it.
 
 		:return: The cluster's centroid.
 		:rtype: :class:`~vsm.vector.Vector`
@@ -129,8 +153,8 @@ class Cluster(Attributable, Exportable):
 	def get_representative_vectors(self, vectors=1, similarity_measure=vector_math.cosine):
 		"""
 		Get the vectors that are closest to the centroid.
-		If the number of vectors that is sought is one, only the vector is returned.
-		Otherwise, a list of vectors is returned.
+		By default, :func:`~vsm.vector_math.cosine` is used to calculate the similarity between a vector and the centroid.
+		However, a different similarity measure can be provided.
 
 		:param vectors: The number of vectors the fetch.
 		:type vectors: int
@@ -138,6 +162,8 @@ class Cluster(Attributable, Exportable):
 		:type similarity_measure: func
 
 		:return: The representative vectors.
+				 If the number of vectors that is sought is one, only the closest vector is returned.
+		 		 Otherwise, a list of vectors is returned.
 		:rtype: :class:`~vsm.vector.Vector` or list of :class:`~vsm.vector.Vector`
 		"""
 
@@ -163,6 +189,11 @@ class Cluster(Attributable, Exportable):
 	def get_intra_similarity(self, similarity_measure=vector_math.cosine):
 		"""
 		Get the average similarity between vectors and the cluster.
+		This is calculated by comparing each vector with the cluster's centroid individually.
+		At the end, the function calculates the average similarity.
+
+		By default, :func:`~vsm.vector_math.cosine` is used to calculate the similarity between vectors in the cluster.
+		However, a different similarity measure can be provided.
 
 		:param similarity_measure: The similarity function to use to compare the likeliness of the vector with the cluster.
 		:type similarity_measure: func
@@ -190,7 +221,7 @@ class Cluster(Attributable, Exportable):
 	def to_array(self):
 		"""
 		Export the cluster as an associative array.
-		The centroid is not included as it is calculated upon instantiation.
+		The centroid is not included as it is calculated every time it is requested.
 
 		:return: The cluster as an associative array.
 		:rtype: dict
@@ -205,13 +236,13 @@ class Cluster(Attributable, Exportable):
 	@staticmethod
 	def from_array(array):
 		"""
-		Create an instance of the cluster from the given associative array.
+		Create a :class:`~vsm.clustering.cluster.Cluster` instance from the given associative array.
 
 		:param array: The associative array with the attributes to create the cluster.
 		:type array: dict
 
 		:return: A new instance of an object with the same attributes stored in the object.
-		:rtype: :class:`~vsm.cluster.cluster.Cluster`
+		:rtype: :class:`~vsm.clustering.cluster.Cluster`
 		"""
 
 		vectors = [ ]
