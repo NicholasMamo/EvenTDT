4fb35b87d23ec7b9bebb49a27d6bfa308ec88846
NicholasMamo <nicholasmamo@gmail.com>
2020-08-16 13:22:28 +0200

    Create the initial summarization tool

---
+++ tools/summarize.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""
+The summarization tool receives a timeline and creates a summary for each node.
+This tool is meant to summarize the ``consume`` tool's output retrospectively, after all tweets have been assigned to the correct cluster.
+Moreover, the summarization tool makes it easier to experiment with different parameters on the fly.
+"""
