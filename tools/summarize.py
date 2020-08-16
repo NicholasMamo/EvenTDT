e6bb9323ddac60448d51c4ec2b02e0a3fbfe93b1
NicholasMamo <nicholasmamo@gmail.com>
2020-08-16 13:23:25 +0200

    Make the summarization tool executable

--- tools/summarize.py
+++ tools/summarize.py
@@ -4,4 +4,10 @@
 The summarization tool receives a timeline and creates a summary for each node.
 This tool is meant to summarize the ``consume`` tool's output retrospectively, after all tweets have been assigned to the correct cluster.
 Moreover, the summarization tool makes it easier to experiment with different parameters on the fly.
+
+To run the script, use:
+
+.. code-block:: bash
+
+    ./tools/summarize.py
 """
