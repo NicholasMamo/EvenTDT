336e38a04afa01c1a7d29a5b841ade31808be240
NicholasMamo <nicholasmamo@gmail.com>
2020-08-16 13:37:07 +0200

    Update the summarization tool to support saving

--- tools/summarize.py
+++ tools/summarize.py
@@ -9,17 +9,44 @@ To run the script, use:
 
 .. code-block:: bash
 
-    ./tools/summarize.py
+    ./tools/summarize.py \\
+	--output data/summaries.json
+
+Accepted arguments:
+
+	- ``-o --output``		*<Required>* The path to the file where to store the generated summaries.
 """
 
 import argparse
 
+import os
+import sys
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+root = os.path.join(file_path, '..')
+lib = os.path.join(root, 'eventdt')
+sys.path.insert(-1, root)
+sys.path.insert(-1, lib)
+
+import tools
+
 def setup_args():
 	"""
 	Set up and get the list of command-line arguments.
+
+	Accepted arguments:
+
+		- ``-o --output``		*<Required>* The path to the file where to store the generated summaries.
+
+	:return: The command-line arguments.
+	:rtype: :class:`argparse.Namespace`
 	"""
 
 	parser = argparse.ArgumentParser(description="Summarize a timeline.")
+
+	parser.add_argument('-o', '--output',
+						type=str, required=True,
+						help='<Required> The path to the file where to store the generated summaries.')
 
 	args = parser.parse_args()
 	return args
@@ -30,6 +57,13 @@ def main():
 	"""
 
 	args = setup_args()
+
+	"""
+	Get the meta arguments.
+	"""
+	cmd = tools.meta(args)
+
+	tools.save(args.output, { 'meta': cmd })
 
 if __name__ == "__main__":
 	main()
