6675d364914e2402860feb10498f5ee0ecdbfcb5
NicholasMamo <nicholasmamo@gmail.com>
2020-08-18 17:31:19 +0200

    Update the aggregation module's documentation

--- eventdt/twitter/corpus/aggregate.py
+++ eventdt/twitter/corpus/aggregate.py
@@ -1,7 +1,6 @@
 """
-Functions to aggregate Twitter corpora.
-This can be used to create volume time-series, for example.
-Other aggregation functions can be provided.
+Aggregation functions are quick solutions to group tweets.
+These can be used to create tweet volume time-series quickly, for example.
 """
 
 import json
@@ -18,11 +17,48 @@ import twitter
 
 def aggregate(corpus, agg, bin_size=60, track=None, *args, **kwargs):
 	"""
-	Aggregate the given corpus.
+	Aggregate the given corpus, binning the tweets according to the when they were published.
 	The tracking keywords as well as any other arguments or keyword arguments are passed on to the aggregation function.
 
 	The function is data-agnostic in terms of whether it receives tweets or not.
 	However in order to bin tweets, the function expects that there is a ``timestamp_ms`` or ``created_at`` attribute in each object.
+
+	In addition, the aggregation function needs to be specified using the ``agg`` parameter.
+	Aggregation functions receive as input:
+
+	- A list of tweets as dictionaries, and
+	- The keyword which is being aggregated.
+
+	They return a value, or a score for the keyword's bin at a particular timestamp.
+	For example, a simple volume-based aggregation function could look like the following:
+
+	.. code-block:: python
+
+		def volume(tweets, keyword=None, *args, **kwargs):
+		  \"""
+		  Count the number of tweets that mention the given keyword.
+		  If no keyword is given, all tweets are counted.
+
+		  :param tweets: The list of tweets in the bin.
+		  :type tweets: list of dict
+		  :param keyword: The keyword to consider.
+		  				  The function looks for the keyword in the text.
+						  If no keyword is given, the number of tweets is returned.
+		  :type keyword: str or None
+
+		  :return: The number of tweets that mention the given keyword.
+		           If no keyword is given, the number of tweets is returned.
+		  :rtype: int
+		  \"""
+
+		  if keyword:
+		    return len([ tweet for tweet in tweets
+		                       if keyword in tweet['text'] ])
+		    else:
+		      return len(tweets)
+
+	The aggregation function vary depending on the application.
+	However, the return value must always be a float or an integer.
 
 	:param corpus: The file handle containing the data.
 	:type corpus: :class:`_io.TextIOWrapper`
