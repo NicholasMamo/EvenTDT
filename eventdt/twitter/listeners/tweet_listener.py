955538cce9525780c29f7459d8b05733d4e411b6
NicholasMamo <nicholasmamo@gmail.com>
2020-08-18 14:35:43 +0200

    Update the TweetListener class' documentation

--- eventdt/twitter/listeners/tweet_listener.py
+++ eventdt/twitter/listeners/tweet_listener.py
@@ -25,21 +25,40 @@ from logger import logger
 
 class TweetListener(StreamListener):
 	"""
-	The listener that handles the collected tweets.
+	The :class:`~twitter.listeners.tweet_listener.TweetListener` handles the tweets that the stream sends.
+	It does not configure the stream, but only processes the tweets it sends.
+
+	The :class:`~twitter.listeners.tweet_listener.TweetListener`'s state stores the file pointer where to save files.
+	By default, it accumulates tweets until the ``THRESHOLD`` constant is reached.
+	At that point, the accumulated tweets are saved to file.
+	In the meantime, it stores the tweets in the list of ``tweets``.
+
+	Although listeners do not control the stream's specifications, they can stop it.
+	The :class:`~twitter.listeners.tweet_listener.TweetListener` receives the ``max_time`` parameter which specifies, in seconds, the time to spend receiving tweets.
+	The :func:`~twitter.listeners.tweet_listener.TweetListener.on_data` function stops the stream when it receives a tweet after this time expires.
+	When it stops the stream, it saves any pending tweets to file.
+
+	Tweet objects contain many attributes, some of which may not be needed, but which create large corpora.
+	For example, feature-pivot algorithms that rely only on volume do not need anything except the tweet ID and the timestamp.
+	Therefore the :class:`~twitter.listeners.tweet_listener.TweetListener` also stores the ``attributes`` variable.
+	This represents a list of tweet attributes to save to file.
+	If it is not given, the tweets are stored without any filtering.
 
 	:cvar THRESHOLD: The number of tweets to accumulate before writing them to file.
 	:vartype THRESHOLD: int
 
-	:ivar file: The opened file pointer to which to write the tweets.
+	:ivar file: The opened file pointer where to write the tweets.
 	:vartype file: file
 	:ivar tweets: The list of read tweets that have not been written to file yet.
 	:vartype tweets: list
-	:ivar max_time: The maximum time in seconds to spend reading the file.
+	:ivar max_time: The maximum time in seconds to spend receiving tweets.
+					When this time expires, the listener instructs the stream to stop accepting tweets.
 	:vartype max_time: int
 	:ivar start: The timestamp when the listener started waiting for tweets.
+				 This variable is used to calculate the time the listener has spent receiving tweets.
 	:vartype start: int
 	:ivar attributes: The attributes to save from each tweet.
-					  If `None` is given, the entire tweet objects are saved.
+					  If ``None`` is given, the entire tweet objects are saved.
 	:vartype attributes: list of str or None
 	"""
 
@@ -51,12 +70,13 @@ class TweetListener(StreamListener):
 		Simultaneously set the file, the list of tweets and the number of processed tweets.
 		By default, the stream continues processing for an hour.
 
-		:param f: The opened file pointer to which to write the tweets.
+		:param f: The opened file pointer where to write the tweets.
 		:type f: file
-		:param max_time: The maximum time in seconds to spend reading the file.
+		:param max_time: The maximum time in seconds to spend receiving tweets.
+						 When this time expires, the listener instructs the stream to stop accepting tweets.
 		:type max_time: int
 		:param attributes: The attributes to save from each tweet.
-						   If `None` is given, the entire tweet objects are saved.
+						   If ``None`` is given, the entire tweet objects are saved.
 		:type attributes: list of str or None
 		"""
 
@@ -69,8 +89,12 @@ class TweetListener(StreamListener):
 	def flush(self):
 		"""
 		Flush the tweets to file.
-
 		Data is saved as a string to file.
+
+		.. note::
+
+			At this point, tweets are already JSON dictionaries and have a newline symbol at the end.
+			Since each tweet is a string representing a line, this function only concatenates these lines together.
 		"""
 
 		self.file.write(''.join(self.tweets))
@@ -78,16 +102,16 @@ class TweetListener(StreamListener):
 
 	def on_data(self, data):
 		"""
-		When tweets are received, add them to a list.
+		When the listener receives tweets, add them to a list.
 		If there are many tweets, save them to file and reset the list of tweets.
-		The override flag indicates whether the function should skip checking if the tweet is valid.
-
-		Data is saved as a string to file.
 
 		:param data: The received data.
 		:type data: listener
 
 		:return: A boolean indicating if the listener has finished reading tweets.
+				 It is set to ``True`` normally.
+				 When the elapsed time exceeds the ``max_time`` parameter, the :class:`~twitter.listeners.tweet_listener.TweetListener` returns ``False``.
+				 This instructs the stream to stop receiving tweets.
 		:rtype: bool
 		"""
 
@@ -115,8 +139,8 @@ class TweetListener(StreamListener):
 
 	def filter(self, tweet):
 		"""
-		Filter the given tweet using the attributes.
-		If no attributes are given, the tweet's attributes are all retained.
+		Filter the given tweet using the attributes specified when creating the :class:`~twitter.listeners.tweet_listener.TweetListener`.
+		If no attributes were given, the tweet's attributes are all retained.
 
 		:param tweet: The tweet attributes as a dictionary.
 					  The keys are the attribute names and the values are the data.
