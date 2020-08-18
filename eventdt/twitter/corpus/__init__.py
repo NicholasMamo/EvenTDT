c04c2cc07f664fb604c5432316c573243a5aff2d
NicholasMamo <nicholasmamo@gmail.com>
2020-08-18 17:23:53 +0200

    Update the corpus' documentation introduction

--- eventdt/twitter/corpus/__init__.py
+++ eventdt/twitter/corpus/__init__.py
@@ -1,1 +1,6 @@
+"""
+In addition corpus collection and reading, the Twitter package includes a few functions to help you process corpora.
+These functions are not meant to be complete solutions to any problem, but they can be used to quickly make sense of corpora or otherwise modify them using simple operations.
+"""
+
 from .aggregate import aggregate
