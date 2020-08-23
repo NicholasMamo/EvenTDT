9121ed0a09d7b16b54f4d57c874c3caf300bc982
NicholasMamo <nicholasmamo@gmail.com>
2020-08-23 14:36:57 +0200

    Implement a Twitter function to extract the full text from tweets

--- eventdt/twitter/__init__.py
+++ eventdt/twitter/__init__.py
@@ -11,7 +11,7 @@ def extract_timestamp(tweet):
 	This function looks for the timestamp in one of two fields:
 
 	1. ``timestamp_ms``: always present in the top-level tweets, and
-	2. ``created_at``: present in `retweeted_status`, for example.
+	2. ``created_at``: present in ``retweeted_status``, for example.
 
 	:param tweet: The tweet from which to extract the timestamp.
 	:type tweet: dict
@@ -30,3 +30,30 @@ def extract_timestamp(tweet):
 		return parse(tweet['created_at']).timestamp()
 
 	raise KeyError("Neither the 'timestamp_ms' attribute, nor the 'created_at' attribute could be found in the tweet.")
+
+def full_text(tweet):
+	"""
+	Extract the full text from the tweet.
+
+	Normally, long tweets are truncated (they end with a `…`).
+	This function looks for the original full text.
+	If it's a retweet, the text is somewhere in the ``retweeted_status``.
+	If the tweet has an ``extended_tweet`` attribute, then the ``full_text`` may be set there.
+	Otherwise, the function defaults to using the ``text``.
+
+	:param tweet: The tweet from which to extract the timestamp.
+	:type tweet: dict
+
+	:return: The full text of the tweet.
+	:rtype: str
+	"""
+
+	while "retweeted_status" in tweet:
+		tweet = tweet["retweeted_status"]
+
+	if "extended_tweet" in tweet:
+		text = tweet["extended_tweet"].get("full_text", tweet.get("text", ""))
+	else:
+		text = tweet.get("text", "")
+
+	return text
--- eventdt/twitter/tests/test_package.py
+++ eventdt/twitter/tests/test_package.py
@@ -14,6 +14,7 @@ if path not in sys.path:
     sys.path.append(path)
 
 import twitter
+from logger import logger
 
 class TestPackage(unittest.TestCase):
 	"""
@@ -89,3 +90,104 @@ class TestPackage(unittest.TestCase):
 			self.assertFalse('timestamp_ms' in tweet)
 			self.assertFalse('created_at' in tweet)
 			self.assertRaises(KeyError, twitter.extract_timestamp, tweet)
+
+	def test_full_text_ellipsis(self):
+		"""
+		Test that when the text has an ellipsis, the full text is retrieved.
+		"""
+
+		found = False
+		with open(os.path.join(os.path.dirname(__file__), '..', '..', 'tests', 'corpora', 'CRYCHE-500.json'), 'r') as f:
+			for line in f:
+				tweet = json.loads(line)
+				if '…' in tweet['text']:
+					found = True
+					text = twitter.full_text(tweet)
+
+					"""
+					Make an exception for a special case.
+					"""
+					if not ('retweeted_status' in tweet):
+						self.assertFalse(text.endswith('…'))
+
+		if not found:
+			logger.warning('Trivial test')
+
+	def test_full_text_quoted(self):
+		"""
+		Test that when the tweet is a quote, the text is used, not the quoted tweet's text.
+		"""
+
+		found = False
+		with open(os.path.join(os.path.dirname(__file__), '..', '..', 'tests', 'corpora', 'CRYCHE-500.json'), 'r') as f:
+			for line in f:
+				tweet = json.loads(line)
+				if 'retweeted_status' in tweet:
+					timestamp = tweet['timestamp_ms']
+					tweet = tweet['retweeted_status']
+					tweet['timestamp_ms'] = timestamp
+
+				if 'quoted_status' in tweet:
+					found = True
+					text = twitter.full_text(tweet)
+
+					if 'extended_tweet' in tweet:
+						self.assertEqual(tweet["extended_tweet"].get("full_text", tweet.get("text", "")), text)
+					else:
+						self.assertEqual(tweet.get('text'), text)
+
+		if not found:
+			logger.warning('Trivial test')
+
+	def test_full_text_retweeted(self):
+		"""
+		Test that when the tweet is a quote, the retweet's text is used.
+		"""
+
+		found = False
+		with open(os.path.join(os.path.dirname(__file__), '..', '..', 'tests', 'corpora', 'CRYCHE-500.json'), 'r') as f:
+			for line in f:
+				tweet = json.loads(line)
+				if 'retweeted_status' in tweet:
+					found = True
+					text = twitter.full_text(tweet)
+
+					retweet = tweet['retweeted_status']
+					if 'extended_tweet' in retweet:
+						self.assertEqual(retweet["extended_tweet"].get("full_text", retweet.get("text", "")), text)
+					else:
+						self.assertEqual(retweet.get('text'), text)
+
+					"""
+					Tweets shouldn't start with 'RT'.
+					"""
+					self.assertFalse(text.startswith('RT'))
+
+		if not found:
+			logger.warning('Trivial test')
+
+	def test_full_text_normal(self):
+		"""
+		Test that when the tweet is not a quote or retweet, the full text is used.
+		"""
+
+		found = False
+		with open(os.path.join(os.path.dirname(__file__), '..', '..', 'tests', 'corpora', 'CRYCHE-500.json'), 'r') as f:
+			for line in f:
+				tweet = json.loads(line)
+				if not 'retweeted_status' in tweet and not 'quoted_status' in tweet:
+					found = True
+					text = twitter.full_text(tweet)
+
+					if 'extended_tweet' in tweet:
+						self.assertEqual(tweet["extended_tweet"].get("full_text", tweet.get("text", "")), text)
+					else:
+						self.assertEqual(tweet.get('text'), text)
+
+					"""
+					There should be no ellipsis in the text now.
+					"""
+					self.assertFalse(text.endswith('…'))
+
+		if not found:
+			logger.warning('Trivial test')
