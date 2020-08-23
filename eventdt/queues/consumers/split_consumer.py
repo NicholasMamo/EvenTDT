8ee14feb8bbb94d9b2185a6ae2a6aa5087855dc1
NicholasMamo <nicholasmamo@gmail.com>
2020-08-23 17:04:01 +0200

    Create the initial SplitConsumer script

---
+++ eventdt/queues/consumers/split_consumer.py
@@ -0,0 +1,31 @@
+"""
+The :class:`~queues.consumers.split_consumer.SplitConsumer` builds on the base :class:`~queues.consumers.Consumer` class, but splits the incoming stream of tweets into several streams.
+These streams are simply a list of :class:`~queues.Queue` instances.
+
+The :class:`~queues.consumers.split_consumer.SplitConsumer` creates as many :class:`~queues.consumers.Consumer` instances as there :class:`~queues.Queue` instances—one :class:`~queues.Queue` instances for each :class:`~queues.consumers.Consumer`.
+All consumers process the tweets in their own :class:`~queues.Queue` as usual.
+
+The way the consumer splits the stream depends entirely on the the implementation.
+The consumer can assign each tweet to just one stream, or to multiple streams.
+It can also discard tweets.
+
+.. note::
+
+	The :class:`~queues.consumers.split_consumer.SplitConsumer` is based on the :class:`~queues.consumers.Consumer` class, so it operates in real-time.
+	All this means is that it sends tweets to the appropriate consumer without delay.
+	The processing, however, does not have to be in real-time.
+	The individual consumers that process the split streams can be based on the :class:`~queues.consumers.buffered_consumer.BufferedConsumer` and process tweets in batches.
+"""
+
+path = os.path.join(os.path.dirname(__file__), '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from queues import Queue
+from queues.consumers import Consumer
+
+class SplitConsumer(Consumer):
+	"""
+	"""
+
+	pass
