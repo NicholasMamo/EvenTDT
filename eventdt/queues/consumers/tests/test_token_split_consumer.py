d3a9cab2913814f2b05afd600c44c9ef4a7b36b3
NicholasMamo <nicholasmamo@gmail.com>
2020-08-24 16:24:13 +0200

    Create a function for asynchronous tests in the TokenSplitConsumer

--- eventdt/queues/consumers/tests/test_token_split_consumer.py
+++ eventdt/queues/consumers/tests/test_token_split_consumer.py
@@ -28,6 +28,14 @@ class TestTokenSplitConsumer(unittest.TestCase):
 	Test the implementation of the token split consumer.
 	"""
 
+	def async_test(f):
+		def wrapper(*args, **kwargs):
+			coro = asyncio.coroutine(f)
+			future = coro(*args, **kwargs)
+			loop = asyncio.get_event_loop()
+			loop.run_until_complete(future)
+		return wrapper
+
 	def test_init_custom_tokenizer(self):
 		"""
 		Test that when creating the token split consumer with a custom tokenizer, it is used instead of the default one.
