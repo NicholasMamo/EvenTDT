c3834ba31e6a0ad55afd3e17c65013e170f4d962
NicholasMamo <nicholasmamo@gmail.com>
2020-08-24 14:44:18 +0200

    Add the SplitConsumer unit tests to the general test script

--- tests.sh
+++ tests.sh
@@ -116,6 +116,7 @@ queue_tests() {
 	python3 -m unittest eventdt.queues.tests.test_queue
 
 	echo -e "${HIGHLIGHT}Consumers${DEFAULT}"
+	python -m unittest eventdt.queues.consumers.tests.test_split_consumer
 	python3 -m unittest eventdt.queues.consumers.algorithms.tests.test_eld_consumer
 	python3 -m unittest eventdt.queues.consumers.algorithms.tests.test_fire_consumer
 	python3 -m unittest eventdt.queues.consumers.algorithms.tests.test_zhao_consumer
