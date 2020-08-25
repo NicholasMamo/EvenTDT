fc2e0cba3062f48f443efb2ef927c2d01f45cb41
NicholasMamo <nicholasmamo@gmail.com>
2020-08-25 11:56:08 +0200

    Add a decorator for asynchronous unit tests in the buffered consumer

--- eventdt/queues/consumers/tests/test_buffered_consumer.py
+++ eventdt/queues/consumers/tests/test_buffered_consumer.py
@@ -22,3 +22,11 @@ class TestSimulatedBufferedConsumer(unittest.TestCase):
 	"""
 	Test the implementation of the buffered consumer.
 	"""
+
+	def async_test(f):
+		def wrapper(*args, **kwargs):
+			coro = asyncio.coroutine(f)
+			future = coro(*args, **kwargs)
+			loop = asyncio.get_event_loop()
+			loop.run_until_complete(future)
+		return wrapper
