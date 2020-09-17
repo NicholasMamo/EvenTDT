e3c97b58ab9672f6cd73517d6f8e3c42c13bb5dc
NicholasMamo <memonick@pop-os.localdomain>
2020-09-17 12:47:35 +0200

    Create the initial evaluation tools package

---
+++ tools/evaluation/__init__.py
@@ -0,0 +1,4 @@
+"""
+The evaluation package includes tools to evaluate different applications.
+At the package-level are implementations of common metrics, with a focus on information retrieval.
+"""
