2d4bcb5f97fcd10923cbdb1a9af61f7a0691c1b3
NicholasMamo <nicholasmamo@gmail.com>
2020-09-18 15:21:41 +0200

    Add support for the input argument in the ATE evaluation tool

--- tools/evaluation/ate.py
+++ tools/evaluation/ate.py
@@ -7,7 +7,12 @@ To run the tool, use:
 
 .. code-block:: bash
 
-    ./tools/evaluation/ate.py
+    ./tools/evaluation/ate.py \\
+    -- file evaluation/ate/results/terms.json
+
+Accepted arguments:
+
+	- ``-f --file``					*<Required>* The terms output to evaluate, which may be the output of the :mod:`~tools.terms` or :mod:`~tools.bootstrap` tools.
 """
 
 import argparse
@@ -16,11 +21,17 @@ def setup_args():
     """
     Set up and get the list of command-line arguments.
 
+    Accepted arguments:
+
+    	- ``-f --file``				*<Required>* The terms output to evaluate, which may be the output of the :mod:`~tools.terms` or :mod:`~tools.bootstrap` tools.
+
     :return: The command-line arguments.
     :rtype: :class:`argparse.Namespace`
     """
 
     parser = argparse.ArgumentParser(description="Evaluate the quality of an ATE algorithm's ranking.")
+    parser.add_argument('-f', '--file', type=str, required=True,
+						help='<Required> The file to use to construct the tokenized corpus.')
 
     args = parser.parse_args()
     return args
