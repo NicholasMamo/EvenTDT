26708a63f9544c778efd9e5fdc22db96c6ac9f4a
NicholasMamo <nicholasmamo@gmail.com>
2020-09-18 15:09:20 +0200

    Create the main ATE tool loop

--- tools/evaluation/ate.py
+++ tools/evaluation/ate.py
@@ -2,4 +2,36 @@
 
 """
 A tool to evaluate the ATE algorithms' output as a ranking.
+
+To run the tool, use:
+
+.. code-block:: bash
+
+    ./tools/evaluation/ate.py
 """
+
+import argparse
+
+def setup_args():
+    """
+    Set up and get the list of command-line arguments.
+
+    :return: The command-line arguments.
+    :rtype: :class:`argparse.Namespace`
+    """
+
+    parser = argparse.ArgumentParser(description="Evaluate the quality of an ATE algorithm's ranking.")
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    """
+    Main program loop.
+    """
+
+    args = setup_args()
+    print(args)
+
+if __name__ == "__main__":
+    main()
