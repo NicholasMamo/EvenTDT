ba0a51627e701423217b959f81f992ac438e0f2d
Nicholas Mamo <nicholasmamo@gmail.com>
2020-12-27 14:22:46 +0100

    Add EF-IDF-Entropy to package-level

--- eventdt/ate/application/__init__.py
+++ eventdt/ate/application/__init__.py
@@ -2,8 +2,4 @@
 Application-based approaches use the target application to learn the domain lexicon.
 """
 
-from .event import EF
-from .event import LogEF
-from .event import EFIDF
-from .event import Variability
-from .event import Entropy
+from .event import EF, LogEF, EFIDF, EFIDFEntropy, Variability, Entropy
