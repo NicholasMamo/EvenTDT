6acf2c96d048616e5dc93bf97a318a1cae71c4dc
Nicholas Mamo <nicholasmamo@gmail.com>
2021-01-10 16:46:28 +0100

    Add the attribute profile to the package level

--- eventdt/attributes/__init__.py
+++ eventdt/attributes/__init__.py
@@ -2,3 +2,5 @@
 Attribute extraction is the problem of extracting attributes and their values for classes or their instances.
 The classes in this library resolve around the :class:`~attributes.profile.Profile` class, which serves as a container for attributes.
 """
+
+from .profile import Profile
