1f6a03170a2f5d3a65c088d33ed1810fb9c43bbd
Nicholas Mamo <nicholasmamo@gmail.com>
2021-01-15 19:44:12 +0100

    Update the linguistic extractor to refer to subtrees by labels

--- eventdt/attributes/extractors/linguistic.py
+++ eventdt/attributes/extractors/linguistic.py
@@ -74,16 +74,16 @@ class LinguisticExtractor(Extractor):
         sentences = nltk.sent_tokenize(text)
         for sentence in sentences:
             tree = self._parse(sentence)
-            subtrees = self._attribute_subtrees(tree)
-            for subtree in subtrees:
-                name, PPATTR = self._to_attributes(subtree)
+            ATTR = self._attribute_subtrees(tree)
+            for _ATTR in ATTR:
+                NAME, PPATTR = self._to_attributes(_ATTR)
                 for _PPATTR in PPATTR:
-                    preposition = self._get_preposition(_PPATTR)
-                    name_pp = f"{name}_{ preposition }" if preposition else name
-                    profile.attributes[name_pp] = profile.attributes.get(name_pp) or set()
+                    IN = self._get_preposition(_PPATTR)
+                    name = f"{NAME}_{ IN }" if IN else NAME
+                    profile.attributes[name] = profile.attributes.get(name) or set()
                     attributes = self._get_attribute(_PPATTR)
                     for attribute in attributes:
-                        profile.attributes[name_pp].add(self._attribute_value(attribute))
+                        profile.attributes[name].add(self._attribute_value(attribute))
 
         return profile
 
@@ -110,74 +110,73 @@ class LinguisticExtractor(Extractor):
         :param tree: The parse tree of a sentence.
         :type tree: :class:`nltk.tree.Tree`
 
-        :return: Subtrees from the tree that correspond to attributes.
+        :return: Subtrees from the tree that correspond to attributes (having the label ``ATTR``).
         :rtype: list of :class:`nltk.tree.Tree`
         """
 
         subtrees = [ node for node in tree if type(node) is nltk.tree.Tree ]
-        subtrees = [ subtree for subtree in subtrees if subtree.label() == 'ATTR' ]
-        return subtrees
+        ATTR = [ subtree for subtree in subtrees if subtree.label() == 'ATTR' ]
+        return ATTR
 
-    def _to_attributes(self, subtree):
+    def _to_attributes(self, ATTR):
         """
-        Convert the given attribute subtree into a list of attributes.
-        One subtree may return multiple attributes because of conjunctions.
+        Convert the given ATTR subtree into a list of attributes.
+        One subtree may return multiple ATTR subtrees when it has conjunctions.
 
-        :param subtree: The subtree which will be split into attributes.
-        :type subtree: :class:`nltk.tree.Tree`
+        :param ATTR: The subtree which will be split into attributes.
+        :type ATTR: :class:`nltk.tree.Tree`
 
         :return: The attribute as a tuple, with the first value being the name and the second being the value.
-                 The attribute values are returned as lists of subtrees.
+                 The attribute values are returned as lists of ``PPATR`` subtrees.
         :rtype: tuple of str and list :class:`nltk.tree.Tree`
         """
 
-        name = [ component for component in subtree.subtrees() if component.label() == 'NAME' ][0]
+        name = [ component for component in ATTR.subtrees() if component.label() == 'NAME' ][0]
         name = [ text for text, pos in name.leaves() ]
-        values = [ component for component in subtree.subtrees() if component.label() == 'PPATTR' ]
+        PPATR = [ component for component in ATTR.subtrees() if component.label() == 'PPATTR' ]
+        return ('_'.join(name).lower(), PPATR)
 
-        return ('_'.join(name).lower(), values)
-
-    def _get_preposition(self, subtree):
+    def _get_preposition(self, PPATR):
         """
-        Extract the preposition from the given subtree, or ``None`` if the subtree doesn't have one.
+        Extract the preposition from the given ATTR subtree, or ``None`` if the subtree doesn't have one.
 
-        :param subtree: The subtree from where to extract the preposition.
-        :type subtree: :class:`nltk.tree.Tree`
+        :param PPATR: The subtree from where to extract the preposition.
+        :type PPATR: :class:`nltk.tree.Tree`
 
-        :return: The first preposition in the subtree or ``None`` if there is no preposition.
+        :return: The first preposition (with label ``IN``) in the subtree or ``None`` if there is no preposition.
         :rtype: str or None
         """
 
-        text, pos = subtree.flatten()[0]
+        text, pos = PPATR.flatten()[0]
         return text if pos == 'IN' else None
 
-    def _get_attribute(self, subtree):
+    def _get_attribute(self, PPATR):
         """
         Extract the attribute subtree from the given subtree.
 
-        :param subtree: The subtree from where to extract the attribute value.
-        :type subtree: :class:`nltk.tree.Tree`
+        :param PPATR: The subtree from where to extract the attribute value.
+        :type PPATR: :class:`nltk.tree.Tree`
 
         :return: The attribute value in the subtree.
         :rtype: :class:`nltk.tree.Tree`
         """
 
-        return [ _subtree for _subtree in subtree.subtrees() if _subtree.label() == 'VALUE' ]
+        return [ _subtree for _subtree in PPATR.subtrees() if _subtree.label() == 'VALUE' ]
 
-    def _attribute_value(self, subtree):
+    def _attribute_value(self, VALUE):
         """
         Extract the attribute value from the given value subtree.
         The tree has several components, including adjectives; this function extracts only the value.
 
-        :param subtree: The subtree from where to extract the attribute value.
-        :type subtree: :class:`nltk.tree.Tree`
+        :param VALUE: The subtree from where to extract the attribute value.
+        :type VALUE: :class:`nltk.tree.Tree`
 
-        :return: The actual value from the attribute value subtree.
+        :return: The actual textual value from the attribute value subtree.
         :rtype: str
         """
 
         value = [ ]
-        head = subtree[-1] if (type(subtree[-1]) is nltk.tree.Tree and subtree[-1].label() == 'ENT') else subtree
+        head = VALUE[-1] if (type(VALUE[-1]) is nltk.tree.Tree and VALUE[-1].label() == 'ENT') else VALUE
         for text, pos in head.leaves():
             value.append(text)
         return (' '.join(value).lower())
