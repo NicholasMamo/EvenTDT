f5535641643f6cf1d300011bbaeca5ae5de25cac
Nicholas Mamo <nicholasmamo@gmail.com>
2021-01-26 16:41:48 +0100

    Add the meta file path as a parameter to the shareable tool

--- tools/shareable.py
+++ tools/shareable.py
@@ -10,12 +10,24 @@ To run this tool, use the following:
 .. code-block:: bash
 
     ./tools/shareable.py \\
-    --file data/corpus.json
+    --file data/corpus.json \\
+    --output data/shareable.json
+
+By default, the tool saves the meta details to a file in the same directory.
+However, you can save the meta file in a different path by providing the ``--meta` parameter:
+
+.. code-block:: bash
+
+    ./tools/shareable.py \\
+    --file data/corpus.json \\
+    --output data/shareable.json \\
+    --meta meta/shareable.json
 
 The full list of accepted arguments:
 
     - ``-f --file``                          *<Required>* The original corpus collected by the :mod:`~tools.collect` tool.
     - ``-o --output``                        *<Required>* The file where to save the shareable corpus.
+    - ``--meta``                             *<Optional>* The file where to save the meta data, defaults to [--file].meta.json.
 """
 
 import argparse
@@ -28,6 +40,7 @@ def setup_args():
 
         - ``-f --file``             *<Required>* The original corpus collected by the :mod:`~tools.collect` tool.
         - ``-o --output``           *<Required>* The file where to save the shareable corpus.
+        - ``--meta``                *<Optional>* The file where to save the meta data, defaults to [--file].meta.json.
 
     :return: The command-line arguments.
     :rtype: :class:`argparse.Namespace`
@@ -39,6 +52,8 @@ def setup_args():
                         help='<Required> The original corpus collected by the `collect` tool.')
     parser.add_argument('-o', '--output', type=str, required=True,
                         help='<Required> The file where to save the shareable corpus.')
+    parser.add_argument('--meta', type=str, required=False,
+                        help='<Optional> The file where to save the meta data, defaults to [--file].meta.json.')
 
     args = parser.parse_args()
     return args
