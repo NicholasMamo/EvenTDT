9aead08748f125bb3d7d031f9f0f0f7de42226e7
Nicholas Mamo <nicholasmamo@gmail.com>
2021-01-26 16:35:09 +0100

    Create the initial structure of the shareable tool

--- tools/shareable.py
+++ tools/shareable.py
@@ -11,3 +11,31 @@ To run this tool, use the following:
 
     ./tools/shareable.py
 """
+
+import argparse
+
+def setup_args():
+    """
+    Set up and get the list of command-line arguments.
+
+    :return: The command-line arguments.
+    :rtype: :class:`argparse.Namespace`
+    """
+
+    parser = argparse.ArgumentParser(description="Make a dataset shareable.")
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    """
+    Main program loop.
+    """
+
+    """
+    Set up the arguments, create the tokenizer and prepare the data directory.
+    """
+    args = setup_args()
+
+if __name__ == "__main__":
+    main()
