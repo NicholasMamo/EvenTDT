86a1d54ec042ea1f7085887c318abd1d1247554a
Nicholas Mamo <nicholasmamo@gmail.com>
2021-01-26 16:33:58 +0100

    Make the shareable tool executable

--- tools/shareable.py
+++ tools/shareable.py
@@ -4,4 +4,10 @@
 The shareable tool is used to convert tweet datasets into shareable datasets.
 As per `Twitter's Developer Policy <https://developer.twitter.com/en/developer-terms/policy>`_, only tweet IDs can be shared.
 Therefore this tool retains only the tweet IDs from corpora.
+
+To run this tool, use the following:
+
+.. code-block:: bash
+
+    ./tools/shareable.py
 """
