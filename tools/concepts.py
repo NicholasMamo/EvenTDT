9666f9c2b7e8dfbb54c7d7f7bd16adc4e55f5803
Nicholas Mamo <nicholasmamo@gmail.com>
2021-05-30 11:34:56 +0200

    Add support for saving in the concepts tool

--- tools/concepts.py
+++ tools/concepts.py
@@ -3,6 +3,33 @@
 """
 A tool to extract lexical concepts from terms.
 
+To extract concepts, specify the output file:
+
+.. code-block:: bash
+
+    ./tools/terms.py \\
+    --output data/concepts.json
+
+The output is a JSON file with the following structure:
+
+.. code-block:: json
+
+    {
+        "cmd": {
+            "output": "data/concepts.json",
+            "_date": "2021-05-30T11:32:04.775216",
+            "_timestamp": 1622367124.7752266,
+            "_cmd": "./tools/concepts.py --output data/concepts.json"
+        },
+        "pcmd": {
+            "output": "data/concepts.json",
+            "_date": "2021-05-30T11:32:04.775232",
+            "_timestamp": 1622367124.775234,
+            "_cmd": "./tools/concepts.py --output data/concepts.json"
+        }
+    }
+
+
 The full list of accepted arguments:
 
     - ``-o --output``           *<Required>* The path to the file where to store the extracted terms.
@@ -49,6 +76,9 @@ def main():
 
     # get the meta arguments
     cmd, pcmd = tools.meta(args), tools.meta(args)
+
+    # save the meta data and concepts to file
+    tools.save(args.output, { 'cmd': cmd, 'pcmd': pcmd })
 
 if __name__ == "__main__":
     main()
