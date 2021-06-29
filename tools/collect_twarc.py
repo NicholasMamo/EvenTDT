3157d1f36c067fbc941d9efbef6487e5c545f812
Nicholas Mamo <nicholasmamo@gmail.com>
2021-06-29 20:19:01 +0200

    Add an example of the Twitter v2's filtered stream that uses twarc

---
+++ tools/collect_twarc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from twarc import Twarc2, expansions
+import json
+
+# Replace your bearer token below
+client = Twarc2(bearer_token="AAAAAAAAAAAAAAAAAAAAAADMNwEAAAAA%2BbeDu2ehGnP%2BS55%2Fi8x9D0IpvrU%3DyzmsQO8ilo9B4H4M4ZvgSpy7aHr05alm67Bt7j27jkZVtJOsSn")
+
+# NOTE: twarc automatically expands everything. See the `__twarc` key in each collected tweet for the full list of expansions.
+
+def main():
+    # Remove existing rules
+    existing_rules = client.get_stream_rules()
+    if 'data' in existing_rules and len(existing_rules['data']) > 0:
+        rule_ids = [rule['id'] for rule in existing_rules['data']]
+        client.delete_stream_rule_ids(rule_ids)
+
+    # Add new rules
+    # Replace the rules below with the ones that you want to add as discussed in module 5
+    new_rules = [
+        {"value": "#engger lang:en", "tag": "euro 2020"},
+        {"value": "covid", "tag": "corona-related-tweets"}
+    ]
+    added_rules = client.add_stream_rules(rules=new_rules)
+
+    # Connect to the filtered stream
+    for count, result in enumerate(client.stream()):
+        # The Twitter API v2 returns the Tweet information and the user, media etc.  separately
+        # so we use expansions.flatten to get all the information in a single JSON
+        tweet = expansions.flatten(result)
+        # Here we are printing the full Tweet object JSON to the console
+        print(json.dumps(tweet))
+        # Replace with the desired number of Tweets
+        if count > 100:
+            break
+
+    # Delete the rules once you have collected the desired number of Tweets
+    rule_ids = [rule['id'] for rule in added_rules['data']]
+    client.delete_stream_rule_ids(rule_ids)
+
+
+if __name__ == "__main__":
+    main()
