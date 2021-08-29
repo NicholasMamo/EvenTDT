4b289cd19b488af6fb0a116929658f0abd655e46
Nicholas Mamo <nicholasmamo@gmail.com>
2021-08-29 13:08:06 +0200

    Add support for the arguments in the download tool

--- tools/download.py
+++ tools/download.py
@@ -3,6 +3,21 @@
 """
 The download tool is used to download tweets from shareable datasets, which are made up of tweet IDs.
 The tool expects as input a file with one tweet ID on each line, produced by the :mod:`~tools.shareable` tool.
+
+To run this tool, use the following:
+
+.. code-block:: bash
+
+    ./tools/download.py \\
+    --file data/shareable.txt \\
+    --output data/output.json \\
+    --meta meta/output.json
+
+The full list of accepted arguments:
+
+    - ``-f --file``                          *<Required>* The shareable corpus created by the :mod:`~tools.collect` tool.
+    - ``-o --output``                        *<Required>* The file where to save the downloaded corpus.
+    - ``--meta``                             *<Optional>* The file where to save the meta data, defaults to [--file].meta.
 """
 
 import argparse
@@ -19,11 +34,24 @@ def setup_args():
     """
     Set up and get the list of command-line arguments.
 
+    Accepted arguments:
+
+        - ``-f --file``                          *<Required>* The shareable corpus created by the :mod:`~tools.collect` tool.
+        - ``-o --output``                        *<Required>* The file where to save the downloaded corpus.
+        - ``--meta``                             *<Optional>* The file where to save the meta data, defaults to [--file].meta.
+
     :return: The command-line arguments.
     :rtype: :class:`argparse.Namespace`
     """
 
     parser = argparse.ArgumentParser(description="Download a shareable dataset.")
+
+    parser.add_argument('-f', '--file', type=str, required=True,
+                        help='<Required> The shareable corpus created by the `shareable` tool.')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='<Required> The file where to save the downloaded corpus.')
+    parser.add_argument('--meta', type=str, required=False,
+                        help='<Optional> The file where to save the meta data, defaults to [--file].meta.')
 
     args = parser.parse_args()
     return args
