5fde0b8f99e8021246813bca45e6f297a79fcd76
Nicholas Mamo <nicholasmamo@gmail.com>
2022-02-21 17:00:43 +0100

    Add the BearerTokenAuth class to the Twitter listeners package

--- eventdt/twitter/listeners/__init__.py
+++ eventdt/twitter/listeners/__init__.py
@@ -3,5 +3,6 @@ Listeners are used to download corpora in real-time.
 These classes automatically connect to the APIs to fetch tweets based on a number of parameters.
 """
 
+from .bearer_token_auth import BearerTokenAuth
 from .tweet_listener import TweetListener
 from .queued_tweet_listener import QueuedTweetListener
