c46579903c35a6cb29839e498fb3c4881d26b262
Nicholas Mamo <nicholasmamo@gmail.com>
2022-02-25 13:19:08 +0100

    Update the token extractor to read tokens from files

--- eventdt/apd/extractors/local/tests/test_token_extractor.py
+++ eventdt/apd/extractors/local/tests/test_token_extractor.py
@@ -2,6 +2,7 @@
 Test the functionality of the token extractor.
 """
 
+import json
 import os
 import sys
 import unittest
@@ -17,6 +18,7 @@ from apd.extractors.local.token_extractor import TokenExtractor
 
 from nlp.document import Document
 from nlp.tokenizer import Tokenizer
+import twitter
 
 def ignore_warnings(test):
     """
@@ -42,25 +44,18 @@ class TestExtractors(unittest.TestCase):
     """
 
     @ignore_warnings
-    def test_token_extractor(self):
+    def test_token_extractor_no_tokenizer(self):
         """
-        Test the token extractor with normal input.
+        Test that the token extractor without a tokenizer simply splits the text.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stopwords=stopwords.words("english"), stem=False)
-        posts = [
-            "Manchester United falter against Tottenham Hotspur",
-            "Mourinho under pressure as Manchester United follow with a loss",
-        ]
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
 
         extractor = TokenExtractor()
-        candidates = extractor.extract(corpus)
-        self.assertEqual(set([ "manchester", "united", "falter", "tottenham", "hotspur" ]), set(candidates[0]))
-        self.assertEqual(set([ "mourinho", "pressure", "manchester", "united", "follow", "loss" ]), set(candidates[1]))
+        candidates = extractor.extract(path)
+        self.assertEqual(['Bayern', 'Munich', 'make', 'second', 'bid', 'worth',
+                          'more', 'than', '£20m', 'for', '18-year-old',
+                          'Chelsea', 'forward', 'Callum', 'Hudson-Odoi'], candidates[0])
 
     @ignore_warnings
     def test_empty_corpus(self):
@@ -69,7 +64,7 @@ class TestExtractors(unittest.TestCase):
         """
 
         extractor = TokenExtractor()
-        candidates = extractor.extract([ ])
+        candidates = extractor.extract(os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'empty.json'))
         self.assertFalse(len(candidates))
 
     @ignore_warnings
@@ -78,64 +73,74 @@ class TestExtractors(unittest.TestCase):
         Test that the token extractor returns as many token sets as the number of documents given.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stopwords=stopwords.words("english"), stem=False)
-        posts = [
-            "Manchester United falter against Tottenham Hotspur",
-            "",
-        ]
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
 
         extractor = TokenExtractor()
-        candidates = extractor.extract(corpus)
-        self.assertEqual(2, len(candidates))
-        self.assertEqual(set([ "manchester", "united", "falter", "tottenham", "hotspur" ]), set(candidates[0]))
-        self.assertEqual(set([ ]), set(candidates[1]))
+        candidates = extractor.extract(path)
+        self.assertEqual(100, len(candidates))
 
     @ignore_warnings
     def test_extract_with_custom_tokenizer(self):
         """
-        Test that when a custom tokenizer is given, it is used instead of the dimensions.
+        Test that when a custom tokenizer is given, it is used instead of splitting the text.
         """
 
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+
+        # create the extractor with a tokenizer that removes stopwords and applies stemming
+        extractor = TokenExtractor(tokenizer=Tokenizer(stopwords=stopwords.words('english'), stem=True))
+        candidates = extractor.extract(path)
+        self.assertEqual(['bayern', 'munich', 'make', 'second', 'bid',
+                          'worth', '£20m', 'year', 'old', 'chelsea',
+                          'forward', 'callum', 'hudson', 'odoi'], candidates[0])
+
+    @ignore_warnings
+    def test_extract_full_text(self):
         """
-        Create the test data, which uses stemming.
+        Test that the extractor uses the full text.
         """
-        tokenizer = Tokenizer(stopwords=stopwords.words("english"), stem=True)
-        posts = [
-            "Manchester United back to winning ways",
-        ]
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
+
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
 
         extractor = TokenExtractor()
-        candidates = extractor.extract(corpus)
-        self.assertEqual(set([ "manchest", "unit", "back", "win", "way" ]), set(candidates[0]))
+        candidates = extractor.extract(path)
+        self.assertTrue(all( '…' not in tokens for tokens in candidates ))
+        self.assertTrue(all( '…' not in token for tokens in candidates for token in tokens ))
 
-        extractor = TokenExtractor(tokenizer=Tokenizer(stopwords=stopwords.words('english'), stem=False))
-        candidates = extractor.extract(corpus)
-        self.assertEqual(set([ "manchester", "united", "back", "winning", "ways" ]), set(candidates[0]))
+    @ignore_warnings
+    def test_extract_expands_mentions(self):
+        """
+        Test that the extractor expands mentions.
+        """
+
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+
+        extractor = TokenExtractor()
+        candidates = extractor.extract(path)
+        self.assertTrue(all( '@' not in tokens or 'bet' in tokens for tokens in candidates ))
+        self.assertTrue(all( '@' not in token or 'bet' in tokens or 'Stream' in tokens for tokens in candidates for token in tokens ))
+
+    @ignore_warnings
+    def test_repeated_tokens(self):
+        """
+        Test that when no tokenizer is given, repeated tokens may appear multiple times.
+        """
+
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+
+        extractor = TokenExtractor()
+        candidates = extractor.extract(path)
+        self.assertEqual(2, candidates[1].count('Eden'))
 
     @ignore_warnings
     def test_repeated_tokens_with_custom_tokenizer(self):
         """
-        Test that when a custom tokenizer is given, repeated tokenizers appear multiple times.
+        Test that when a custom tokenizer is given, repeated tokens may appear multiple times.
         """
 
-        """
-        Create the test data.
-        """
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+
         tokenizer = Tokenizer(stopwords=stopwords.words("english"), stem=False)
-        posts = [
-            "Manchester United back to winning ways after defeating Manchester City.",
-        ]
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
-        extractor = TokenExtractor()
-        candidates = extractor.extract(corpus)
-        self.assertEqual(1, candidates[0].count('manchester'))
-
         extractor = TokenExtractor(tokenizer=tokenizer)
-        candidates = extractor.extract(corpus)
-        self.assertEqual(2, candidates[0].count('manchester'))
+        candidates = extractor.extract(path)
+        self.assertEqual(2, candidates[1].count('eden'))
--- eventdt/apd/extractors/local/token_extractor.py
+++ eventdt/apd/extractors/local/token_extractor.py
@@ -3,7 +3,16 @@ The token extractor considers all the tokens in the corpus to be candidate parti
 Therefore it does not perform any filtering whatsoever on the corpus.
 """
 
+import json
+import os
+import sys
+
+path = os.path.join(os.path.dirname(__file__), '..', '..', '..')
+if path not in sys.path:
+    sys.path.append(path)
+
 from ..extractor import Extractor
+import twitter
 
 class TokenExtractor(Extractor):
     """
@@ -41,14 +50,21 @@ class TokenExtractor(Extractor):
         Each outer list represents a document.
         Each inner list is the candidates in that document.
 
-        :param corpus: The corpus of documents where to extract candidate participants.
-        :type corpus: list
+        :param corpus: A path to the corpus of documents from where to extract candidate participants.
+        :type corpus: str
 
         :return: A list of candidates separated by the document in which they were found.
         :rtype: list of list of str
         """
 
-        if self.tokenizer:
-            return [ self.tokenizer.tokenize(document.text) for document in corpus ]
-        else:
-            return [ [ token for token in document.dimensions ] for document in corpus ]
+        candidates = [ ]
+
+        with open(corpus) as f:
+            for line in f:
+                tweet = json.loads(line)
+                text = twitter.full_text(tweet)
+                text = twitter.expand_mentions(text, tweet)
+
+                candidates.append(self.tokenizer.tokenize(text) if self.tokenizer else text.split())
+
+        return candidates
