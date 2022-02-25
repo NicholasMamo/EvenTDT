542532dcf77a6549e25c426fbeb13f974c2ca35e
Nicholas Mamo <nicholasmamo@gmail.com>
2022-02-25 20:10:30 +0100

    Update the `ThresholdFilter`'s unit tests

--- eventdt/apd/filters/local/tests/test_threshold_filter.py
+++ eventdt/apd/filters/local/tests/test_threshold_filter.py
@@ -2,6 +2,7 @@
 Test the functionality of the threshold filter.
 """
 
+import copy
 import math
 import os
 import sys
@@ -24,182 +25,117 @@ class TestThresholdFilter(unittest.TestCase):
     Test the implementation and results of the threshold filter.
     """
 
-    def test_threshold_filter(self):
+    def test_threshold_filter_does_not_change_scores(self):
         """
-        Test the basic functionality of the threshold filter.
+        Test that the threshold filter does not change the scores.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = EntityExtractor()
         scorer = TFScorer()
-        filter = ThresholdFilter(0.75)
+        filter = ThresholdFilter(5)
 
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
+        filtered = filter.filter(scores)
+        self.assertTrue(all( score == scores[candidate]
+                             for candidate, score in filtered.items() ))
 
-        self.assertEqual(1, scores.get('erdogan', 0))
-        self.assertEqual(0.5, scores.get('damascus', 0))
+    def test_scores_not_changed(self):
+        """
+        Test that the original scores are not changed when filtering.
+        """
 
-        scores = filter.filter(scores)
-        self.assertTrue('erdogan' in scores)
-        self.assertFalse('damascus' in scores)
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+        extractor = EntityExtractor()
+        scorer = TFScorer()
+        filter = ThresholdFilter(5)
+
+        candidates = extractor.extract(path)
+        scores = scorer.score(candidates)
+        original = copy.deepcopy(scores)
+        filtered = filter.filter(scores)
+        self.assertEqual(original, scores)
 
     def test_negative_threshold(self):
         """
         Test that when a negative threshold is given, all candidate participants are retained.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = EntityExtractor()
         scorer = TFScorer()
         filter = ThresholdFilter(-1)
 
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
 
-        self.assertEqual(1, scores.get('erdogan', 0))
-        self.assertEqual(0.5, scores.get('damascus', 0))
-
-        scores = filter.filter(scores)
-        self.assertTrue('erdogan' in scores)
-        self.assertTrue('damascus' in scores)
+        filtered = filter.filter(scores)
+        self.assertEqual(scores, filtered)
 
     def test_zero_threshold(self):
         """
         Test that when a threshold of zero is given, all candidate participants are retained.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = EntityExtractor()
         scorer = TFScorer()
         filter = ThresholdFilter(0)
 
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
 
-        self.assertEqual(1, scores.get('erdogan', 0))
-        self.assertEqual(0.5, scores.get('damascus', 0))
-
-        scores = filter.filter(scores)
-        self.assertTrue('erdogan' in scores)
-        self.assertTrue('damascus' in scores)
+        filtered = filter.filter(scores)
+        self.assertEqual(scores, filtered)
 
     def test_equal_threshold(self):
         """
         Test that when a named entity has a score that is equal to the threshold, it is retained.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = EntityExtractor()
         scorer = TFScorer()
-        filter = ThresholdFilter(0.5)
+        threshold = 5
+        filter = ThresholdFilter(threshold)
 
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
-
-        self.assertEqual(1, scores.get('erdogan', 0))
-        self.assertEqual(0.5, scores.get('damascus', 0))
-
-        scores = filter.filter(scores)
-        self.assertTrue('erdogan' in scores)
-        self.assertTrue('damascus' in scores)
+        filtered = filter.filter(scores)
+        self.assertTrue(all( candidate in filtered
+                             for candidate, score in scores.items()
+                             if score >= threshold ))
 
     def test_marginal_threshold(self):
         """
         Test that when a threshold exceeds a candidate score, the filter excludes them.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = EntityExtractor()
         scorer = TFScorer()
-        filter = ThresholdFilter(0.51)
+        threshold = 5
+        filter = ThresholdFilter(threshold)
 
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
-
-        self.assertEqual(1, scores.get('erdogan', 0))
-        self.assertEqual(0.5, scores.get('damascus', 0))
-
-        scores = filter.filter(scores)
-        self.assertTrue('erdogan' in scores)
-        self.assertFalse('damascus' in scores)
+        filtered = filter.filter(scores)
+        self.assertTrue(all( candidate not in filtered
+                             for candidate, score in scores.items()
+                             if score < threshold ))
 
     def test_high_threshold(self):
         """
         Test that when a high threshold is given, no candidate participants are retained.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = EntityExtractor()
         scorer = TFScorer()
-        filter = ThresholdFilter(1.1)
+        filter = ThresholdFilter(1.01)
 
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
-
-        self.assertEqual(1, scores.get('erdogan', 0))
-        self.assertEqual(0.5, scores.get('damascus', 0))
-
-        scores = filter.filter(scores)
-        self.assertFalse('erdogan' in scores)
-        self.assertFalse('damascus' in scores)
+        filtered = filter.filter(scores)
+        self.assertFalse(filtered)
