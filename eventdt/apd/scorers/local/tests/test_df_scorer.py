b77d6ef9eb087f5fdea5ba56f2d3880361511291
Nicholas Mamo <nicholasmamo@gmail.com>
2022-02-25 17:57:43 +0100

    Update the `DFScorer` to case-fold candidates

--- eventdt/apd/scorers/local/tests/test_df_scorer.py
+++ eventdt/apd/scorers/local/tests/test_df_scorer.py
@@ -2,6 +2,7 @@
 Test the functionality of the DF scorer.
 """
 
+import copy
 import math
 import os
 import sys
@@ -28,44 +29,61 @@ class TestDFScorer(unittest.TestCase):
         Test the basic functionality of the DF scorer.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = TokenExtractor()
         scorer = DFScorer()
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
+        scores = scorer.score(candidates, normalize_scores=False)
+        candidates = scorer._fold(candidates)
+        chelsea = sum([ True if 'chelsea' in candidate_set else False for candidate_set in candidates ])
+        self.assertEqual(chelsea, scores.get('chelsea', 0))
+
+    def test_score_candidates_unchanged(self):
+        """
+        Test that the original list of candidates do not change after undergoing case-folding.
+        """
+
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+        extractor = TokenExtractor()
+        scorer = DFScorer()
+        candidates = extractor.extract(path)
+        original = copy.deepcopy(candidates)
+        scorer.score(candidates)
+        self.assertEqual(original, candidates)
+
+    def test_score_candidates_folded(self):
+        """
+        Test that the candidates are case-folded when scoring.
+        """
+
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+        extractor = TokenExtractor()
+        scorer = DFScorer()
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
-        self.assertEqual(1, scores.get('erdogan', 0))
-        self.assertEqual(0.5, scores.get('damascus', 0))
-        self.assertEqual(1, scores.get('threats', 0))
+        self.assertTrue(all( candidate.lower() == candidate for candidate in scores ))
+
+    def test_score_candidates_all_retained(self):
+        """
+        Test that when scoring candidates, all of them are retained, if folded.
+        """
+
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
+        extractor = TokenExtractor()
+        scorer = DFScorer()
+        candidates = extractor.extract(path)
+        scores = scorer.score(candidates)
+        self.assertTrue(all( candidate.lower() in scores for candidate_set in candidates for candidate in candidate_set ))
 
     def test_min_score(self):
         """
         Test that the minimum score is greater than 0.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = TokenExtractor()
         scorer = DFScorer()
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
         self.assertTrue(all( score > 0 for score in scores.values() ))
 
@@ -74,20 +92,10 @@ class TestDFScorer(unittest.TestCase):
         Test that the maximum score is 1 when normalization is enabled.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = TokenExtractor()
         scorer = DFScorer()
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
         self.assertTrue(all( score <= 1 for score in scores.values() ))
 
@@ -96,84 +104,33 @@ class TestDFScorer(unittest.TestCase):
         Test that the score of an unknown token is 0.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = TokenExtractor()
         scorer = DFScorer()
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates)
         self.assertFalse(scores.get('unknown'))
-
-    def test_score_across_multiple_documents(self):
-        """
-        Test that the score is based on document frequency.
-        """
-
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "After Erdogan's statement, Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
-        extractor = TokenExtractor()
-        scorer = DFScorer()
-        candidates = extractor.extract(corpus, tokenizer=tokenizer)
-        scores = scorer.score(candidates, normalize_scores=False)
-        self.assertEqual(2, scores.get('erdogan'))
 
     def test_normalization(self):
         """
         Test that when normalization is disabled, the returned scores are integers.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "Erdogan with threats to attack regime forces 'everywhere' in Syria",
-            "After Erdogan's statement, Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = TokenExtractor()
         scorer = DFScorer()
-        candidates = extractor.extract(corpus)
+        candidates = extractor.extract(path)
         scores = scorer.score(candidates, normalize_scores=False)
-        self.assertEqual(2, scores.get('erdogan'))
+        self.assertTrue(all( score >= 1 for score in scores.values() ))
+        self.assertTrue(all( score % 1 == 0 for score in scores.values() ))
 
     def test_repeated_tokens(self):
         """
         Test that when tokens are repeated, the frequency that is returned is the document frequency.
         """
 
-        """
-        Create the test data.
-        """
-        tokenizer = Tokenizer(stem=False)
-        posts = [
-            "After Erdogan's statement, Damascus says Erdogan 'disconnected from reality' after threats",
-        ]
-
-        corpus = [ Document(post, tokenizer.tokenize(post)) for post in posts ]
-
+        path = os.path.join(os.path.dirname(__file__), '..', '..',  '..', '..', 'tests', 'corpora', 'CRYCHE-100.json')
         extractor = TokenExtractor()
         scorer = DFScorer()
-        candidates = extractor.extract(corpus, tokenizer=tokenizer)
-        scores = scorer.score(candidates, normalize_scores=False)
-        self.assertEqual(1, scores.get('erdogan'))
+        candidates = extractor.extract(path)
+        self.assertTrue(all( score == 1 for candidate_set in candidates for score in scorer.score([ candidate_set ]).values() ))
