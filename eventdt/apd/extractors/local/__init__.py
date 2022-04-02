6676cdabbf8d0d613a8b8ac4e0636c2d07fe7950
Nicholas Mamo <nicholasmamo@gmail.com>
2022-04-02 18:59:52 +0200

    Add the `AnnotationExtractor` to the package level

--- eventdt/apd/extractors/local/__init__.py
+++ eventdt/apd/extractors/local/__init__.py
@@ -2,5 +2,6 @@
 Local extractors look for candidate participants in the event's corpus.
 """
 
+from .annotation_extractor import AnnotationExtractor
 from .entity_extractor import EntityExtractor
 from .token_extractor import TokenExtractor
