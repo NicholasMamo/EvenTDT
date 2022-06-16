c1e3b4c2f307975812f92a45464d5bd82bac3aa5
Nicholas Mamo <nicholasmamo@gmail.com>
2022-06-16 18:29:25 +0200

    Create the initial `EventModel` class

--- eventdt/modeling/__init__.py
+++ eventdt/modeling/__init__.py
@@ -1,3 +1,10 @@
 """
 The event modeling package contains classes and functions that make it easier to formally represent events.
 """
+
+class EventModel(object):
+    """
+    The :class:`~modeling.EventModel` represents events.
+    """
+
+    pass
