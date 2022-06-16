035417aa14665dba09fa8054cc805f7a23bd0f65
Nicholas Mamo <nicholasmamo@gmail.com>
2022-06-16 19:12:05 +0200

    Make the `EventModel` extend and use the `Exportable` class

--- eventdt/modeling/tests/test_package.py
+++ eventdt/modeling/tests/test_package.py
@@ -2,6 +2,7 @@
 Test the functionality of the package functions.
 """
 
+from datetime import datetime
 import json
 import os
 import sys
@@ -43,3 +44,87 @@ class TestPackage(unittest.TestCase):
         model = EventModel(who='Truman', attributes={ 'version': 1 })
         self.assertEqual('Truman', model.who)
         self.assertEqual(1, model.version)
+
+    def test_event_model_copy(self):
+        """
+        Test that copying the event model includes all attributes.
+        """
+
+        model = EventModel(who='Truman', what='speech', where='Maryland',
+                           when=datetime(1947, 6, 29), attributes={ 'version': 1 })
+
+        copy = model.copy()
+        self.assertEqual(model.who, copy.who)
+        self.assertEqual(model.what, copy.what)
+        self.assertEqual(model.where, copy.where)
+        self.assertEqual(model.when, copy.when)
+        self.assertEqual(model.why, copy.why)
+        self.assertEqual(model.how, copy.how)
+
+    def test_event_model_copy_attributes(self):
+        """
+        Test that the attributes are also copied.
+        """
+
+        model = EventModel(who='Truman', what='speech', where='Maryland',
+                           when=datetime(1947, 6, 29), attributes={ 'version': 1 })
+        copy = model.copy()
+        self.assertEqual(model.attributes, copy.attributes)
+
+    def test_event_model_copy_attributes_original(self):
+        """
+        Test that changing the copy's attributes does not affect the original's, and vice-versa.
+        """
+
+        model = EventModel(who='Truman', what='speech', where='Maryland',
+                           when=datetime(1947, 6, 29), attributes={ 'version': 1 })
+        copy = model.copy()
+
+        copy.attributes['who'] = 'Harry Truman'
+        self.assertEqual('Harry Truman', copy.who)
+        self.assertEqual('Truman', model.who)
+
+        model.attributes['who'] = 'Harry S. Truman'
+        self.assertEqual('Harry Truman', copy.who)
+        self.assertEqual('Harry S. Truman', model.who)
+
+    def test_event_model_copy_nested_attributes_original(self):
+        """
+        Test that changing the copy's nested attributes does not affect the original's, and vice-versa.
+        """
+
+        model = EventModel(who='Truman', what='speech', where='Maryland',
+                           when=datetime(1947, 6, 29), attributes={ 'version': { 'library': 'EvenTDT', 'version': 1 } })
+        copy = model.copy()
+
+        copy.attributes['version']['version'] = 2
+        self.assertEqual(2, copy.version['version'])
+        self.assertEqual(1, model.version['version'])
+
+        model.attributes['version']['version'] = 2.2
+        self.assertEqual(2, copy.version['version'])
+        self.assertEqual(2.2, model.version['version'])
+
+    def test_event_model_export(self):
+        """
+        Test exporting and importing an event model.
+        """
+
+        model = EventModel(who='Truman', what='speech', where='Maryland',
+                           when=datetime(1947, 6, 29), attributes={ 'version': { 'library': 'EvenTDT', 'version': 1 } })
+        exported = model.to_array()
+
+        self.assertEqual(model.attributes, EventModel.from_array(exported).attributes)
+        self.assertEqual(model.__dict__, EventModel.from_array(exported).__dict__)
+
+    def test_event_model_export_attributes(self):
+        """
+        Test that exporting and importing event models includes attributes.
+        """
+
+        model = EventModel(who='Truman', what='speech', where='Maryland',
+                           when=datetime(1947, 6, 29), attributes={ 'version': { 'library': 'EvenTDT', 'version': 1 } })
+        exported = model.to_array()
+
+        self.assertEqual(model.attributes, EventModel.from_array(exported).attributes)
+        self.assertEqual(model.__dict__, EventModel.from_array(exported).__dict__)
