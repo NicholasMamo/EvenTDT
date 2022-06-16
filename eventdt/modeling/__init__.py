7127b2c66e1e3ceba9d5d2c10a7f5254159f79ef
Nicholas Mamo <nicholasmamo@gmail.com>
2022-06-16 18:57:18 +0200

    Make the `EventModel` extend and use the `Attributable` class

--- eventdt/modeling/__init__.py
+++ eventdt/modeling/__init__.py
@@ -2,9 +2,41 @@
 The event modeling package contains classes and functions that make it easier to formally represent events.
 """
 
-class EventModel(object):
+import os
+import sys
+
+path = os.path.join(os.path.dirname(__file__), '..')
+if path not in sys.path:
+    sys.path.append(path)
+
+from objects import Attributable
+
+class EventModel(Attributable):
     """
-    The :class:`~modeling.EventModel` represents events.
+    The :class:`~modeling.EventModel` represents events using the `five Ws and one H` as attributes.
+    The class always initializes the six attributes and does not impose any restrictions on the class.
+    Nevertheless, event modelers might impose their own restrictions, such as using :class:`datetime.datetime` instances for the When.
     """
 
-    pass
+    def __init__(self, who=None, what=None, where=None, when=None, why=None, how=None, *args, **kwargs):
+        """
+        Create the event model from the `five Ws and one H`.
+        The constructor stores the six aspects as attributes.
+
+        :param who: The person or persons involved in the event.
+        :type who: Any
+        :param what: The subject, action or changes of an event.
+        :type what: Any
+        :param where: The location where the event is taking place.
+        :type where: Any
+        :param when: The time or time periods when the event takes place.
+        :type when: Any
+        :param why: The reasons why an event occurred, which could be another :class:`~modeling.EventModel`.
+        :type why: Any
+        :param how: The reasons how an event occurred, which could be another :class:`~modeling.EventModel`.
+        :type how: Any
+        """
+
+        super(EventModel, self).__init__(*args, **kwargs)
+        self.attributes.update({ 'who': who, 'what': what, 'where': where,
+                                 'when': when, 'why': why, 'how': how })
--- eventdt/modeling/tests/test_package.py
+++ eventdt/modeling/tests/test_package.py
@@ -19,4 +19,27 @@ class TestPackage(unittest.TestCase):
     Test the functionality of the package functions.
     """
 
-    pass
+    def test_event_model_init_empty(self):
+        """
+        Test that by default, the event model still stores the `five Ws and one H`.
+        """
+
+        model = EventModel()
+        self.assertEqual({ 'who', 'what', 'where', 'when', 'why', 'how' }, set(model.attributes.keys()))
+
+    def test_event_model_init_with_attributes(self):
+        """
+        Test that the event model accepts additional attributes in additional to the `five Ws and one H`.
+        """
+
+        model = EventModel(attributes={ 'version': 1 })
+        self.assertEqual({ 'version', 'who', 'what', 'where', 'when', 'why', 'how' }, set(model.attributes.keys()))
+
+    def test_event_model_init_no_overwrite(self):
+        """
+        Test that the event model does not overwrite any attributes.
+        """
+
+        model = EventModel(who='Truman', attributes={ 'version': 1 })
+        self.assertEqual('Truman', model.who)
+        self.assertEqual(1, model.version)
