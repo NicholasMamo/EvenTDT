337f9d93e4d76fcf7da891697ed8bb7b222ced06
Nicholas Mamo <nicholasmamo@gmail.com>
2022-06-24 20:26:26 +0200

    Add names to the profiles created in the `UnderstandingModeler` test cases

--- eventdt/modeling/modelers/tests/test_understanding_modeler.py
+++ eventdt/modeling/modelers/tests/test_understanding_modeler.py
@@ -29,16 +29,16 @@ class TestUnderstandingModeler(unittest.TestCase):
         Create a list of mock participants that represent the Who and the Where.
         """
 
-        corpus = [
-            "Max Emilian Verstappen (born 30 September 1997) is a Belgian-Dutch racing driver and the 2021 Formula One World Champion.",
-            "Pierre Gasly (French pronunciation: ​[pjɛʁ ɡasli]; born 7 February 1996) is a French racing driver, currently competing in Formula One under the French flag, racing for Scuderia AlphaTauri.",
-            "Lance Strulovitch,[2] (born 29 October 1998) better known as Lance Stroll, is a Canadian-Belgian[3] racing driver competing under the Canadian flag in Formula One.",
-            "The Circuit Gilles Villeneuve (also spelled Circuit Gilles-Villeneuve in French) is a 4.361 km (2.710 mi) motor racing circuit in Montreal, Quebec, Canada.",
-            "Montreal (/ˌmʌntriˈɔːl/ (listen) MUN-tree-AWL; officially Montréal, French: [mɔ̃ʁeal] (listen)) is the second-most populous city in Canada and most populous city in the Canadian province of Quebec.",
-        ]
+        corpus = {
+            "Max Verstappen": "Max Emilian Verstappen (born 30 September 1997) is a Belgian-Dutch racing driver and the 2021 Formula One World Champion.",
+            "Pierre Gasly": "Pierre Gasly (French pronunciation: ​[pjɛʁ ɡasli]; born 7 February 1996) is a French racing driver, currently competing in Formula One under the French flag, racing for Scuderia AlphaTauri.",
+            "Lance Stroll": "Lance Strulovitch,[2] (born 29 October 1998) better known as Lance Stroll, is a Canadian-Belgian[3] racing driver competing under the Canadian flag in Formula One.",
+            "Circuit Gilles Villeneuve": "The Circuit Gilles Villeneuve (also spelled Circuit Gilles-Villeneuve in French) is a 4.361 km (2.710 mi) motor racing circuit in Montreal, Quebec, Canada.",
+            "Montreal": "Montreal (/ˌmʌntriˈɔːl/ (listen) MUN-tree-AWL; officially Montréal, French: [mɔ̃ʁeal] (listen)) is the second-most populous city in Canada and most populous city in the Canadian province of Quebec.",
+        }
 
         extractor = LinguisticExtractor()
-        return [ extractor.extract(text) for text in corpus ]
+        return [ extractor.extract(text, name=name) for name, text in corpus.items() ]
 
     def test_init_saves_participants(self):
         """
