d4fa223d7c7c646c8aa600bab9ce9fade2fe6ba6
Nicholas Mamo <nicholasmamo@gmail.com>
2022-07-17 14:46:39 +0200

    Update the `participants` tool to pass the tokenizer to the APD models

--- tools/participants.py
+++ tools/participants.py
@@ -331,7 +331,8 @@ def create_model(model, extractor, scorer, filter, resolver, extrapolator, postp
         scheme = tools.load(scheme)['tfidf']
         return model(scheme=scheme, extractor=extractor,
                      scorer=scorer, filter=filter, resolver=resolver,
-                     extrapolator=extrapolator, postprocessor=postprocessor, corpus=kwargs.get('file'), *args, **kwargs)
+                     extrapolator=extrapolator, postprocessor=postprocessor,
+                     corpus=kwargs.get('file'), tokenizer=tokenizer, *args, **kwargs)
     elif model.__name__ == ParticipantDetector.__name__:
         extractor = extractor or local.EntityExtractor(*args, **kwargs)
         scorer = scorer or TFScorer(*args, **kwargs)
