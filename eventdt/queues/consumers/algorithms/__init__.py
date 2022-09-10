d04a7757bdeff7870256fb883344d2a6eddd3398
Nicholas Mamo <nicholasmamo@gmail.com>
2022-09-10 12:39:22 +0200

    Add a new enum that represents the `FilterLevel`

--- eventdt/queues/consumers/algorithms/__init__.py
+++ eventdt/queues/consumers/algorithms/__init__.py
@@ -17,14 +17,30 @@ class DynamicThreshold(Enum):
     The type of dynamic threshold to use.
     Options:
 
-        - MEAN - The mean volume observed so far
-        - MOVING_MEAN - The mean volume from the previous $n$ windows
-        - MEAN_STDEV - One standard deviation above the mean volume observed so far
+        - MEAN:        The mean volume observed so far
+        - MOVING_MEAN: The mean volume from the previous $n$ windows
+        - MEAN_STDEV:  One standard deviation above the mean volume observed so far
     """
 
     MEAN = 1
     MOVING_MEAN = 2
     MEAN_STDEV = 3
+
+class FilterLevel(Enum):
+    """
+    The amount of filtering to apply on tweets.
+    The actual interpretations, except for `NONE`, depends on the algorithms.
+    The `LENIENT` option can be used when it is acceptable for tweets to have URLs; for example, when detecting breaking news in unspecified domains, newsrooms often break the news themselves.
+    Options:
+
+        - NONE:     Do not filter any tweets.
+        - LENIENT:  Filter tweets leniently.
+        - STRICT:   Filter tweets strictly.
+    """
+
+    NONE = 1
+    LENIENT = 2
+    STRICT = 3
 
 from .eld_consumer import ELDConsumer
 from .fire_consumer import FIREConsumer
